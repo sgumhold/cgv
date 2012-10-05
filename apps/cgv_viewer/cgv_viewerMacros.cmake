e161b19528d4b07bad6e93d929b15a0c45199ade
Joachim Staib <joachim.staib@tu-dresden.de>
2012-10-05 14:26:05 +0200

    CMake: the working directory of a cgv_viewer can now be specified

--- apps/cgv_viewer/cgv_viewerMacros.cmake
+++ apps/cgv_viewer/cgv_viewerMacros.cmake
@@ -1,5 +1,101 @@
 get_filename_component(BASE_PATH "${CMAKE_CURRENT_LIST_FILE}" PATH)
 
+
+
+macro(cgv_set_viewer_workdir dir)
+	set(${PROJECT_NAME}_VIEWER_WORK_DIR ${dir})
+endmacro()
+
+
+
+macro(_cgv_add_shared_viewer name)
+	include(rpavlik/CreateLaunchers)
+	if (NOT TARGET ${name})
+		add_custom_target(${name})
+	endif()
+	add_dependencies(${name} ${cgv_viewer_EXECUTABLE} ${LIBS})
+	
+	set(ARGS_LIST "")
+
+	set(CONFIGS ${CMAKE_CONFIGURATION_TYPES})
+	if (NOT CONFIGS)
+		set(CONFIGS "Release;Debug")
+	endif()		
+
+	foreach(config ${CONFIGS})
+		get_target_property(V_${config} ${cgv_viewer_EXECUTABLE} LOCATION_${config})
+		set_target_properties(${name} PROPERTIES EXECUTE_LOCATION_${config} "${V_${config}}")
+		
+		set(CFG_ARGS "")
+		set(LIB_PATHS ".")
+		foreach(lib ${LIBS})
+			if (NOT TARGET ${lib})
+				message(FATAL_ERROR "Error generating viewer ${name}. One of the requested plugins does not seem to be a plugin.")
+			endif()
+			
+			get_target_property(LOC ${lib} LOCATION_${config})
+			get_filename_component(ABS_LOC_PATH "${LOC}" PATH)
+			list(APPEND LIB_PATHS "${ABS_LOC_PATH}")
+
+			set(CFG_ARGS "${CFG_ARGS} plugin:\"${LOC}\"")
+
+			get_filename_component(LOC_PATH "${LOC}" PATH)
+
+		endforeach()
+		list(APPEND ARGS_LIST "ARGS_${config};${CFG_ARGS}")
+		set(ARGS_${config} ${CFG_ARGS})
+	endforeach()
+
+	list(REMOVE_DUPLICATES LIB_PATHS)
+
+	set(VIEWER_WORK_DIR ${${PROJECT_NAME}_VIEWER_WORK_DIR})
+#	if (NOT VIEWER_WORK_DIR)
+#		set(VIEWER_WORK_DIR ${CMAKE_CURRENT_SOURCE_DIR})
+#	endif()
+	if (NOT IS_ABSOLUTE "${VIEWER_WORK_DIR}")
+		set(VIEWER_WORK_DIR "${CMAKE_CURRENT_SOURCE_DIR}/${VIEWER_WORK_DIR}")
+	endif()
+
+	create_target_launcher( 
+		${name} 
+		${ARGS_LIST}
+		RUNTIME_LIBRARY_DIRS ${LIB_PATHS}
+		WORKING_DIRECTORY ${VIEWER_WORK_DIR})			
+endmacro()
+
+
+
+
+macro(_cgv_add_static_viewer name)
+	set(VIEWERNAME ${name})
+	# FIXME: If the viewer name equals the name of a plugin then do not create a new target, but:
+	# 1. Change the target to be an executable
+	# 2. Include the main.cxx file into its sources
+	if (TARGET ${name})
+		set(VIEWERNAME ${name}_viewer)
+	endif()
+	
+	add_executable(${VIEWERNAME} ${CGV_DIR}/apps/cgv_viewer/main.cxx)
+
+	set(STATIC_DEFS ${STATIC_DEFS} "CGV_FORCE_STATIC")
+	list(REMOVE_DUPLICATES STATIC_DEFS)
+	set_target_properties(${VIEWERNAME} PROPERTIES COMPILE_DEFINITIONS "${STATIC_DEFS}")
+	
+	# Linux needs the additional "--whole-archive" flag. Otherwise the libraries will not be linked
+	# As there are no direct references in the viewer main file
+	if (WIN32)
+		target_link_libraries(${VIEWERNAME} ${LIBS})
+	else()
+		target_link_libraries(${VIEWERNAME} "-Wl,--whole-archive" ${LIBS} "-Wl,--no-whole-archive")
+	endif()
+
+	# FIXME: set the viewer working directory
+
+endmacro()
+
+
+
+
 macro(cgv_add_viewer name)
 	set(LIBS "")
 	set(STATIC_DEFS "")
@@ -24,70 +120,8 @@ macro(cgv_add_viewer name)
 	endforeach()
 
 	if (BUILD_SHARED_LIBS)
-		include(rpavlik/CreateLaunchers)
-		if (NOT TARGET ${name})
-			add_custom_target(${name})
-		endif()
-		add_dependencies(${name} ${cgv_viewer_EXECUTABLE} ${LIBS})
-		
-		set(ARGS_LIST "")
-
-		set(CONFIGS ${CMAKE_CONFIGURATION_TYPES})
-		if (NOT CONFIGS)
-			set(CONFIGS "Release;Debug")
-		endif()		
-
-		foreach(config ${CONFIGS})
-			get_target_property(V_${config} ${cgv_viewer_EXECUTABLE} LOCATION_${config})
-			set_target_properties(${name} PROPERTIES EXECUTE_LOCATION_${config} "${V_${config}}")
-			
-			set(CFG_ARGS "")
-			set(LIB_PATHS ".")
-			foreach(lib ${LIBS})
-				if (NOT TARGET ${lib})
-					message(FATAL_ERROR "Error generating viewer ${name}. One of the requested plugins does not seem to be a plugin.")
-				endif()
-				
-				get_target_property(LOC ${lib} LOCATION_${config})
-				file(RELATIVE_PATH LOC "${CMAKE_CURRENT_SOURCE_DIR}" "${LOC}")
-
-				set(CFG_ARGS "${CFG_ARGS} plugin:\"${LOC}\"")
-
-				get_filename_component(LOC_PATH "${LOC}" PATH)
-				list(APPEND LIB_PATHS "${LOC_PATH}")
-
-			endforeach()
-			list(APPEND ARGS_LIST "ARGS_${config};${CFG_ARGS}")
-			set(ARGS_${config} ${CFG_ARGS})
-		endforeach()
-
-		list(REMOVE_DUPLICATES LIB_PATHS)
-
-		create_target_launcher( 
-			${name} 
-			${ARGS_LIST}
-			RUNTIME_LIBRARY_DIRS ${LIB_PATHS})			
+		_cgv_add_shared_viewer(${name})
 	else()
-		set(VIEWERNAME ${name})
-		# FIXME: If the viewer name equals the name of a plugin then do not create a new target, but:
-		# 1. Change the target to be an executable
-		# 2. Include the main.cxx file into its sources
-		if (TARGET ${name})
-			set(VIEWERNAME ${name}_viewer)
-		endif()
-		
-		add_executable(${VIEWERNAME} ${CGV_DIR}/apps/cgv_viewer/main.cxx)
-
-		set(STATIC_DEFS ${STATIC_DEFS} "CGV_FORCE_STATIC")
-		list(REMOVE_DUPLICATES STATIC_DEFS)
-		set_target_properties(${VIEWERNAME} PROPERTIES COMPILE_DEFINITIONS "${STATIC_DEFS}")
-		
-		# Linux needs the additional "--whole-archive" flag. Otherwise the libraries will not be linked
-		# As there are no direct references in the viewer main file
-		if (WIN32)
-			target_link_libraries(${VIEWERNAME} ${LIBS})
-		else()
-			target_link_libraries(${VIEWERNAME} "-Wl,--whole-archive" ${LIBS} "-Wl,--no-whole-archive")
-		endif()
+		_cgv_add_static_viewer(${name})
 	endif()
 endmacro()
