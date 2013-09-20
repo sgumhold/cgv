8f1739217bce9592b794087a0c4ef9144cabf567
Your Name <you@example.com>
2013-09-20 13:38:18 +0200

    on the way to get rid of the urgent problem

--- libs/cgv_reflect_types/media/color.h
+++ libs/cgv_reflect_types/media/color.h
@@ -21,12 +21,16 @@ struct color : public cgv::media::color<T,cm,am>
 };
 
 		}
-
-template <typename T, cgv::media::ColorModel cm, cgv::media::AlphaModel am> 
-extern_string_reflection_traits<cgv::media::color<T,cm,am>, cgv::reflect::media::color<T,cm,am> > get_reflection_traits(const cgv::media::color<T,cm,am>&) { return extern_string_reflection_traits<cgv::media::color<T,cm,am>, cgv::reflect::media::color<T,cm,am> >(); }
-
 extern CGV_API enum_reflection_traits<cgv::media::ColorModel> get_reflection_traits(const cgv::media::ColorModel&);
 extern CGV_API enum_reflection_traits<cgv::media::AlphaModel> get_reflection_traits(const cgv::media::AlphaModel&);
+	}
+}
+
+namespace cgv {
+	namespace media {
+template <typename T, ColorModel cm, AlphaModel am> 
+cgv::reflect::extern_string_reflection_traits<color<T,cm,am>, cgv::reflect::media::color<T,cm,am> > get_reflection_traits(const color<T,cm,am>&) { return cgv::reflect::extern_string_reflection_traits<color<T,cm,am>, cgv::reflect::media::color<T,cm,am> >(); }
+
 
 	}
 }
