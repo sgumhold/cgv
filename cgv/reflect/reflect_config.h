76b7710358686b8e4f8797e1c17710c39d67220f
unknown <gumhold@.(none)>
2013-09-20 23:20:01 +0200

    ups, forgot to switch back configuration

--- cgv/reflect/reflect_config.h
+++ cgv/reflect/reflect_config.h
@@ -3,8 +3,8 @@
 #include <cgv/config/cpp_version.h>
 
 #ifdef CPP11
-#define REFLECT_IN_CLASS_NAMESPACE
-#else
 #define REFLECT_TRAITS_WITH_DECLTYPE
+#else
+#define REFLECT_IN_CLASS_NAMESPACE
 #endif
 
