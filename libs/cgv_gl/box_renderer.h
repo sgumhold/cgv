65070fa878b54774c1452db1caba126d5671126c
gumhold <stefan.gumhold@tu-dresden.de>
2018-07-25 14:25:55 +0200

    added comments and started to work on per box transformation

--- libs/cgv_gl/box_renderer.h
+++ libs/cgv_gl/box_renderer.h
@@ -11,7 +11,12 @@ namespace cgv { // @<
 		class CGV_API box_renderer : public surface_renderer
 		{
 		protected:
+			/// store whether extent array has been specified is specified
 			bool has_extents;
+			/// whether array with per box translations has been specified
+			bool has_translations;
+			/// whether array with per box rotations has been specified
+			bool has_rotations;
 			/// whether position is box center, if not it is lower left bottom corner
 			bool position_is_center;
 			/// overload to allow instantiation of box_renderer
@@ -24,9 +29,10 @@ namespace cgv { // @<
 			bool init(context& ctx);
 			/// 
 			bool enable(context& ctx);
-
+			/// extent array specifies box extends in case of position_is_center=true, otherwise the maximum point of each box
 			template <typename T>
 			void set_extent_array(context& ctx, const std::vector<T>& extents) { has_extents = true;  set_attribute_array(ctx, ref_prog().get_attribute_location(ctx, "extent"), extents); }
+			/// extent array specifies box extends in case of position_is_center=true, otherwise the maximum point of each box
 			template <typename T>
 			void set_extent_array(context& ctx, const T* extents, size_t nr_elements, size_t stride_in_bytes = 0) { has_extents = true;  set_attribute_array(ctx, ref_prog().get_attribute_location(ctx, "extent"), extents, nr_elements, stride_in_bytes); }
 			bool validate_attributes(context& ctx);
