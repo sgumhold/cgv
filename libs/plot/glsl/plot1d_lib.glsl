efb7a31bc9c60a823d89b53dfa3be8c68997c49e
gumhold <stefan.gumhold@tu-dresden.de>
2018-10-08 00:27:29 +0200

    implemented log scale plot and added support for multiple series in one sub plot

--- libs/plot/glsl/plot1d_lib.glsl
+++ libs/plot/glsl/plot1d_lib.glsl
@@ -12,7 +12,19 @@ uniform vec3 center_location;
 
 vec4 map_plot_to_world(in vec2 pnt)
 {
-	vec2 delta = extent*(pnt - 0.5*(domain_min_pnt + domain_max_pnt) )/(domain_max_pnt - domain_min_pnt);
+	vec2 delta;
+	if (x_axis_log_scale) {
+		delta.x = extent.x * (log(pnt.x) - 0.5*(log(domain_min_pnt.x) + log(domain_max_pnt.x))) / (log(domain_max_pnt.x) - log(domain_min_pnt.x));
+	}
+	else {
+		delta.x = extent.x * (pnt.x - 0.5*(domain_min_pnt.x + domain_max_pnt.x)) / (domain_max_pnt.x - domain_min_pnt.x);
+	}
+	if (y_axis_log_scale) {
+		delta.y = extent.y * (log(pnt.y) - 0.5*(log(domain_min_pnt.y) + log(domain_max_pnt.y))) / (log(domain_max_pnt.y) - log(domain_min_pnt.y));
+	}
+	else {
+		delta.y = extent.y * (pnt.y - 0.5*(domain_min_pnt.y + domain_max_pnt.y)) / (domain_max_pnt.y - domain_min_pnt.y);
+	}
 	vec3 p     = center_location + delta.x * x_axis + delta.y * y_axis;
 	return vec4(p,1.0);
 }
