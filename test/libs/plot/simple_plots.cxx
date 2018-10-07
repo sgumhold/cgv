20e28903ddc2b84acfcac6bc2e64014c49d88ffc
gumhold <stefan.gumhold@tu-dresden.de>
2018-10-08 00:28:14 +0200

    implemented cool browser that shows plots of browsed files

--- test/libs/plot/simple_plots.cxx
+++ test/libs/plot/simple_plots.cxx
@@ -6,6 +6,7 @@
 #include <cgv/signal/rebind.h>
 #include <libs/cgv_gl/gl/gl.h>
 
+/*
 template <typename T>
 void sample_plot1d_interval(std::function<T(T)> f, T xmin, T xmax, unsigned nr_x, std::vector<cgv::math::fvec<T,2> >& pnts) 
 {
@@ -90,11 +91,11 @@ void configure_ribbon_plots(cgv::plot::plot1d& p1, float beta)
 		}
 	}
 
-/*
-	p1.add_sub_plot("sin");
-	sample_plot1d_interval<float>([&](float x) { return fabs(sin(x)); }, -M_PI, M_PI, 1000, p1.ref_sub_plot_samples(3));
-	p1.set_sub_plot_colors(3, cgv::plot::plot1d::Clr(1, 0, 1));
-	*/
+
+//	p1.add_sub_plot("sin");
+//	sample_plot1d_interval<float>([&](float x) { return fabs(sin(x)); }, -M_PI, M_PI, 1000, p1.ref_sub_plot_samples(3));
+//	p1.set_sub_plot_colors(3, cgv::plot::plot1d::Clr(1, 0, 1));
+	
 }
 
 
@@ -353,7 +354,596 @@ public:
 		//gg->get_parent()->cast<cgv::gui::gui_group>()->select_child(cgv::base::base_ptr(gg),true);
 	}
 };
+*/
+
+
+#include <cgv/utils/file.h>
+#include <cgv/utils/dir.h>
+#include <cgv/utils/scan.h>
+#include <cgv/gui/event_handler.h>
+#include <cgv/gui/mouse_event.h>
+#include <cgv/utils/advanced_scan.h>
+#include <cgv/base/register.h>
+
+using namespace cgv::base;
+using namespace cgv::reflect;
+using namespace cgv::render;
+using namespace cgv::gui;
+using namespace cgv::media;
+using namespace cgv::utils;
+using namespace cgv::utils::file;
+using namespace std;
+
+void scan_dir(const std::string& path, const std::string& dir, vector<string>& dirs, vector<string>& files)
+{
+	string p = path + "/";
+	if (!dir.empty())
+		p += dir + "/";
+	p += "*";
+	void* h = find_first(p);
+	while (h) {
+		string p = dir;
+		if (!p.empty())
+			p += "/";
+		p = find_name(h);
+		if (find_directory(h)) {
+			if (find_name(h) != ".." && find_name(h) != ".")
+				dirs.push_back(p);
+		}
+		else
+			files.push_back(p);
+		h = find_next(h);
+	}
+}
+string split_path(const string& path)
+{
+	string p(path);
+	replace(p, '\\', '/');
+	return p;
+}
+
+enum BrowserSelectionMode
+{
+	BSM_FILE,
+	BSM_DIRECTORY,
+	BSM_TREE
+};
+
+struct dataset
+{
+	std::string title;
+	std::vector<std::string> headers;
+	std::vector<std::vector<double> > data;
+};
+
+class browser_test : public node, public cgv::render::drawable, public cgv::gui::event_handler, public  cgv::gui::provider, public argument_handler
+{
+protected:
+	BrowserSelectionMode selection_mode;
+	unsigned selected_dataset;
+	bool auto_x, auto_y;
+	bool adjust_min;
+	std::vector<std::string> headers;
+	std::string file_path;
+	bool incremental_build;
+	cgv::gui::gui_group_ptr gp;
+	//cgv::gui::gui_group_ptr ap;
+	//cgv::gui::gui_group_ptr bp;
+	//cgv::gui::gui_group_ptr pp;
+	std::map<void*, std::string> file_path_map;
+	cgv::plot::plot1d p1;
+
+	std::vector<dataset*> datasets;
+
+	std::vector<int> column_selection;
+
+	bool is_directory(const std::string& file_name)
+	{
+		void* handle = cgv::utils::file::find_first(file_name);
+		if (!handle)
+			return false;
+		return find_directory(handle);
+	}
+	bool consider_path_or_file(const std::string& name)
+	{
+		if (cgv::utils::file::exists(name)) {
+			file_path = cgv::utils::file::get_path(name);
+			post_recreate_gui();
+			return true;
+		}
+		if (cgv::utils::dir::exists(name)) {
+			file_path = name;
+			post_recreate_gui();
+			return true;
+		}
+		return false;
+	}
+public:
+	browser_test()
+	{
+		// anchor_child_index = 0;
+		adjust_min = true;
+		incremental_build = true;
+		selected_dataset = 0;
+		file_path = "S:/data/EvaluationData";
+		column_selection.resize(6, 0);
+		headers.resize(6);
+		auto_x = auto_y = true;
+		selection_mode = BSM_DIRECTORY;
+		p1.set_extent(cgv::plot::plot_types::P2D(3.0f, 2.0f));
+		set_name("plot_explorer");
+	}
+	/// overload and implement this method to handle events
+	bool handle(event& e)
+	{
+		if (e.get_kind() != cgv::gui::EID_MOUSE)
+			return false;
+		if (e.get_flags() != cgv::gui::EF_DND)
+			return false;
+		
+		cgv::gui::mouse_event& me = reinterpret_cast<cgv::gui::mouse_event&>(e);
+		switch (me.get_action()) {
+		case cgv::gui::MA_DRAG:
+		case cgv::gui::MA_MOVE:
+		case cgv::gui::MA_LEAVE:
+		case cgv::gui::MA_ENTER:
+			return true;
+		case cgv::gui::MA_RELEASE:
+			consider_path_or_file(me.get_dnd_text());
+			return true;
+		}
+		return false;
+	}
+	/// overload to stream help information to the given output stream
+	void stream_help(std::ostream& os)
+	{
+		os << "drag and drop file or directory";
+	}
+
+	void init_frame(cgv::render::context& ctx)
+	{
+		static bool my_tab_selected = false;
+		if (!my_tab_selected) {
+			my_tab_selected = true;
+			cgv::gui::gui_group_ptr gg = ((provider*)this)->get_parent_group();
+			if (gg) {
+				cgv::gui::gui_group_ptr tab_group = gg->get_parent()->cast<cgv::gui::gui_group>();
+				if (tab_group) {
+					cgv::base::base_ptr c = gg;
+					tab_group->select_child(c, true);
+				}
+			}
+		}
+	}
+	void handle_args(std::vector<std::string>& args)
+	{
+		consider_path_or_file(args[0]);
+	}
+
+	void adjust_axes()
+	{
+		if (auto_x) {
+			p1.adjust_domain_axis_to_data(0, adjust_min, true);
+			if (!adjust_min)
+				p1.ref_domain().ref_min_pnt()(0) = 0;
+		}
+		if (auto_y) {
+			p1.adjust_domain_axis_to_data(1, adjust_min, true);
+			if (!adjust_min)
+				p1.ref_domain().ref_min_pnt()(1) = 0;
+		}
+		p1.adjust_tick_marks_to_domain();
+	}
+
+	void construct_plot(dataset* dp)
+	{
+		while (p1.get_nr_sub_plots() > 0)
+			p1.delete_sub_plot(0);
+		for (size_t c = 0; c < dp->data.size(); ++c) {
+			if (column_selection[c] != 0) {
+				unsigned pi = p1.add_sub_plot(dp->headers[c]);
+				p1.ref_sub_plot_samples(pi).resize(dp->data[c].size());
+				for (size_t i = 0; i < dp->data[c].size(); ++i)
+					p1.ref_sub_plot_samples(pi)[i] = cgv::plot::plot1d::P2D((float)(i+1), (float)dp->data[c][i]);
+			}
+		}
+		adjust_axes();
+		post_redraw();
+	}
+	void construct_multi_plot(const std::vector<dataset*> dps)
+	{
+		while (p1.get_nr_sub_plots() > 0)
+			p1.delete_sub_plot(0);
+		for (size_t c = 0; c < dps[0]->data.size(); ++c) {
+			if (column_selection[c] != 0) {
+				unsigned pi = p1.add_sub_plot(dps[0]->headers[c]);
+				p1.ref_sub_plot_strips(pi).resize(dps.size());
+				size_t fst = 0;
+				for (size_t i = 0; i < dps.size(); ++i) {
+					p1.ref_sub_plot_strips(pi)[i] = dps[i]->data[c].size();
+
+					p1.ref_sub_plot_samples(pi).resize(fst + dps[i]->data[c].size());
+					for (size_t j = 0; j < dps[i]->data[c].size(); ++j)
+						p1.ref_sub_plot_samples(pi)[fst+j] = cgv::plot::plot1d::P2D((float)(j+1), (float)dps[i]->data[c][j]);
+
+					fst += dps[i]->data[c].size();
+				}
+			}
+		}
+		adjust_axes();
+		post_redraw();
+	}
+	void update_gui(dataset* dp)
+	{
+		//
+		unsigned nr_cols = dp->headers.size();
+		for (unsigned j = 0; j < 6; ++j) {
+			if (j < nr_cols) {
+				headers[j] = dp->headers[j];
+				if (find_control(column_selection[j])) {
+					find_control(column_selection[j])->set("label", dp->headers[j]);
+					find_control(column_selection[j])->set("active", true);
+				}
+			}
+			else {
+				headers[j] = "";
+				if (find_control(column_selection[j])) {
+					find_control(column_selection[j])->set("label", "");
+					find_control(column_selection[j])->set("active", false);
+				}
+			}
+		}
+		size_t nr_selected = 0;
+		for (unsigned c = 0; c < nr_cols; ++c)
+			if (column_selection[c] != 0)
+				++nr_selected;
+		if (nr_selected == 0) {
+			column_selection[0] = 1;
+			update_member(&column_selection[0]);
+		}
+		if (find_control(selected_dataset))
+			find_control(selected_dataset)->set("max", datasets.size() == 0 ? 0 : datasets.size() - 1);
+
+	}
+
+
+	dataset* read_file(const std::string& path, bool debug = false)
+	{
+		if (debug)
+			std::cout << "reading " << path << std::endl;
+		std::string content;
+		if (!cgv::utils::file::read(path, content, true))
+			return 0;
+		std::vector<line> lines;
+		split_to_lines(content, lines);
+
+		// find first data line
+		size_t i0;
+		size_t nr_cols = 0;
+		for (i0 = 0; i0 < lines.size(); ++i0) {
+			std::vector<token> tokens;
+			split_to_tokens(lines[i0].begin, lines[i0].end, tokens, "");
+			if (tokens.size() > 0) {
+				bool failed = false;
+				for (size_t j = 0; j < tokens.size(); ++j) {
+					double v;
+					if (!is_double(tokens[j].begin, tokens[j].end, v)) {
+						failed = true;
+						break;
+					}
+				}
+				if (!failed) {
+					nr_cols = tokens.size();
+					break;
+				}
+			}
+		}
+		if (nr_cols == 0)
+			return false;
+
+		dataset* dp = new dataset;
+		std::vector<token> tokens;
+		if (i0 > 1)
+			dp->title = to_string(lines[0]);
+		else
+			dp->title = path;
+
+		if (i0 > 0)
+			split_to_tokens(lines[i0-1].begin, lines[i0 - 1].end, tokens, "");
+		
+		dp->headers.resize(nr_cols);
+		size_t c;
+		for (c = 0; c < nr_cols; ++c) {
+			if (c >= tokens.size()) {
+				dp->headers[c] = "column ";
+				dp->headers[c] += to_string(c + 1);
+			}
+			else {
+				dp->headers[c] = to_string(tokens[c]);
+			}
+			if (debug)
+				std::cout << "name of column " << c + 1 << ": " << dp->headers[c] << std::endl;
+		}
+		
+		size_t nr = 0;
+		std::vector<double> v;
+		v.resize(nr_cols);
+		dp->data.clear();
+		dp->data.resize(nr_cols);
+		for (size_t i = i0; i < lines.size(); ++i) {
+			tokens.clear();
+			split_to_tokens(lines[i].begin, lines[i].end, tokens, "");
+			if (tokens.size() != nr_cols)
+				continue;
+			size_t c, nr_success = 0;
+			for (c = 0; c < nr_cols; ++c) {
+				if (is_double(tokens[c].begin, tokens[c].end, v[c])) {
+					++nr_success;
+				}
+			}
+			if (nr_success < nr_cols)
+				continue;
+			++nr;
+			for (c = 0; c < nr_cols; ++c) {
+				dp->data[c].resize(nr);
+				dp->data[c][nr - 1] = v[c];
+			}
+		}
+		if (debug)
+			std::cout << "extracted " << nr_cols << " x " << nr << " data values" << std::endl;
+		return dp;
+	}
+
+	unsigned read_directory(const std::string& path)
+	{
+		vector<string> dirs, files;
+		std::cout << "scanning " << path << std::endl;
+		scan_dir(path, "", dirs, files);
+		unsigned cnt = 0;
+		for (unsigned i = 0; i < files.size(); ++i) {
+			dataset* dp = read_file(path + "/" + files[i]);
+			if (dp) {
+				if (cnt == 0)
+					clear_datasets();
+				++cnt;
+				datasets.push_back(dp);
+			}
+		}
+		return cnt;
+	}
+	std::string get_type_name() const
+	{
+		return "browser_test";
+	}
+	bool self_reflect(reflection_handler& rh)
+	{
+		return rh.reflect_member("file_path", file_path);
+	}
+	void on_set(void* member_ptr)
+	{
+		if (member_ptr == &file_path)
+			recreate_browser_gui();
+		if (member_ptr == &adjust_min || member_ptr == &p1.ref_axis_config(0).log_scale || member_ptr == &p1.ref_axis_config(1).log_scale) {
+			adjust_axes();
+		}
+		if (member_ptr == &selected_dataset) {
+			if (selected_dataset >= 0 && selected_dataset < datasets.size()) {
+				update_gui(datasets[selected_dataset]);
+				construct_plot(datasets[selected_dataset]);
+			}
+		}
+		for (size_t c = 0; c < column_selection.size(); ++c) {
+			if (member_ptr == &column_selection[c]) {
+				construct_plot(datasets[selected_dataset]);
+				post_redraw();
+			}
+		}
+		post_redraw();
+	}
+
+	void adjust_x(button& B)
+	{
+		p1.adjust_domain_axis_to_data(0, adjust_min, true);
+		p1.adjust_tick_marks_to_domain();
+		post_redraw();
+	}
+	void adjust_y(button& B)
+	{
+		p1.adjust_domain_axis_to_data(1, adjust_min, true);
+		p1.adjust_tick_marks_to_domain();
+		post_redraw();
+	}
+	void button_callback(button& B)
+	{
+		std::cout << "clicked button " << B.get_name() << " path = " << file_path_map[&B] << std::endl;
+		gp->unselect_child(base_ptr(&B));
+	}
+
+	void create_gui_base(const std::string& path, gui_group_ptr ggp, bool first_level = true)
+	{
+		vector<string> dirs, files;
+		std::cout << "scanning " << path << std::endl;
+		scan_dir(path, "", dirs, files);
+		unsigned i;
+		for (i = 0; i < dirs.size(); ++i) {
+			gui_group_ptr new_ggp = ggp->add_group(split_path(dirs[i]), "", "", "");
+			new_ggp->set("color", 0xffff88);
+			gui_group& g = *new_ggp;
+			file_path_map[&g] = path + "/" + dirs[i];
+			if (first_level)
+				create_gui_base(path + "/" + dirs[i], new_ggp, false);
+		}
+		for (i = 0; i < files.size(); ++i) {
+			button_ptr bp = ggp->add_button(split_path(files[i]) + "\tfile", "", "");
+			bp->set("color", 0xffcccc);
+			button& b = *bp;
+			file_path_map[&b] = path + "/" + files[i];
+			connect(bp->click, this, &browser_test::button_callback);
+		}
+
+	}
+	void clear_datasets()
+	{
+		for (dataset* dp : datasets)
+			delete dp;
+		datasets.clear();
+	}
+	void select_cb(base_ptr b, bool select)
+	{
+		if (select) {
+			if (b->cast<button>()) {
+				dataset* dp = read_file(file_path_map[&*(b->cast<button>())]);
+				if (dp) {
+					clear_datasets();
+					datasets.push_back(dp);
+					selected_dataset = 0;
+					on_set(&selected_dataset);
+				}
+			}
+			else if (b->cast<gui_group>()) {
+				if (read_directory(file_path_map[&*(b->cast<gui_group>())]) > 0) {
+					if (selected_dataset > datasets.size())
+						selected_dataset = 0;
+					update_gui(datasets[selected_dataset]);
+					construct_multi_plot(datasets);
+				}
+			}
+		}
+	}
+	void remove_closed_children(gui_group_ptr g)
+	{
+		for (unsigned i = 0; i < g->get_nr_children(); ++i) {
+			gui_group_ptr cg = g->get_child(i)->cast<gui_group>();
+			if (cg) {
+				if (gp->is_open_child_group(cg)) {
+					std::cout << "recurse on " << cg->get_name() << std::endl;
+					remove_closed_children(cg);
+				}
+				else {
+					cg->remove_all_children();
+					std::cout << "removing children of " << cg->get_name() << std::endl;
+				}
+			}
+		}
+	}
+	void scan_closed_children(gui_group_ptr g, const std::string& path)
+	{
+		for (unsigned i = 0; i < g->get_nr_children(); ++i) {
+			gui_group_ptr cg = g->get_child(i)->cast<gui_group>();
+			if (cg) {
+				if (gp->is_open_child_group(cg)) {
+					std::cout << "recurse on " << cg->get_name() << std::endl;
+					scan_closed_children(cg, path + cg->get_name() + "/");
+				}
+				else {
+					create_gui_base(path + cg->get_name(), cg, false);
+				}
+			}
+		}
+	}
+	void open_cb(gui_group_ptr g, bool open)
+	{
+		if (open) {
+			std::string path;
+			gui_group_ptr pg = g;
+			while (pg && pg != gp) {
+				path = pg->get_name() + "/" + path;
+				pg = pg->get_parent()->cast<gui_group>();
+			}
+			path = file_path + "/" + path;
+			scan_closed_children(g, path);
+		}
+		else {
+			remove_closed_children(g);
+		}
+	}
+	bool init(cgv::render::context& ctx)
+	{
+		ctx.set_bg_clr_idx(4);
+		get_root()->set("show_stats", false);
+		return p1.init(ctx);
+	}
+	void draw(cgv::render::context& ctx)
+	{
+		glDisable(GL_LIGHTING);
+		p1.draw(ctx);
+		glEnable(GL_LIGHTING);
+	}
+
+	void recreate_browser_gui()
+	{
+		gp->remove_all_children();
+		gp->release_all_managed_objects();
+		create_gui_base(file_path, gp);
+	}
+	void create_browser_gui()
+	{
+		add_decorator("browser", "heading", "level=3;w=100", " ");
+		add_member_control(this, "mode", selection_mode, "dropdown", "enums='file,dir,tree';w=50");
+		add_gui("path", file_path, "directory", "align=' '");
+		file_path_map.clear();
+		gp = add_group("", "tree_group", "w=300;h=400;column_heading_0='path';column_width_0=200;column_heading_1='size';column_width_1=100");
+		gp->set("color", 0xbbbbff);
+		connect(gp->on_selection_change, this, &browser_test::select_cb);
+		connect(gp->on_open_state_change, this, &browser_test::open_cb);
+		create_gui_base(file_path, gp);
+	}
+
+	void create_dynamic_gui()
+	{
+		add_decorator("plot", "heading", "level=3");
+		add_member_control(this, "dataset", selected_dataset, "value_slider", "min=0");
+		find_control(selected_dataset)->set("max", datasets.size() == 0 ? 0 : datasets.size() - 1);
+		for (size_t c = 0; c < 6; ++c) {
+			add_member_control(this, c < headers.size() ? headers[c] : "", (bool&)column_selection[c], "check");
+		}
+		add_member_control(this, "adjust_min", adjust_min, "check");
+		
+		connect(add_button("adjust x", "w=100", " ")->click, this, &browser_test::adjust_x);
+		add_member_control(this, "auto", auto_x, "toggle", "w=70");
+		connect(add_button("adjust y", "w=100", " ")->click, this, &browser_test::adjust_y);
+		add_member_control(this, "auto", auto_y, "toggle", "w=70");
+		p1.create_gui(this, *this);
+	}
+/*
+	std::string get_parent_type() const
+	{
+		return "layout_group";
+	}
+
+	void recreate_gui()
+	{
+		if (!ap) {
+			provider::recreate_gui();
+			return;
+		}
+		if (!parent_group)
+			return;
+		int yscroll = parent_group->get<int>("yscroll");
+		parent_group->remove_all_children();
+		parent_group->release_all_managed_objects();
+		create_dynamic_gui();
+		parent_group->set("dolayout", true);
+		parent_group->set("yscroll", yscroll);
+	}
+	*/
+	void create_gui()
+	{
+		// pp = parent_group;
+		// parent_group->multi_set("layout=table;border_style=framed");
+		// ap = add_group("", "align_group", "w=500;h=400", "cX\n");
+		// parent_group = ap;
+		
+		create_browser_gui();
+
+		// bp = add_group("", "align_group", "w=500;h=400", "cX\n");
+		// parent_group = bp;
+
+		create_dynamic_gui();
+	}
+};
+
 
 #include <cgv/base/register.h>
 
-cgv::base::object_registration<simple_plots> sp_or("");
+cgv::base::object_registration<browser_test> sp_or("");
