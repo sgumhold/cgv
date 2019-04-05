8b92e47872b9d40ac1837e82a8747ebcfbd695bf
gumhold <stefan.gumhold@tu-dresden.de>
2019-04-05 20:24:36 +0200

    removed some shader bugs and improved ui

--- plugins/examples/ping_pong.cxx
+++ plugins/examples/ping_pong.cxx
@@ -298,7 +298,7 @@ public:
 	{
 		glDisable(GL_CULL_FACE);
 		ctx.push_modelview_matrix();
-		ctx.mul_modelview_matrix(cgv::math::scale4<double>(double(w) / h, -1.0, 1.0));
+		ctx.mul_modelview_matrix(cgv::math::scale4<double>(double(w) / h, 1.0, 1.0));
 		prog.enable(ctx);
 			T[3-current_target].enable(ctx, current_target);
 				prog.set_uniform_array(ctx, "HPP_colors", HPP_colors);
