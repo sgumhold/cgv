8294cc12156f9fd0c9e4763e3e603e41fc3bc475
gumhold <stefan.gumhold@tu-dresden.de>
2019-04-05 20:35:47 +0200

    more to be removed

--- bin/remove_generated_headers.bat
+++ bin/remove_generated_headers.bat
@@ -6,4 +6,8 @@ del %CGV_DIR%\cgv\signal\abst_signal.h
 del %CGV_DIR%\cgv\signal\bool_signal.h
 del %CGV_DIR%\cgv\signal\rebind.h
 del %CGV_DIR%\cgv\utils\ostream_printf.h
+del %CGV_DIR%\cgv\base\call_interface_impl.h
+del %CGV_DIR%\cgv\reflect\method_interface_impl.h
+del %CGV_DIR%\cgv\type\traits\function_pointer.h
+
 pause
