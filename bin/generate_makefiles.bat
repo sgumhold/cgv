ad6c8c665031fde5fc46858e0bc364b6444bacbe
gumhold <stefan.gumhold@tu-dresden.de>
2019-09-04 21:46:07 +0200

    made generate makefiles start visual studio also in 64 Bit version and made batch script window disappear automatically

--- bin/generate_makefiles.bat
+++ bin/generate_makefiles.bat
@@ -9,4 +9,9 @@ cd %~p1
 call "%CGV_DIR%\bin\ppp" -script "%CGV_DIR%\make\ppp\templates\make.ppp" "%~f1"
 echo to open solution, press any key (otherwise close window)
 pause
-"%CGV_BUILD%\%CGV_COMPILER%\%~n1\%~n1.sln"
+if "%CGV_PLATFORM%" == "WIN32" (
+   start "Visual Studio" "%CGV_BUILD%\%CGV_COMPILER%\%~n1\%~n1.sln"
+)
+if "%CGV_PLATFORM%" == "x64" (
+   start "Visual Studio" "%CGV_BUILD%\%CGV_COMPILER%_x64\%~n1\%~n1.sln"
+)
