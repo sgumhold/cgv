2a8a41e4fa78a097de5c1108f2444ad736e1525b
gumhold <stefan.gumhold@tu-dresden.de>
2019-11-14 01:30:04 +0100

    improved split seethrough shader in reusable parts and added convenience function to render helpers that sets all uniform variables of reusable seethrough shader parts

--- plugins/crg_vr_view/vr_render_helpers.h
+++ plugins/crg_vr_view/vr_render_helpers.h
@@ -3,6 +3,7 @@
 #include <vr/vr_kit.h>
 #include <vr/vr_state.h>
 #include <cgv/render/render_types.h>
+#include <cgv/render/shader_program.h>
 
 #include "lib_begin.h"
 
@@ -26,6 +27,11 @@ namespace vr {
 	extern CGV_API cgv::render::render_types::mat4 get_camera_projection_transform(const vr_kit* vr_kit_ptr, float z_near, float z_far, int eye, bool undistorted);
 	/// query the texture matrix needed for projective texture mapping for a given camera (0 ... left or mono, 1 ... right only for stereo cameras)
 	extern CGV_API cgv::render::render_types::mat4 get_texture_transform(const vr_kit* vr_kit_ptr, const vr_kit_state& state, float z_near, float z_far, int eye, bool undistorted);
+	//! set all uniforms of seethrough shader program for a given camera (0 ... left or mono, 1 ... right only for stereo cameras). 
+	/*! For this the get_texture_transform is used to compute the texture matrix.
+	    Return whether setting the uniforms was successful.*/
+	extern CGV_API bool configure_seethrough_shader_program(cgv::render::context& ctx, cgv::render::shader_program& prog, 
+		uint32_t frame_width, uint32_t frame_height, const vr_kit* vr_kit_ptr, const vr_kit_state& state, float z_near, float z_far, int eye, bool undistorted);
 }
 
 #include <cgv/config/lib_end.h>
