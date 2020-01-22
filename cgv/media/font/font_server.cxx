20cd02782e8638afb300854edb9bb83c531fd7ce
gumhold <stefan.gumhold@tu-dresden.de>
2020-01-22 23:18:11 +0100

    added support for endian and font analysis

--- cgv/media/font/font_server.cxx
+++ cgv/media/font/font_server.cxx
@@ -1,9 +1,15 @@
 #include <cgv/media/font/font_server.h>
+#include <cgv/utils/endian.h>
+#include <cgv/utils/scan.h>
+#include <cgv/utils/dir.h>
+#include <cgv/utils/file.h>
+#include <stdio.h>
+#include <map>
 
 namespace cgv {
 	namespace media {
 		namespace font {
-			/*
+
 typedef struct _tagTT_OFFSET_TABLE {    
     uint16_t	uMajorVersion;
     uint16_t	uMinorVersion;
@@ -35,19 +41,6 @@ typedef struct _tagTT_NAME_RECORD {
     uint16_t	uStringOffset;	//from start of storage area
 } TT_NAME_RECORD;
 
-#define MAKEWORD(a, b)      ((WORD)(((BYTE)(((DWORD_PTR)(a)) & 0xff)) | ((WORD)((BYTE)(((DWORD_PTR)(b)) & 0xff))) << 8))
-#define MAKELONG(a, b)      ((LONG)(((WORD)(((DWORD_PTR)(a)) & 0xffff)) | ((DWORD)((WORD)(((DWORD_PTR)(b)) & 0xffff))) << 16))
-#define LOWORD(l)           ((WORD)(((DWORD_PTR)(l)) & 0xffff))
-#define HIWORD(l)           ((WORD)((((DWORD_PTR)(l)) >> 16) & 0xffff))
-#define LOBYTE(w)           ((BYTE)(((DWORD_PTR)(w)) & 0xff))
-#define HIBYTE(w)           ((BYTE)((((DWORD_PTR)(w)) >> 8) & 0xff))
-
-#define SWAPWORD(x)		MAKEWORD(HIBYTE(x), LOBYTE(x))
-#define SWAPLONG(x)		MAKELONG(SWAPWORD(HIWORD(x)), SWAPWORD(LOWORD(x)))
-
-#include <string>
-#include <stdio.h>
-
 bool extract_name_and_face(const std::string& file_path, std::string& font_name, std::string& font_face)
 {
 	FILE* fp = fopen(file_path.c_str(), "rb");
@@ -58,9 +51,9 @@ bool extract_name_and_face(const std::string& file_path, std::string& font_name,
 	if (1 != fread(&ttOffsetTable, sizeof(TT_OFFSET_TABLE), 1, fp))
 		return false;
 
-	ttOffsetTable.uNumOfTables  = SWAPWORD(ttOffsetTable.uNumOfTables);
-	ttOffsetTable.uMajorVersion = SWAPWORD(ttOffsetTable.uMajorVersion);
-	ttOffsetTable.uMinorVersion = SWAPWORD(ttOffsetTable.uMinorVersion);
+	cgv::utils::convert_endian_from<cgv::utils::E_BIG_32>(ttOffsetTable.uNumOfTables);
+	cgv::utils::convert_endian_from<cgv::utils::E_BIG_32>(ttOffsetTable.uMajorVersion);
+	cgv::utils::convert_endian_from<cgv::utils::E_BIG_32>(ttOffsetTable.uMinorVersion);
 
 	//check is this is a true type font and the version is 1.0
 	if (ttOffsetTable.uMajorVersion != 1 || ttOffsetTable.uMinorVersion != 0)
@@ -77,9 +70,9 @@ bool extract_name_and_face(const std::string& file_path, std::string& font_name,
 			toupper(tblDir.szTag[2]) == 'M' &&
 			toupper(tblDir.szTag[3]) == 'E')
 		{
-			found = TRUE;
-			tblDir.uLength = SWAPLONG(tblDir.uLength);
-			tblDir.uOffset = SWAPLONG(tblDir.uOffset);
+			found = true;
+			cgv::utils::convert_endian_from<cgv::utils::E_BIG_32>(tblDir.uLength);
+			cgv::utils::convert_endian_from<cgv::utils::E_BIG_32>(tblDir.uOffset);
 			break;
 		}
 	}
@@ -88,25 +81,25 @@ bool extract_name_and_face(const std::string& file_path, std::string& font_name,
 		fseek(fp, tblDir.uOffset, SEEK_SET);
 		TT_NAME_TABLE_HEADER ttNTHeader;
 		fread(&ttNTHeader, sizeof(TT_NAME_TABLE_HEADER), 1, fp);
-		ttNTHeader.uNRCount = SWAPWORD(ttNTHeader.uNRCount);
-		ttNTHeader.uStorageOffset = SWAPWORD(ttNTHeader.uStorageOffset);
+		cgv::utils::convert_endian_from<cgv::utils::E_BIG_32>(ttNTHeader.uNRCount);
+		cgv::utils::convert_endian_from<cgv::utils::E_BIG_32>(ttNTHeader.uStorageOffset);
 		TT_NAME_RECORD ttRecord;
 		found = false;
 		for (int i = 0; i < ttNTHeader.uNRCount; i++) {
 			fread(&ttRecord, sizeof(TT_NAME_RECORD), 1, fp);
-			ttRecord.uNameID = SWAPWORD(ttRecord.uNameID);
+			cgv::utils::convert_endian_from<cgv::utils::E_BIG_32>(ttRecord.uNameID);
 			if (ttRecord.uNameID == 1 || ttRecord.uNameID == 2) {
-				ttRecord.uStringLength = SWAPWORD(ttRecord.uStringLength);
-				ttRecord.uStringOffset = SWAPWORD(ttRecord.uStringOffset);
+				cgv::utils::convert_endian_from<cgv::utils::E_BIG_32>(ttRecord.uStringLength);
+				cgv::utils::convert_endian_from<cgv::utils::E_BIG_32>(ttRecord.uStringOffset);
 				long nPos = ftell(fp);
 				fseek(fp, tblDir.uOffset + ttRecord.uStringOffset + ttNTHeader.uStorageOffset, SEEK_SET);
 				std::string name(ttRecord.uStringLength + 1, char(0));
 				fread(&name[0], 1, ttRecord.uStringLength, fp);
 				if (name.length() > 0) {
 					if (ttRecord.uNameID == 1)
-						res = &name[0];
+						font_name = &name[0];
 					else {
-						sub = &name[0];
+						font_face = &name[0];
 						break;
 					}
 				}
@@ -115,15 +108,69 @@ bool extract_name_and_face(const std::string& file_path, std::string& font_name,
 		}
 	}
 	fclose(fp);
-	res += '|';
-	res += sub;
-	return res;
+	return true;
 }
-*/
+
+struct font_file_info {
+	std::string file_names[4];
+};
+
+typedef std::map<std::string, font_file_info> font_map_type;
+
+font_map_type ref_font_map_type()
+{
+	static font_map_type font_map;
+	return font_map;
+}
+
+void analyze_font_dir(const std::string& path)
+{
+	font_map_type& font_map = ref_font_map_type();
+
+	std::vector<std::string> file_names;
+	if (!cgv::utils::dir::glob(path, file_names, "*.ttf"))
+		return;
+
+	for (const auto& file_name : file_names) {
+		std::string font_name, font_face;
+		if (!extract_name_and_face(file_name, font_name, font_face))
+			continue;
+		if (font_name.empty())
+			continue;
+		// check for supported font faces
+		font_face = cgv::utils::to_upper(font_face);
+		int font_face_idx = -1;
+		if (font_face.empty() || font_face == "REGULAR")
+			font_face_idx = (int)FFA_REGULAR;
+		else if (font_face == "BOLD")
+			font_face_idx = (int)FFA_BOLD;
+		else if (font_face == "ITALIC")
+			font_face_idx = (int)FFA_ITALIC;
+		else if (font_face == "BOLD ITALIC")
+			font_face_idx = (int)FFA_BOLD_ITALIC;
+
+		if (font_face_idx != -1)
+			font_map[font_name].file_names[font_face_idx] = cgv::utils::file::get_file_name(file_name);
+	}
+
+
+	for (auto fi = font_map.begin(); fi != font_map.end(); ++fi) {
+		std::cout << "[";
+		for (int i = 0; i < 4; ++i)
+			std::cout << (fi->second.file_names[i].empty()?' ':'*');
+		std::cout << "] " << fi->first << std::endl;
+	}
+	std::cout << font_map.size() << " fonts." << std::endl;
+}
 
 font_server_ptr& ref_font_server()
 {
 	static font_server_ptr fs;
+	static bool analyzed = false;
+	if (!analyzed) {
+		analyze_font_dir("C:\\Windows\\Fonts");
+		analyzed = true;
+	}
 	return fs;
 }
 
