41e9ee36a4c167b4516ea3893ad17260386ee6aa
gumhold <stefan.gumhold@tu-dresden.de>
2020-01-28 23:17:12 +0100

    added multi context rendering to vr_wall

--- plugins/crg_vr_wall/vr_wall.h
+++ plugins/crg_vr_wall/vr_wall.h
@@ -11,6 +11,8 @@
 #include <cgv/gui/window.h>
 #include <cgv/gui/provider.h>
 #include <cgv_gl/point_renderer.h>
+#include <cgv_gl/box_renderer.h>
+#include <cgv_gl/arrow_renderer.h>
 
 #include "lib_begin.h"
 
@@ -23,7 +25,7 @@ namespace vr {
 	{
 		std::string kit_enum_definition;
 	protected:
-		cgv::render::context* fst_context;
+		cgv::render::context* main_context;
 		cgv::gui::window_ptr window;
 		int vr_wall_kit_index;
 		int vr_wall_hmd_index;
@@ -36,6 +38,51 @@ namespace vr {
 
 		cgv::render::point_render_style prs;
 		cgv::render::point_renderer pr;
+
+
+		cgv::render::arrow_render_style ars;
+		cgv::render::box_render_style brs;
+
+		vec3 test_screen_center;
+		vec3 test_screen_x;
+		vec3 test_screen_y;
+
+		void add_screen_box(const vec3& center, const vec3& x, const vec3& y, const rgb& color)
+		{
+			boxes.push_back(box3(vec3(-x.length(),-y.length(),-0.01f), vec3(x.length(),y.length(),0)));
+			box_colors.push_back(color);
+			box_translations.push_back(center);
+			mat3 R;
+			vec3 x_dir = x; x_dir.normalize();
+			vec3 y_dir = y; y_dir.normalize();
+			vec3 z_dir = cross(x_dir, y_dir);
+			R.set_col(0, x_dir);
+			R.set_col(1, y_dir);
+			R.set_col(2, z_dir);
+			box_rotations.push_back(quat(R));
+		}
+
+		std::vector<box3> boxes;
+		std::vector<rgb> box_colors;
+		std::vector<vec3> box_translations;
+		std::vector<quat> box_rotations;
+
+		void add_arrows(const vec3& center, const vec3& x, const vec3& y, float lum)
+		{
+			arrow_positions.push_back(center);
+			arrow_directions.push_back(x);
+			arrow_colors.push_back(rgb(1, lum, lum));
+			arrow_positions.push_back(center);
+			arrow_directions.push_back(y);
+			arrow_colors.push_back(rgb(lum, 1, lum));
+		}
+
+		std::vector<vec3> arrow_positions;
+		std::vector<vec3> arrow_directions;
+		std::vector<rgb> arrow_colors;
+
+
+
 
 		std::vector<vec3> points;
 		std::vector<rgb> colors[2];
