07bc1b572cc3d46b5fc8d575b960e1b01247b627
sgumhold <stefan.gumhold@tu-dresden.de>
2020-02-17 17:06:49 +0100

    This is a hand made cherry pick command where onlz a selection of files have been picked by hand
    * added first support to read gltf files
    for this the libs/cgv_gl/gl/render_info.h class was added that manages opengl objects and render calls
    the new file libs/cgv_gl/gl/gltf_support.h adds functions to extract a render_info, a bbox or a mesh from a gltf file. Writing to gltf files is not yet implemented
    the libs/cgv_gl/gl/mesh_render_info.h class is now derived from the general render_info class and just adds functionality to deal with simple_meshes. The bind function now takes another boolean whether to enforce binding also in case that not all vertex attributes are supported by the program. Typically, on sets this to true. Also the render_mesh methods is gone and replaces by draw_all, draw_primitive and draw_wireframe. In case of using wireframe one needs to call bind_wireframe before.
    
    All other changes are induced by the ones described above.
    
    One to be mentioned change is that now the autodetect of standard vertex attributes and uniforms of shader_programs is already done after linkage, such that these indices are available also before the first enable command of a shader program

--- cgv/media/mesh/simple_mesh.h
+++ cgv/media/mesh/simple_mesh.h
@@ -2,6 +2,7 @@
 
 #include <vector>
 #include <cgv/math/fvec.h>
+#include <cgv/math/fmat.h>
 #include <cgv/utils/file.h>
 #include <cgv/media/illum/textured_surface_material.h>
 #include <cgv/media/axis_aligned_box.h>
@@ -52,12 +53,30 @@ public:
 	idx_type face_degree(idx_type fi) const { return end_corner(fi) - begin_corner(fi); }
 	/// return number of materials in mesh
 	size_t get_nr_materials() const { return materials.size(); }
-	/// return reference to i-th material
+	/// add a new material and return its index
+	idx_type new_material() { materials.push_back(mat_type()); return idx_type(materials.size() - 1); }
+	/// return const reference to i-th material
 	const mat_type& get_material(size_t i) const { return materials[i]; }
+	/// return reference to i-th material
+	mat_type& ref_material(size_t i) { return materials[i]; }
+	/// return material index of given face
+	const idx_type& material_index(idx_type fi) const { return material_indices[fi]; }
+	/// return reference to material index of given face
+	idx_type& material_index(idx_type fi) { return material_indices[fi]; }
 	/// return number of face groups
 	size_t get_nr_groups() const { return group_names.size(); }
 	/// return the name of the i-th face group
-	const std::string& get_group_name(size_t i) const { return group_names[i]; }
+	const std::string& group_name(size_t i) const { return group_names[i]; }
+	/// set a new group name
+	std::string& group_name(size_t i) { return group_names[i]; }
+	/// add a new group and return its index
+	idx_type new_group(const std::string& name) { group_names.push_back(name); return idx_type(group_names.size() - 1); }
+	/// return group index of given face
+	const idx_type& group_index(idx_type fi) const { return group_indices[fi]; }
+	/// return reference to group index of given face
+	idx_type& group_index(idx_type fi) { return group_indices[fi]; }
+	/// revert face orientation
+	void revert_face_orientation();
 	/// sort faces by group and material indices with two bucket sorts
 	void sort_faces(std::vector<idx_type>& perm, bool by_group = true, bool by_material = true) const;
 	/// merge the three indices into one index into a vector of unique index triples
@@ -80,6 +99,8 @@ public:
 	typedef typename cgv::math::fvec<T, 3> vec3;
 	/// type of 2d vector
 	typedef typename cgv::math::fvec<T, 2> vec2;
+	/// linear transformation 
+	typedef typename cgv::math::fmat<T, 3, 3> mat3;
 	/// color type used in surface materials
 	typedef typename illum::surface_material::color_type clr_type;
 	/// textured surface materials are supported by mat_type
@@ -123,14 +144,20 @@ public:
 	box_type compute_box() const;
 	/// compute vertex normals by averaging triangle normals
 	void compute_vertex_normals();
-	/// read simple mesh from file
+	/// read simple mesh from file (currently only obj is supported)
 	bool read(const std::string& file_name);
+	/// write simple mesh to file (currently only obj is supported)
+	bool write(const std::string& file_name) const;
 	/// extract vertex attribute array, return size of color in bytes
 	unsigned extract_vertex_attribute_buffer(
 		const std::vector<idx_type>& vertex_indices,
 		const std::vector<vec3i>& unique_triples,
 		bool include_tex_coords, bool include_normals, 
 		std::vector<T>& attrib_buffer, bool *include_colors_ptr = 0) const;
+	/// apply transformation to mesh
+	void transform(const mat3& linear_transformation, const vec3& translation);
+	/// apply transformation to mesh with given inverse linear transformation
+	void transform(const mat3& linear_transform, const vec3& translation, const mat3& inverse_linear_transform);
 };
 
 		}
--- libs/cgv_gl/gl/gl_context.cxx
+++ libs/cgv_gl/gl/gl_context.cxx
@@ -2196,7 +2196,7 @@ bool gl_context::shader_program_link(shader_program_base& spb) const
 	int result;
 	glGetProgramiv(p_id, GL_LINK_STATUS, &result); 
 	if (result == 1)
-		return true;
+		return context::shader_program_link(spb);
 	GLint infologLength = 0;
 	glGetProgramiv(p_id, GL_INFO_LOG_LENGTH, &infologLength);
 	if (infologLength > 0) {
@@ -2754,8 +2754,9 @@ bool gl_context::set_element_array(attribute_array_binding_base* aab, const vert
 		return false;
 	}
 	if (vbb->type != VBT_INDICES) {
-		error("gl_context::set_element_array(): called on vertex buffer object that is not of type VBT_INDICES", vbb);
-		return false;
+		std::cout << "gl_context::set_element_array() : called on vertex buffer object that is not of type VBT_INDICES" << std::endl;
+//		error("gl_context::set_element_array(): called on vertex buffer object that is not of type VBT_INDICES", vbb);
+//		return false;
 	}
 	if (aab) {
 		if (!aab->handle) {
