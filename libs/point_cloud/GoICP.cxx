f01319557b1599cffc006f8fa593c774a3b7acc5
NicoKlaus <nico.volkens@mailbox.tu-dresden.de>
2020-04-16 16:03:02 +0200

    additional classes for go-icp

--- libs/point_cloud/GoICP.cxx
+++ libs/point_cloud/GoICP.cxx
@@ -1,9 +1,20 @@
 #include "GoICP.h"
 
-void GoICP::set_source_cloud(point_cloud & inputCloud)
+GoICP::GoICP() 
 {
+	sourceCloud = nullptr;
+	targetCloud = nullptr;
+
+	//settings
+	mse_threshhold = 0.001;
 }
 
-void GoICP::set_target_cloud(const point_cloud & inputCloud)
+void GoICP::build_distance_transform()
 {
+
+}
+
+float GoICP::icp(mat & R_icp, mat & t_icp)
+{
+	return 0.0f;
 }
