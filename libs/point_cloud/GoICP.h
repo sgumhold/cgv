ecd24cd668cf6dba18cef49103eeae559d9efa8f
NicoKlaus <nico.volkens@mailbox.tu-dresden.de>
2020-04-16 16:03:02 +0200

    additional classes for go-icp

--- libs/point_cloud/GoICP.h
+++ libs/point_cloud/GoICP.h
@@ -8,17 +8,59 @@
 
 #include "lib_begin.h"
 
+struct rotation_node
+{
+	float a, b, c, w;
+	float ub, lb;
+	int l;
+	friend bool operator < (const struct rotation_node & n1, const struct rotation_node & n2)
+	{
+		if (n1.lb != n2.lb)
+			return n1.lb > n2.lb;
+		else
+			return n1.w < n2.w;
+	}
+
+};
+
+struct translation_node
+{
+	float x, y, z, w;
+	float ub, lb;
+	friend bool operator < (const translation_node & n1, const translation_node & n2)
+	{
+		if (n1.lb != n2.lb)
+			return n1.lb > n2.lb;
+		else
+			return n1.w < n2.w;
+	}
+};
+
 class CGV_API GoICP : public point_cloud_types{
 	typedef cgv::math::fvec<float, 3> vec3;
+	typedef cgv::math::fmat<float, 4, 4> mat;
 public:
-	point_cloud *sourceCloud;
+	GoICP();
+	void build_distance_transform();
+
+protected:
+	float icp(mat& R_icp, mat& t_icp);
+private:
+	const point_cloud *sourceCloud;
 	const point_cloud *targetCloud;
 
 	Mat rotation;
 	Dir translation;
 
-	void set_source_cloud(point_cloud &inputCloud);
-	void set_target_cloud(const point_cloud &inputCloud);
+	inline void set_source_cloud(const point_cloud &inputCloud) {
+		sourceCloud = &inputCloud;
+	}
+	inline void set_target_cloud(const point_cloud &inputCloud) {
+		targetCloud = &inputCloud;
+	};
+
+	//settings
+	float mse_threshhold;
 };
 
 #include <cgv/config/lib_end.h>
