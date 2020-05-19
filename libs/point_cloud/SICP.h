471a8d7c1c74bc50840f494216bd911a7e620fa6
NicoKlaus <nico.volkens@mailbox.tu-dresden.de>
2020-05-19 12:33:23 +0200

    added shrink operator

--- libs/point_cloud/SICP.h
+++ libs/point_cloud/SICP.h
@@ -14,9 +14,7 @@ namespace cgv {
 
 	namespace pointcloud {
 
-		//Enumerator for setting the underlying ICP method used
-		enum IcpMethod { pointToPoint, pointToPlane };
-
+		// Sparse ICP using Alternate Direction Method of Multipliers
 		class CGV_API SICP : public point_cloud_types {
 		public:
 			struct SICPParameters {
@@ -24,11 +22,11 @@ namespace cgv {
 				int max_inner_loop;
 				int max_outer_loop;
 				float mu;
+				float p;
 			}parameters;
 
 			const point_cloud *sourceCloud;
 			const point_cloud *targetCloud;
-			point_cloud closest_points;
 			ann_tree neighbor_tree;
 
 			SICP();
@@ -36,7 +34,7 @@ namespace cgv {
 			void set_source_cloud(const point_cloud &inputCloud);
 			void set_target_cloud(const point_cloud &inputCloud);
 			void get_center_point(const point_cloud &input, Pnt &center_point);
-			void register_pointcloud(float mu=0.00001f, int max_runs=1000);
+			void register_pointcloud();
 		};
 
 	}
