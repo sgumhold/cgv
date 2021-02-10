ffdd43bc36fb849e71d22bbe5abc712eece27cca
Benjamin Russig <benjamin.russig@tu-dresden.de>
2021-02-10 17:36:47 +0100

    improved regular grid point retrieval

--- plugins/tubes/regulargrid.cxx
+++ plugins/tubes/regulargrid.cxx
@@ -21,6 +21,7 @@ struct grid3D<flt_type>::Impl
 	typename grid3D::point_accessor pnt_access;
 
 	// fields
+	static const typename grid3D<flt_type>::cell n26 [26];
 	real cellwidth;
 	std::unordered_map<
 		typename grid3D::cell, std::vector<size_t>,
@@ -31,6 +32,17 @@ struct grid3D<flt_type>::Impl
 	Impl(real cellwidth, const grid3D::point_accessor &point_accessor)
 		: cellwidth(cellwidth), pnt_access(point_accessor)
 	{}
+};
+template <class flt_type>
+const typename grid3D<flt_type>::cell grid3D<flt_type>::Impl::n26 [26] = {
+	// 1-distance cells (basically, the 3d-cross centered around the middle cell)
+	{-1,  0,  0}, { 1,  0,  0}, { 0,  0, -1}, { 0,  0,  1}, { 0,  1,  0}, { 0, -1,  0},
+	// 2-distance cells
+	{-1,  0, -1}, { 1,  0,  1}, {-1,  0,  1}, { 1,  0, -1}, // xz-slice
+	{-1,  1,  0}, { 1, -1,  0}, { 1,  1,  0}, {-1, -1,  0}, // xy-slice
+	{ 0,  1, -1}, { 0, -1,  1}, { 0,  1,  1}, { 0, -1, -1}, // yz-slice
+	// 3-distance cells (basically, the outer corners)
+	{-1,  1, -1}, { 1, -1,  1}, {-1,  1,  1}, { 1, -1, -1}, {-1, -1, -1}, { 1,  1,  1}, {-1, -1,  1}, { 1,  1, -1}
 };
 
 template <class flt_type>
@@ -65,21 +77,29 @@ template <class flt_type>
 bool grid3D<flt_type>::query (std::vector<size_t> *out, const Vec3 &query_point, bool distance_sort) const
 {
 	// shortcut for saving one indirection
-	auto &impl = *pimpl;
+	const auto &impl = *pimpl;
 
 	// keep track of first new element position in output array
-	size_t sort_start = out->size();
+	const size_t sort_start = out->size();
 
 	// determine query cell
-	cell qc = cell::get(query_point, impl.cellwidth);
+	const cell qc = cell::get(query_point, impl.cellwidth);
 
-	// loop through 27-neighborhood
+	// look inside the query cell
 	bool found = false;
-	for (unsigned i=0; i<27; i++)
 	{
-		cell q_cur{/* x = */ qc.x-1 + long(i%3),
-		           /* y = */ qc.y-1 + long((i/3)%3),
-		           /* z = */ qc.z-1 + long(i/9)};
+		auto c = impl.grid.find(qc);
+		if (c != impl.grid.end())
+		{
+			found = true;
+			out->insert(out->end(), c->second.begin(), c->second.end());
+		}
+	}
+
+	// look in the 26-neighborhood of the query cell
+	cell q_cur; for (unsigned i=0; i<26; i++)
+	{
+		cell::offset(q_cur, qc, Impl::n26[i]);
 		auto c = impl.grid.find(q_cur);
 		if (c != impl.grid.end())
 		{
@@ -88,7 +108,7 @@ bool grid3D<flt_type>::query (std::vector<size_t> *out, const Vec3 &query_point,
 		}
 	}
 
-	// Sort according to distance if requested
+	// do exact distance sort if requested
 	if (distance_sort)
 		std::sort(out->begin()+sort_start, out->end(),
 		[&impl, &query_point] (size_t l, size_t r) {
