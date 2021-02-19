cfd92ccd065825485b11bc23955fb7783c48fef9
sgumhold <stefan.gumhold@tu-dresden.de>
2021-02-19 11:08:08 +0100

    added support for two named color scales added support for depth offset in rectangle rendering

--- cgv/render/color_scale.h
+++ cgv/render/color_scale.h
@@ -8,16 +8,27 @@
 namespace cgv {
 	namespace render {
 
-/// <summary>
-/// convenience function to configure a shader that uses color_scale.glsl
-/// </summary>
-/// <param name="ctx">render context</param>
-/// <param name="prog">shader program using the color scale</param>
-/// <param name="cs">color scale index that can surpass cgv::media::CS_NAMED to index named color scales</param>
-/// <param name="window_zero_position">window_zero_position which is mapped to center of a bipolar color scale</param>
-	extern CGV_API void configure_color_scale(
-								cgv::render::context& ctx, cgv::render::shader_program& prog, 
-								cgv::media::ColorScale cs, float window_zero_position = 0.5f);
+		/// <summary>
+		/// convenience function to configure a shader that uses color_scale.glsl with a single color scale
+		/// </summary>
+		/// <param name="ctx">render context</param>
+		/// <param name="prog">shader program using the color scale</param>
+		/// <param name="cs">color scale index that can surpass cgv::media::CS_NAMED to index named color scales</param>
+		/// <param name="window_zero_position">window_zero_position which is mapped to center of a bipolar color scale</param>
+		extern CGV_API void configure_color_scale(
+			cgv::render::context& ctx, cgv::render::shader_program& prog,
+			cgv::media::ColorScale cs, float window_zero_position = 0.5f);
+
+		/// <summary>
+		/// convenience function to configure a shader that uses color_scale.glsl with two color scales
+		/// </summary>
+		/// <param name="ctx">render context</param>
+		/// <param name="prog">shader program using the color scales</param>
+		/// <param name="cs">color scale indices that can surpass cgv::media::CS_NAMED to index named color scales</param>
+		/// <param name="window_zero_position">window space positions of attribute value zero which is mapped to center of a bipolar color scale</param>
+		extern CGV_API void configure_color_scale(
+			cgv::render::context& ctx, cgv::render::shader_program& prog,
+			cgv::media::ColorScale cs[2], float window_zero_position[2]);
 
 	}
 }
