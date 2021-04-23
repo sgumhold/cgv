3da2a59a7ddb57c4ffe2d7b5f199ad28e373c369
Hendrik Müller <henne90gen@gmail.com>
2021-04-23 17:15:10 +0200

    Created install targets for the rest of the libs

--- cgv-config.cmake
+++ cgv-config.cmake
@@ -3,6 +3,6 @@ get_filename_component(ROOT_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 message(STATUS "Importing CGV: ${ROOT_DIR}")
 
 # NOTE the order of these includes is important!
-include(${ROOT_DIR}/cgv-3rd/cgv-3rd-config.cmake)
+include(${ROOT_DIR}/cgv/cgv-3rd-config.cmake)
 include(${ROOT_DIR}/cgv/cgv-config.cmake)
-include(${ROOT_DIR}/cgv-libs/cgv-libs-config.cmake)
+include(${ROOT_DIR}/cgv/cgv-libs-config.cmake)
