fdc8c22e18f5f404fe1654b81fb6b1903dbbc81f
sgumhold <stefan.gumhold@tu-dresden.de>
2021-06-09 22:18:29 +0200

    updated documentation and removed deprecated tr1 dependencies

--- cgv/type/cond/is_base_of.h
+++ cgv/type/cond/is_base_of.h
@@ -1,9 +1,7 @@
 #pragma once
 
-#if !defined(_MSC_VER) || (_MSC_VER < 1400)
-#include <tr1/type_traits>
+#include <type_traits>
 #include <cgv/type/func/drop_const.h>
-#endif
 
 namespace cgv {
 	namespace type {
@@ -14,12 +12,8 @@ template <typename base_type, typename derived_type>
 struct is_base_of 
 { 
 	static const bool value =  
-#if _MSC_VER >= 1400
-			__is_base_of(base_type,derived_type);
-#else
-		std::tr1::is_base_of<typename func::drop_const<base_type>::type,
+		std::is_base_of<typename func::drop_const<base_type>::type,
 								   typename func::drop_const<derived_type>::type>::value;
-#endif
 };
 
 		}
