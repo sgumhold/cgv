231e3503b7133cdab7ade89b745c5708cd20dedc
Hendrik Müller <henne90gen@gmail.com>
2021-06-28 11:40:43 +0200

    Renamed CGV_LIB_DEST to CGV_BIN_DEST and added run_examples script

---
+++ tool/run_examples.bat
@@ -0,0 +1,20 @@
+REM @ECHO OFF
+
+cgv_viewer^
+ plugin:cg_fltk^
+ "type(shader_config):shader_path='C:\Users\henne\Workspace\cgv\plugins\examples;C:\Users\henne\Workspace\cgv/libs/plot/glsl;C:\Users\henne\Workspace\cgv/libs/cgv_gl/glsl'"^
+ plugin:cg_ext^
+ plugin:cg_icons^
+ plugin:crg_stereo_view^
+ plugin:crg_antialias^
+ plugin:crg_depth_of_field^
+ plugin:crg_light^
+ plugin:cg_meta^
+ plugin:cmi_io^
+ plugin:cmv_avi^
+ plugin:crg_grid^
+ plugin:co_web^
+ plugin:cmf_tt_gl_font^
+ plugin:examples^
+ gui:"C:\Users\henne\Workspace\cgv/plugins/examples/examples.gui"^
+ config:"C:\Users\henne\Workspace\cgv/plugins/examples/config.def"
--- tool/set_plugin_execution_params.cmake
+++ tool/set_plugin_execution_params.cmake
@@ -1,5 +1,5 @@
 
-function(set_ide_params target_name arguments)
+function(set_plugin_execution_params target_name arguments)
     set_target_properties(${target_name} PROPERTIES VS_DEBUGGER_COMMAND $<TARGET_FILE:cgv_viewer>)
     set_target_properties(${target_name} PROPERTIES VS_DEBUGGER_COMMAND_ARGUMENTS "${arguments}")
     set_target_properties(${target_name} PROPERTIES XCODE_SCHEME_EXECUTABLE $<TARGET_FILE:cgv_viewer>)
