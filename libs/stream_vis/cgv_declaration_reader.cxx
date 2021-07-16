8327ff08bcba5898888ad1ef3bd80dbda58c4c26
sgumhold <stefan.gumhold@tu-dresden.de>
2021-07-16 15:35:14 +0200

    added support to store messages

--- libs/stream_vis/cgv_declaration_reader.cxx
+++ libs/stream_vis/cgv_declaration_reader.cxx
@@ -487,7 +487,6 @@ namespace stream_vis {
 				continue;
 			filtered_declarations.push_back(C);
 		}
-		std::cout << "filtered declarations=\n" << filtered_declarations << std::endl;
 		std::vector<cgv::utils::token> toks;
 		cgv::utils::split_to_tokens(filtered_declarations, toks, ":=", false, "\"'[{", "\"']}");
 		size_t ti = 0;
