c3d05c38f16bb6086db2eb910f183c9c9b816ea0
David Groß <davebig@web.de>
2021-09-06 15:36:43 +0200

    added macro for generic render data class

--- libs/cgv_glutil/transfer_function_editor.h
+++ libs/cgv_glutil/transfer_function_editor.h
@@ -2,16 +2,14 @@
 
 #include <cgv/gui/event_handler.h>
 #include <cgv/gui/provider.h>
-#include <cgv/render/attribute_array_binding.h>
 #include <cgv/render/drawable.h>
-#include <cgv/render/shader_program.h>
 #include <cgv/render/texture.h>
-#include <cgv/render/vertex_buffer.h>
-#include <cgv_gl/box_renderer.h>
 #include <cgv_glutil/frame_buffer_container.h>
 #include <cgv_glutil/overlay.h>
 #include <cgv_glutil/shader_library.h>
 
+#include "generic_render_data.h"
+#include "generic_renderer.h"
 #include "transfer_function.h"
 #include "2d/draggables_collection.h"
 
@@ -113,216 +111,12 @@ protected:
 	texture bg_tex;
 	texture tf_tex;
 
-	template<typename PosType, typename ColType>
-	struct plain_geometry {
-		struct vertex_type {
-			PosType pos;
-			ColType col;
-		};
-
-		std::vector<vertex_type> vertices;
-
-		type_descriptor pos_type_descriptor = cgv::render::element_descriptor_traits<PosType>::get_type_descriptor(PosType());
-		type_descriptor col_type_descriptor = cgv::render::element_descriptor_traits<ColType>::get_type_descriptor(ColType());
-
-		vertex_buffer vb;
-		attribute_array_binding aab;
-
-		size_t size() { return vertices.size(); }
-
-		void clear(context& ctx) {
-			vertices.clear();
-
-			if(vb.is_created())
-				vb.destruct(ctx);
-			if(aab.is_created())
-				aab.destruct(ctx);
-		}
-
-		bool create(context& ctx, const shader_program& prog) {
-			bool success = true;
-			success &= vb.create(ctx, &(vertices[0]), vertices.size());
-			success &= aab.create(ctx);
-			success &= aab.set_attribute_array(ctx, prog.get_position_index(), pos_type_descriptor, vb, 0, vertices.size(), sizeof(vertex_type));
-			success &= aab.set_attribute_array(ctx, prog.get_color_index(), col_type_descriptor, vb, sizeof(PosType), vertices.size(), sizeof(vertex_type));
-			return success;
-		}
-
-		void add(const PosType& pos, const ColType& col) {
-			vertices.push_back({ pos, col });
-		}
-
-		void render(context& ctx, PrimitiveType type, shader_program& prog) {
-			render(ctx, type, 0, size(), prog);
-		}
-
-		void render(context& ctx, PrimitiveType type, int offset, size_t count, shader_program& prog) {
-			if(aab.is_created()) {
-				prog.enable(ctx);
-				aab.enable(ctx);
-				GLenum mode = gl::map_to_gl(type);
-				glDrawArrays(mode, (GLint)offset, (GLsizei)count);
-				aab.disable(ctx);
-				prog.disable(ctx);
-			}
-		}
-	};
-
-	class generic_renderer;
-	class generic_geometry {
-		friend class generic_renderer;
-	private:
-		attribute_array_manager aam;
-		bool state_out_of_date = true;
-
-	protected:
-		template <typename T>
-		bool set_attribute_array(const context& ctx, const shader_program& prog, const std::string& name, const T& array) {
-			int loc = prog.get_attribute_location(ctx, name);
-			if(loc > -1)
-				return aam.set_attribute_array(ctx, loc, array);
-			return false;
-		}
-
-		virtual bool transfer(context& ctx, shader_program& prog) = 0;
-
-		bool enable(context& ctx, shader_program& prog) {
-			if(!aam.is_created())
-				aam.init(ctx);
-			if(state_out_of_date)
-				transfer(ctx, prog);
-			state_out_of_date = false;
-			return aam.enable(ctx);
-		}
-
-		bool disable(context& ctx) {
-			return aam.disable(ctx);
-		}
-
-	public:
-		void destruct(const context& ctx) {
-			aam.destruct(ctx);
-		}
-
-		bool init(context& ctx) {
-			return aam.init(ctx);
-		}
-
-		void set_out_of_date() {
-			state_out_of_date = true;
-		}
-
-		virtual size_t size() = 0;
-	};
-
-	class generic_renderer {
-	protected:
-		std::string prog_name = "";
-		shader_program prog;
-		shader_define_map defines, last_defines;
-
-		bool build_shader_program(const context& ctx, const shader_define_map& defines) {
-			if(prog.build_program(ctx, prog_name, true, defines)) {
-				last_defines = defines;
-				return true;
-			}
-			return false;
-		}
-
-	public:
-		generic_renderer() {}
-		generic_renderer(const std::string& prog_name) : prog_name(prog_name) {}
-
-		void destruct(const context& ctx) {
-			prog.destruct(ctx);
-		}
-
-		bool init(context& ctx) {
-			bool success = true;
-			success &= build_shader_program(ctx, defines);
-			return success;
-		}
-
-		shader_program& ref_prog() { return prog; }
-
-		bool enable(context& ctx, generic_geometry& geometry) {
-			if(defines != last_defines) {
-				if(prog.is_created())
-					prog.destruct(ctx);
-				if(!build_shader_program(ctx, defines))
-					return false;
-			}
-			bool res = prog.enable(ctx);
-			res &= geometry.enable(ctx, prog);
-			return res;
-		}
-
-		bool disable(context& ctx, generic_geometry& geometry) {
-			bool res = geometry.disable(ctx);
-			res &= prog.disable(ctx);
-			return res;
-		}
-
-		void draw(context& ctx, PrimitiveType type, size_t start, size_t count) {
-			GLenum pt = gl::map_to_gl(type);
-			glDrawArrays(pt, (GLint)start, (GLsizei)count);
-		}
-
-		bool render(context& ctx, generic_geometry& geometry, PrimitiveType type, size_t start = 0, size_t count = 0) {
-			if(!enable(ctx, geometry))
-				return false;
-			draw(ctx, type, start, count ? count : geometry.size());
-			return disable(ctx, geometry);
-		}
-	};
-
 	generic_renderer line_renderer;
+	generic_renderer polygon_renderer;
 	
-
-
-	class line_geometry : public generic_geometry {
-	protected:
-		bool transfer(context& ctx, shader_program& prog) {
-			bool success = true;
-			success &= set_attribute_array(ctx, prog, "position", positions);
-			success &= set_attribute_array(ctx, prog, "color", colors);
-			return success;
-		}
-	public:
-		std::vector<vec2> positions;
-		std::vector<rgb> colors;
-
-		size_t size() { return positions.size(); };
-
-		void clear() {
-			positions.clear();
-			colors.clear();
-		}
-
-		void add(const vec2& pos, const rgb& col) {
-			positions.push_back(pos);
-			colors.push_back(col);
-		}
-	};
-
-	line_geometry lines;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	DEFINE_GENERIC_RENDER_DATA_CLASS(line_geometry, 2, vec2, position, rgb, color);
+	DEFINE_GENERIC_RENDER_DATA_CLASS(polygon_geometry, 2, vec2, position, rgba, color);
+	
 	struct tf_container {
 		cgv::glutil::draggables_collection<point> points;
 
@@ -332,7 +126,8 @@ protected:
 		texture hist_tex;
 		unsigned hist_max;
 
-		plain_geometry<vec2, rgba> triangles;
+		line_geometry lines;
+		polygon_geometry triangles;
 		
 		void reset() {
 			points.clear();
