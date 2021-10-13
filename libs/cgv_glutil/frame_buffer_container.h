3ecfe1928bf07c52b71104099ade3456c940bc02
David Groß <davebig@web.de>
2021-10-13 15:28:43 +0200

    added get size to frame buffer container

--- libs/cgv_glutil/frame_buffer_container.h
+++ libs/cgv_glutil/frame_buffer_container.h
@@ -29,6 +29,7 @@ protected:
 		bool attach;
 		std::string format;
 		TextureFilter tf;
+		TextureWrap tw;
 		texture texture;
 
 		bool is_depth_attachment() {
@@ -52,6 +53,8 @@ public:
 
 	void clear(context& ctx);
 
+	ivec2 get_size();
+
 	/** Sets the size of the framebuffer renderbuffers.
 		Set the x and/or y-component to <= 0 to use the width/height from the context.
 		Returns false if the size is larger than the maximum allowed render buffer size,
@@ -59,7 +62,7 @@ public:
 	*/
 	bool set_size(const ivec2& size);
 
-	void add_attachment(const std::string& name, const std::string& format = "uint8[R,G,B]", TextureFilter tf = TF_NEAREST, bool attach = true);
+	void add_attachment(const std::string& name, const std::string& format = "uint8[R,G,B]", TextureFilter tf = TF_NEAREST, TextureWrap tw = TW_CLAMP_TO_EDGE, bool attach = true);
 
 	bool enable_attachment(context& ctx, const std::string& name, int tex_unit = -1);
 
