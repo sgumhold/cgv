15cad4998f5134def41e55d486d2812635ef0c8b
David Groß <davebig@web.de>
2022-02-16 15:19:14 +0100

    fixed bug in xml utils

--- cgv/utils/xml.cxx
+++ cgv/utils/xml.cxx
@@ -35,6 +35,9 @@ xml_tag xml_read_tag(const std::string& str)
 	// trim trailing line breaks
 	rtrim(s, "\n");
 
+	if(s.length() < 3)
+		return tag;
+
 	size_t r = 1;
 	size_t l = 1;
 
@@ -55,14 +58,19 @@ xml_tag xml_read_tag(const std::string& str)
 	s = s.substr(r, s.length() - l - 1 - (r - 1));
 
 	std::vector<cgv::utils::token> tokens;
-	split_to_tokens(s, tokens, "", true, "", "");
+	split_to_tokens(s, tokens, "", true, "\"", "\"");
 
 	if(tokens.size() > 0) {
 		tag.name = to_string(tokens[0]);
 
 		if(tag.type != XTT_CLOSE) {
-			for(size_t i = 1; i < tokens.size(); ++i)
-				tag.attributes.emplace(xml_read_attribute(to_string(tokens[i])));
+			for(size_t i = 1; i < tokens.size(); i += 2) {
+				std::string name = to_string(tokens[i]);
+				if(name.back() == '=')
+					name = name.substr(0, name.length() - 1);
+				tag.attributes.emplace(name, to_string(tokens[i + 1]));
+				//tag.attributes.emplace(xml_read_attribute(to_string(tokens[i])));
+			}
 		}
 	} else {
 		tag.type = XTT_UNDEF;
