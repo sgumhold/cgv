4ac5fae4d284b5ef5b3730980d362d7a3fad50e5
sgumhold <stefan.gumhold@tu-dresden.de>
2022-02-22 17:10:01 +0100

    added resource type names

--- cgv/os/resources.cxx
+++ cgv/os/resources.cxx
@@ -4,6 +4,62 @@
 #ifdef WIN32
 #include <Windows.h>
 
+const char* resource_type_name(unsigned i)
+{
+	static const char* resource_type_names[] = {
+	"",
+	"Cursor",
+	"Bitmap",
+	"Icon",
+	"Menu",
+	"Dialog",
+	"String",
+	"FontDir",
+	"Font",
+	"Accelerator",
+	"RawData",
+	"MessageTable",
+	"GroupCoursor",
+	"",
+	"GroupIcon",
+	"",
+	"Version",
+	"DLGInclude",
+	"",
+	"PlugAndPlay",
+	"VXD",
+	"Animated cursor",
+	"AnimatedIcon",
+	"HTML",
+	"Manifest"
+	};
+	return resource_type_names[i];
+}
+enum class RessourceType
+{
+	CURSOR = 1,
+	BITMAP = 2,
+	ICON = 3,
+	MENU = 4,
+	DIALOG = 5,
+	STRING = 6,
+	FONTDIR = 7,
+	FONT = 8,
+	ACCELERATOR = 9,
+	RCDATA = 10,
+	MESSAGETABLE = 11,
+	GROUP_CURSOR = 12,
+	GROUP_ICON = 14,
+	VERSION = 16,
+	DLGINCLUDE = 17,
+	PLUGPLAY = 19,
+	VXD = 20,
+	ANICURSOR = 21,
+	ANIICON = 22,
+	HTML = 23,
+	MANIFEST = 24
+};
+
 BOOL EnumLangsFunc(HMODULE hModule, LPCTSTR lpType, LPCTSTR lpName, WORD wLang, LONG lParam)
 {
 	HRSRC hResInfo;
@@ -25,8 +81,13 @@ BOOL EnumNamesFunc(HMODULE hModule, LPCTSTR lpType, LPTSTR lpName, LONG lParam)
 
 BOOL EnumTypesFunc(HMODULE hModule, LPTSTR lpType, LONG lParam)
 {
-	if (IS_INTRESOURCE(lpType))
-		std::cout << (USHORT)lpType << std::endl;
+	if (IS_INTRESOURCE(lpType)) {
+		USHORT us = (USHORT)lpType;
+		if (us < 25)
+			std::cout << resource_type_name(us) << std::endl;
+		else
+			std::cout << us << std::endl;
+	}
 	else
 		std::cout << lpType << std::endl;
 	EnumResourceNames(hModule, lpType, (ENUMRESNAMEPROC)EnumNamesFunc, 0);
