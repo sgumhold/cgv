a8d005a81f5b2bbc1d49f67df1896109aa1f06b9
Myzios <8514107+Myzios@users.noreply.github.com>
2022-03-13 19:36:54 +0100

    Modify simple_object to use new base class

--- test/vr_lab_test/simple_object.h
+++ test/vr_lab_test/simple_object.h
@@ -1,67 +1,35 @@
 #pragma once
 
-#include <cgv/base/node.h>
-#include <cgv/render/drawable.h>
-#include <cg_nui/focusable.h>
-#include <cg_nui/pointable.h>
-#include <cg_nui/grabable.h>
-#include <cgv/gui/provider.h>
+#include <cg_nui/grabable_interactable.h>
 #include <cgv_gl/box_renderer.h>
-#include <cgv_gl/sphere_renderer.h>
+
 
 class simple_object :
-	public cgv::base::node,
-	public cgv::render::drawable,
-	public cgv::nui::focusable,
-	public cgv::nui::grabable,
-	public cgv::nui::pointable,
-	public cgv::gui::provider
+	public cgv::nui::grabable_interactable
 {
 	cgv::render::box_render_style brs;
-	cgv::render::sphere_render_style srs;
-	vec3 debug_point;
-	vec3 query_point_at_grab, position_at_grab;
-	vec3 hit_point_at_trigger, position_at_trigger;
-	cgv::nui::focus_configuration original_config;
 	static cgv::render::shader_program prog;
-public:
-	// different possible object states
-	enum class state_enum {
-		idle,
-		close,
-		pointed,
-		grabbed,
-		triggered
-	};
+
 protected:
 	// geometry of box with color
-	vec3 position;
+	// position and rotation already defined by base class (grabable_interactable)
 	vec3 extent;
-	quat rotation;
 	rgb  color;
-	// hid with focus on object
-	cgv::nui::hid_identifier hid_id;
-	// state of object
-	state_enum state = state_enum::idle;
 	/// return color modified based on state
 	rgb get_modified_color(const rgb& color) const;
+
 public:
 	simple_object(const std::string& _name, const vec3& _position, const rgb& _color = rgb(0.5f,0.5f,0.5f), const vec3& _extent = vec3(0.3f,0.2f,0.1f), const quat& _rotation = quat(1,0,0,0));
-	std::string get_type_name() const;
-	void on_set(void* member_ptr);
+	std::string get_type_name() const override;
 
-	bool focus_change(cgv::nui::focus_change_action action, cgv::nui::refocus_action rfa, const cgv::nui::focus_demand& demand, const cgv::gui::event& e, const cgv::nui::dispatch_info& dis_info);
-	void stream_help(std::ostream& os);
-	bool handle(const cgv::gui::event& e, const cgv::nui::dispatch_info& dis_info, cgv::nui::focus_request& request);
+	bool compute_closest_point(const vec3& point, vec3& prj_point, vec3& prj_normal, size_t& primitive_idx) override;
+	bool compute_intersection(const vec3& ray_start, const vec3& ray_direction, float& hit_param, vec3& hit_normal, size_t& primitive_idx) override;
 
-	bool compute_closest_point(const vec3& point, vec3& prj_point, vec3& prj_normal, size_t& primitive_idx);
-	bool compute_intersection(const vec3& ray_start, const vec3& ray_direction, float& hit_param, vec3& hit_normal, size_t& primitive_idx);
+	bool init(cgv::render::context& ctx) override;
+	void clear(cgv::render::context& ctx) override;
+	void draw(cgv::render::context& ctx) override;
 
-	bool init(cgv::render::context& ctx);
-	void clear(cgv::render::context& ctx);
-	void draw(cgv::render::context& ctx);
-
-	void create_gui();
+	void create_gui() override;
 };
 
 typedef cgv::data::ref_ptr<simple_object> simple_object_ptr;
