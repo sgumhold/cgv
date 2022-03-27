68d8a83d82c8ed80cc74753b837f87e5249c9c48
Myzios <8514107+Myzios@users.noreply.github.com>
2022-03-27 19:12:15 +0200

    Add HID position and direction to intersection/proximity info

--- libs/cg_nui/hit_info.h
+++ libs/cg_nui/hit_info.h
@@ -10,6 +10,8 @@ namespace cgv {
 			vec3  hit_point;
 			vec3  hit_normal;
 			size_t primitive_index = 0;
+			vec3 hid_position;
+			vec3 hid_direction;
 		};
 
 		struct hit_dispatch_info : public dispatch_info
