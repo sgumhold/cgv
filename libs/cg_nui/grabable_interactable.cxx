a688be63d8b7d2f861bdf6c2d181b6f6f5a3cc32
Myzios <8514107+Myzios@users.noreply.github.com>
2022-03-28 11:50:17 +0200

    Rework interactable using hid transform in intersection info

--- libs/cg_nui/grabable_interactable.cxx
+++ libs/cg_nui/grabable_interactable.cxx
@@ -2,26 +2,24 @@
 
 #include "cgv/math/proximity.h"
 
-void cgv::nui::grabable_interactable::on_grabbed_start(vec3 query_point)
+void cgv::nui::grabable_interactable::on_grabbed_start()
 {
-	query_point_at_grab = query_point;
 	if (position_ptr_ptr != nullptr && *position_ptr_ptr != nullptr)
 		position_at_grab = **position_ptr_ptr;
 	else if (position_ptr != nullptr)
 		position_at_grab = *position_ptr;
 }
 
-void cgv::nui::grabable_interactable::on_grabbed_drag(vec3 query_point)
+void cgv::nui::grabable_interactable::on_grabbed_drag()
 {
 	if (position_ptr_ptr != nullptr && *position_ptr_ptr != nullptr)
-		**position_ptr_ptr = position_at_grab + query_point - query_point_at_grab;
+		**position_ptr_ptr = position_at_grab + ii_during_focus.query_point - ii_at_grab.query_point;
 	else if (position_ptr != nullptr)
-		*position_ptr = position_at_grab + query_point - query_point_at_grab;
+		*position_ptr = position_at_grab + ii_during_focus.query_point - ii_at_grab.query_point;
 }
 
-void cgv::nui::grabable_interactable::on_triggered_start(vec3 hit_point)
+void cgv::nui::grabable_interactable::on_triggered_start()
 {
-	query_point_at_grab = hit_point;
 	if (position_ptr_ptr != nullptr && *position_ptr_ptr != nullptr)
 		position_at_grab = **position_ptr_ptr;
 	else if (position_ptr != nullptr)
@@ -29,38 +27,22 @@ void cgv::nui::grabable_interactable::on_triggered_start(vec3 hit_point)
 
 }
 
-void cgv::nui::grabable_interactable::on_triggered_drag(vec3 ray_origin, vec3 ray_direction, vec3 hit_point)
+void cgv::nui::grabable_interactable::on_triggered_drag()
 {
 	// to be save even without new intersection, find closest point on ray to hit point at trigger
-	vec3 q = cgv::math::closest_point_on_line_to_point(ray_origin, ray_direction, query_point_at_grab);
+	vec3 q = cgv::math::closest_point_on_line_to_point(ii_during_focus.hid_position, ii_during_focus.hid_direction, ii_at_grab.query_point);
 	if (position_ptr_ptr != nullptr && *position_ptr_ptr != nullptr)
-		**position_ptr_ptr = position_at_grab + q - query_point_at_grab;
+		**position_ptr_ptr = position_at_grab + q - ii_at_grab.query_point;
 	else if (position_ptr != nullptr)
-		*position_ptr = position_at_grab + q - query_point_at_grab;
+		*position_ptr = position_at_grab + q - ii_at_grab.query_point;
 }
 
 void cgv::nui::grabable_interactable::draw(cgv::render::context& ctx)
 {
 	interactable::draw(ctx);
-	if (!(state == state_enum::grabbed || state == state_enum::triggered) || !debug_point_enabled)
-		return;
-	auto& sr = cgv::render::ref_sphere_renderer(ctx);
-	sr.set_render_style(debug_sphere_rs);
-	if (state == state_enum::grabbed || state == state_enum::triggered) {
-		sr.set_position(ctx, query_point_at_grab);
-		sr.set_color_array(ctx, &debug_point_color, 1);
-		sr.render(ctx, 0, 1);
-	}
 }
 
 void cgv::nui::grabable_interactable::create_gui()
 {
 	interactable::create_gui();
-	if (begin_tree_node("grabable_interactable", debug_point_enabled)) {
-		align("\a");
-		add_member_control(this, "enable debug point at grab", debug_point_enabled, "check");
-		add_member_control(this, "debug point color", debug_point_color);
-		align("\b");
-		end_tree_node(debug_point_enabled);
-	}
 }
