debe6623ec4aa2dfa45dfa471a0d219097603435
David Groß <davebig@web.de>
2022-04-05 00:28:58 +0200

    updated color map reader and added writer

--- cgv/utils/xml.h
+++ cgv/utils/xml.h
@@ -37,4 +37,51 @@ extern CGV_API xml_tag xml_read_tag(const std::string& str);
 	}
 }
 
+// maybe useful (save parsing of strings to numbers
+/*
+static bool xml_attribute_to_int(const std::string& attribute, int& value) {
+
+	std::string value_str = xml_attribute_value(attribute);
+
+	if(!value_str.empty()) {
+		int value_i = 0;
+
+		try {
+			value_i = stoi(value_str);
+		} catch(const std::invalid_argument&) {
+			return false;
+		} catch(const std::out_of_range&) {
+			return false;
+		}
+
+		value = value_i;
+		return true;
+	}
+
+	return false;
+}
+
+static bool xml_attribute_to_float(const std::string& attribute, float& value) {
+
+	std::string value_str = xml_attribute_value(attribute);
+
+	if(!value_str.empty()) {
+		float value_f = 0.0f;
+
+		try {
+			value_f = stof(value_str);
+		} catch(const std::invalid_argument&) {
+			return false;
+		} catch(const std::out_of_range&) {
+			return false;
+		}
+
+		value = value_f;
+		return true;
+	}
+
+	return false;
+}
+*/
+
 #include <cgv/config/lib_end.h>
