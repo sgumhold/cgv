9f0f8e4292a1ff4918f3219e504ab7bdc26360d9
Benjamin Russig <benjamin.russig@tu-dresden.de>
2022-04-11 14:13:16 +0200

    CMake: add function for adjusting debug/run working directory

--- tool/set_plugin_execution_params.cmake
+++ tool/set_plugin_execution_params.cmake
@@ -5,3 +5,8 @@ function(set_plugin_execution_params target_name arguments)
     set_target_properties(${target_name} PROPERTIES XCODE_SCHEME_EXECUTABLE $<TARGET_FILE:cgv_viewer>)
     set_target_properties(${target_name} PROPERTIES XCODE_SCHEME_ARGUMENTS "${arguments}")
 endfunction()
+
+function(set_plugin_execution_working_dir target_name path)
+    set_target_properties(${target_name} PROPERTIES VS_DEBUGGER_WORKING_DIRECTORY "${path}")
+    set_target_properties(${target_name} PROPERTIES XCODE_SCHEME_WORKING_DIRECTORY "${path}")
+endfunction()
