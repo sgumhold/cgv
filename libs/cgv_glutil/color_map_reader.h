fe27997c6f69ead4e9162644f7351ead6828e218
David Groß <davebig@web.de>
2022-06-02 14:32:58 +0200

    removed unused transfer function class and made color map reader configurable

--- libs/cgv_glutil/color_map_reader.h
+++ libs/cgv_glutil/color_map_reader.h
@@ -8,12 +8,31 @@
 
 #include "color_map.h"
 
-//#include "lib_begin.h"
-
 namespace cgv {
 namespace glutil {
 
 class color_map_reader : cgv::render::render_types {
+public:
+	// configuration for xml tag and attribute name identifiers
+	struct identifier_config {
+		std::string color_map_tag_id = "ColorMap";
+		std::string point_tag_id = "Point";
+		std::string color_point_tag_id = "ColorPoint";
+		std::string opacity_point_tag_id = "OpacityPoint";
+		std::string name_value_id = "name";
+		std::string position_value_id = "x";
+		std::string red_value_id = "r";
+		std::string green_value_id = "g";
+		std::string blue_value_id = "b";
+		std::string opactiy_value_id = "o";
+		bool color_value_type_float = true;
+		bool opacity_value_type_float = true;
+		bool apply_gamma = false;
+	};
+
+	// data structure of loaded color maps result
+	typedef std::vector<std::pair<std::string, color_map>> result;
+
 private:
 	struct point_info {
 		float x = -1.0f;
@@ -23,25 +42,32 @@ private:
 		float b = -1.0f;
 	};
 
-	static point_info read_point(const cgv::utils::xml_tag& tag) {
+	static float parse_value(const std::string& value, bool as_float) {
+		if(as_float)
+			return std::strtof(value.c_str(), nullptr);
+		else
+			return static_cast<float>(std::strtol(value.c_str(), nullptr, 10)) / 255.0f;
+	}
+
+	static point_info read_point(const cgv::utils::xml_tag& tag, identifier_config& config = identifier_config()) {
 		point_info pi;
 		const auto end = tag.attributes.end();
 
-		auto it = tag.attributes.find("x");
-		if(it != end) pi.x = std::strtof((*it).second.c_str(), nullptr);
-		it = tag.attributes.find("o");
-		if(it != end) pi.o = std::strtof((*it).second.c_str(), nullptr);
-		it = tag.attributes.find("r");
-		if(it != end) pi.r = std::strtof((*it).second.c_str(), nullptr);
-		it = tag.attributes.find("g");
-		if(it != end) pi.g = std::strtof((*it).second.c_str(), nullptr);
-		it = tag.attributes.find("b");
-		if(it != end) pi.b = std::strtof((*it).second.c_str(), nullptr);
+		auto it = tag.attributes.find(config.position_value_id);
+		if(it != end) pi.x = parse_value((*it).second, true);
+		it = tag.attributes.find(config.opactiy_value_id);
+		if(it != end) pi.o = parse_value((*it).second, config.opacity_value_type_float);
+		it = tag.attributes.find(config.red_value_id);
+		if(it != end) pi.r = parse_value((*it).second, config.color_value_type_float);
+		it = tag.attributes.find(config.green_value_id);
+		if(it != end) pi.g = parse_value((*it).second, config.color_value_type_float);
+		it = tag.attributes.find(config.blue_value_id);
+		if(it != end) pi.b = parse_value((*it).second, config.color_value_type_float);
 		
 		return pi;
 	}
 
-	static bool parse_xml(const std::vector<cgv::utils::xml_tag>& tags, std::vector<std::string>& names, std::vector<color_map>& color_maps) {
+	static bool parse_xml(const std::vector<cgv::utils::xml_tag>& tags, result& entries, identifier_config& config = identifier_config()) {
 		std::string name = "";
 		color_map cm;
 		bool read_cm = false;
@@ -54,18 +80,16 @@ private:
 
 			if(tag.name == "ColorMaps") {
 				// is probably a color map file
-			} else if(tag.name == "ColorMap") {
+			} else if(tag.name == config.color_map_tag_id) {
 				if(tag.type == cgv::utils::XTT_OPEN) {
 					if(read_cm) {
-						if(!cm.empty()) {
-							names.push_back(name);
-							color_maps.push_back(cm);
-						}
+						if(!cm.empty())
+							entries.push_back({ name, cm });
 						name = "";
 						cm = color_map();
 					}
 					// a new color map
-					auto it = tag.attributes.find("name");
+					auto it = tag.attributes.find(config.name_value_id);
 					if(it != end) {
 						name = (*it).second;
 					}
@@ -73,18 +97,16 @@ private:
 				} else if(tag.type == cgv::utils::XTT_CLOSE) {
 					// a new color map
 					if(read_cm) {
-						if(!cm.empty()) {
-							names.push_back(name);
-							color_maps.push_back(cm);
-						}
+						if(!cm.empty())
+							entries.push_back({ name, cm });
 						name = "";
 						cm = color_map();
 						read_cm = false;
 					}
 				}
-			} else if(tag.name == "Point" || tag.name == "ColorPoint" || tag.name == "OpacityPoint") {
+			} else if(tag.name == config.point_tag_id || tag.name == config.color_point_tag_id || tag.name == config.opacity_point_tag_id) {
 				if(read_cm) {
-					point_info pi = read_point(tag);
+					point_info pi = read_point(tag, config);
 
 					if(!(pi.x < 0.0f)) {
 						if(!(pi.r < 0.0f || pi.g < 0.0f || pi.b < 0.0f)) {
@@ -92,6 +114,12 @@ private:
 							col[0] = cgv::math::clamp(pi.r, 0.0f, 1.0f);
 							col[1] = cgv::math::clamp(pi.g, 0.0f, 1.0f);
 							col[2] = cgv::math::clamp(pi.b, 0.0f, 1.0f);
+							// apply gamma correction if requested
+							if(config.apply_gamma) {
+								col[0] = pow(col[0], 2.2f);
+								col[1] = pow(col[1], 2.2f);
+								col[2] = pow(col[2], 2.2f);
+							}
 							cm.add_color_point(pi.x, col);
 						}
 
@@ -107,22 +135,21 @@ private:
 	}
 
 public:
-	static bool read_from_xml(const std::vector<cgv::utils::xml_tag>& tags, std::vector<std::string>& names, std::vector<color_map>& color_maps) {
+	static bool read_from_xml(const std::vector<cgv::utils::xml_tag>& tags, result& entries, identifier_config& config = identifier_config()) {
 		// clear previous data
-		names.clear();
-		color_maps.clear();
-		return parse_xml(tags, names, color_maps);
+		entries.clear();
+		return parse_xml(tags, entries, config);
 	}
 
-	static bool read_from_xml(const std::vector<std::string>& lines, std::vector<std::string>& names, std::vector<color_map>& color_maps) {
+	static bool read_from_xml(const std::vector<std::string>& lines, result& entries, identifier_config& config = identifier_config()) {
 		std::vector<cgv::utils::xml_tag> tags;
 		for(const auto& line : lines)
 			tags.push_back(cgv::utils::xml_read_tag(line));
 			
-		return read_from_xml(tags, names, color_maps);
+		return read_from_xml(tags, entries, config);
 	}
 
-	static bool read_from_xml(const std::string& file_name, std::vector<std::string>& names, std::vector<color_map>& color_maps) {
+	static bool read_from_xml(const std::string& file_name, result& entries, identifier_config& config = identifier_config()) {
 		if(!cgv::utils::file::exists(file_name) || cgv::utils::to_upper(cgv::utils::file::get_extension(file_name)) != "XML")
 			return false;
 
@@ -151,11 +178,9 @@ public:
 			tags.push_back(cgv::utils::xml_read_tag(line));
 		}
 
-		return read_from_xml(tags, names, color_maps);
+		return read_from_xml(tags, entries, config);
 	}
 };
 
 }
 }
-
-//#include <cgv/config/lib_end.h>
