d9245e8aa458da951abb36b30ea70e09d8283ff3
Myzios <8514107+Myzios@users.noreply.github.com>
2022-07-04 18:39:15 +0200

    Add global transforms to transforming
    The global transform is accumulated over all transforming interfaces in the node hierarchy. Setting the global transform calculates an appropriate local transform to achieve the given global one.

--- libs/cg_nui/transforming.h
+++ libs/cg_nui/transforming.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "focusable.h"
 #include <cgv/render/context.h>
 
 #include "lib_begin.h"
@@ -10,32 +9,50 @@ namespace cgv {
 		/// interface for objects that provides a modelview_projection_window_matrix
 		class CGV_API transforming : public cgv::render::render_types
 		{
+			base::node* _node;
+			bool tried_node_cast{ false };
+			base::node* get_node();
 		protected:
 			/// store model and inverse model matrix
 			mat4 M, iM;
 		public:
 			/// init to identity matrix
 			transforming();
-			/// read access to model transform
+			virtual ~transforming() {}
+			/// read access to model transform (local)
 			const mat4& get_model_transform() const;
-			/// read access to inverse model transform
+			/// read access to inverse model transform (local)
 			const mat4& get_inverse_model_transform() const;
-			/// set model transform and compute inverse model transform
+			/// read access to accumulated model transform (global)
+			const mat4& get_global_model_transform();
+			/// read access to accumulated inverse model transform (global)
+			const mat4& get_global_inverse_model_transform();
+			/// set model transform and compute inverse model transform (local)
 			void set_model_transform(const mat4& _M);
-			/// set model transform and inverse model transform
+			/// set model transform and inverse model transform (local)
 			void set_model_transform(const mat4& _M, const mat4& _iM);
+			/// set model transform and compute inverse model transform (global)
+			///	Computes local transform such that the accumulated global transform matches the given transform
+			void set_global_model_transform(const mat4& _M);
+			///	set model transform and inverse model transform (global)
+			///	Computes local transform such that the accumulated global transform matches the given transform
+			void set_global_model_transform(const mat4& _M, const mat4& _iM);
 			/// transform a point
-			vec3 transform_point(const vec3& p);
+			vec3 transform_point(const vec3& p) const;
 			/// inverse transform a point
-			vec3 inverse_transform_point(const vec3& p);
+			vec3 inverse_transform_point(const vec3& p) const;
 			/// transform a vector
-			vec3 transform_vector(const vec3& v);
+			vec3 transform_vector(const vec3& v) const;
 			/// inverse transform a vector
-			vec3 inverse_transform_vector(const vec3& v);
+			vec3 inverse_transform_vector(const vec3& v) const;
 			/// transform a normal
-			vec3 transform_normal(const vec3& n);
+			vec3 transform_normal(const vec3& n) const;
 			/// inverse transform a normal
-			vec3 inverse_transform_normal(const vec3& n);
+			vec3 inverse_transform_normal(const vec3& n) const;
+			/// Extract the translation, rotation and scale components of the given 4x4 tranformation matrix (assuming no shear, perspective or negative scale)
+			void extract_transform_components(const mat4& transform, vec3& translation, quat& rotation, vec3& scale) const;
+			/// Extract the translation, rotation and scale components of the given 4x4 tranformation matrix (assuming no shear, perspective or negative scale)
+			mat4 construct_transform_from_components(const vec3& translation, const quat& rotation, const vec3& scale) const;
 		};
 
 	}
