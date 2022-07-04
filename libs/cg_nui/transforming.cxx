66685e2c43c1c435eb59c7ed77c6c6cb0d2c5d56
Myzios <8514107+Myzios@users.noreply.github.com>
2022-07-04 18:39:15 +0200

    Add global transforms to transforming
    The global transform is accumulated over all transforming interfaces in the node hierarchy. Setting the global transform calculates an appropriate local transform to achieve the given global one.

--- libs/cg_nui/transforming.cxx
+++ libs/cg_nui/transforming.cxx
@@ -1,8 +1,21 @@
 #include "transforming.h"
 #include <cgv/math/inv.h>
+#include <cgv/base/node.h>
+#include <cgv/math/ftransform.h>
 
 namespace cgv {
 	namespace nui {
+
+		base::node* transforming::get_node()
+		{
+			if (!tried_node_cast) {
+				_node = dynamic_cast<base::node*>(this);
+				tried_node_cast = true;
+				if (!_node)
+					std::cout << "Error: Objects implementing 'transforming' have to inherit 'base::node'" << std::endl;
+			}
+			return _node;
+		}
 
 		transforming::transforming()
 		{
@@ -19,6 +32,47 @@ namespace cgv {
 		{
 			return iM;
 		}
+
+		const render::render_types::mat4& transforming::get_global_model_transform()
+		{
+			mat4 accumulated_transform = get_model_transform();
+			base::node* this_node = get_node();
+			if (!this_node)
+				return accumulated_transform;
+			cgv::base::node_ptr parent = this_node->get_parent();
+			int nr_iters = 0;
+			do {
+				if (!parent)
+					break;
+				auto* transforming_ptr = parent->get_interface<transforming>();
+				if (transforming_ptr) {
+					accumulated_transform = transforming_ptr->get_model_transform() * accumulated_transform;
+				}
+				parent = parent->get_parent();
+			} while (++nr_iters < 100);
+			return accumulated_transform;
+		}
+
+		const render::render_types::mat4& transforming::get_global_inverse_model_transform()
+		{
+			mat4 accumulated_transform = get_inverse_model_transform();
+			base::node* this_node = get_node();
+			if (!this_node)
+				return accumulated_transform;
+			cgv::base::node_ptr parent = this_node->get_parent();
+			int nr_iters = 0;
+			do {
+				if (!parent)
+					break;
+				auto* transforming_ptr = parent->get_interface<transforming>();
+				if (transforming_ptr) {
+					accumulated_transform = transforming_ptr->get_inverse_model_transform() * accumulated_transform;
+				}
+				parent = parent->get_parent();
+			} while (++nr_iters < 100);
+			return accumulated_transform;
+		}
+
 		/// set model transform and compute inverse model transform
 		void transforming::set_model_transform(const mat4& _M)
 		{
@@ -31,36 +85,66 @@ namespace cgv {
 			M = _M;
 			iM = _iM;
 		}
+
+		void transforming::set_global_model_transform(const mat4& _M)
+		{
+			M = get_global_inverse_model_transform() * _M;
+			iM = inv(M);
+		}
+
+		void transforming::set_global_model_transform(const mat4& _M, const mat4& _iM)
+		{
+			M = get_global_inverse_model_transform() * _M;
+			iM = get_global_model_transform() * _iM;
+		}
+
 		/// transform a point
-		transforming::vec3 transforming::transform_point(const vec3& p)
+		transforming::vec3 transforming::transform_point(const vec3& p) const
 		{
 			return M * vec4(p, 1.0f);
 		}
 		/// inverse transform a point
-		transforming::vec3 transforming::inverse_transform_point(const vec3& p)
+		transforming::vec3 transforming::inverse_transform_point(const vec3& p) const
 		{
 			return iM * vec4(p, 1.0f);
 		}
 		/// transform a vector
-		transforming::vec3 transforming::transform_vector(const vec3& v)
+		transforming::vec3 transforming::transform_vector(const vec3& v) const
 		{
 			return M * vec4(v, 0.0f);
 		}
 		/// inverse transform a vector
-		transforming::vec3 transforming::inverse_transform_vector(const vec3& v)
+		transforming::vec3 transforming::inverse_transform_vector(const vec3& v) const
 		{
 			return iM * vec4(v, 0.0f);
 		}
 
 		/// transform a normal
-		transforming::vec3 transforming::transform_normal(const vec3& n)
+		transforming::vec3 transforming::transform_normal(const vec3& n) const
 		{
 			return vec4(n, 0.0f) * iM;
 		}
 		/// inverse transform a normal
-		transforming::vec3 transforming::inverse_transform_normal(const vec3& n)
+		transforming::vec3 transforming::inverse_transform_normal(const vec3& n) const
 		{
 			return vec4(n, 0.0f) * M;
+		}
+
+		void cgv::nui::transforming::extract_transform_components(const mat4& transform, vec3& translation, quat& rotation,
+			vec3& scale) const
+		{
+			// calculation from https://math.stackexchange.com/q/1463487 (lookup 28.06.2022)
+			// assuming no perspective transformation, no shear, no negative scaling factors
+			translation = transform.col(3);
+			scale = vec3(vec3(transform.col(0)).length(), vec3(transform.col(1)).length(), vec3(transform.col(2)).length());
+			rotation = quat(mat3({ normalize(vec3(transform.col(0))), normalize(vec3(transform.col(1))), normalize(vec3(transform.col(2))) }));
+			rotation.normalize();
+		}
+
+		render::render_types::mat4 transforming::construct_transform_from_components(const vec3& translation,
+			const quat& rotation, const vec3& scale) const
+		{
+			return  cgv::math::translate4<float>(translation) * rotation.get_homogeneous_matrix() * cgv::math::scale4<float>(scale);
 		}
 	}
 }
