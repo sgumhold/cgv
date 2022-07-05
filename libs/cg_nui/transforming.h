9ca3bf06eea9e2260cdd14d049faf252928f013a
Myzios <8514107+Myzios@users.noreply.github.com>
2022-07-05 11:51:03 +0200

    Add extension of transforming that handles multiple sub-transforms
    multi_transforming manages transforms of sub-objects (handled through primitive indices) in addition to the main object. It is used to hide the existence of these sub-objects from code expecting only one transform (e.g. posable or gizmos).

--- libs/cg_nui/transforming.h
+++ libs/cg_nui/transforming.h
@@ -20,35 +20,35 @@ namespace cgv {
 			transforming();
 			virtual ~transforming() {}
 			/// read access to model transform (local)
-			const mat4& get_model_transform() const;
+			virtual const mat4& get_model_transform() const;
 			/// read access to inverse model transform (local)
-			const mat4& get_inverse_model_transform() const;
+			virtual const mat4& get_inverse_model_transform() const;
 			/// read access to accumulated model transform (global)
-			const mat4& get_global_model_transform();
+			virtual const mat4& get_global_model_transform();
 			/// read access to accumulated inverse model transform (global)
-			const mat4& get_global_inverse_model_transform();
+			virtual const mat4& get_global_inverse_model_transform();
 			/// set model transform and compute inverse model transform (local)
-			void set_model_transform(const mat4& _M);
+			virtual void set_model_transform(const mat4& _M);
 			/// set model transform and inverse model transform (local)
-			void set_model_transform(const mat4& _M, const mat4& _iM);
+			virtual void set_model_transform(const mat4& _M, const mat4& _iM);
 			/// set model transform and compute inverse model transform (global)
 			///	Computes local transform such that the accumulated global transform matches the given transform
-			void set_global_model_transform(const mat4& _M);
+			virtual void set_global_model_transform(const mat4& _M);
 			///	set model transform and inverse model transform (global)
 			///	Computes local transform such that the accumulated global transform matches the given transform
-			void set_global_model_transform(const mat4& _M, const mat4& _iM);
+			virtual void set_global_model_transform(const mat4& _M, const mat4& _iM);
 			/// transform a point
-			vec3 transform_point(const vec3& p) const;
+			virtual vec3 transform_point(const vec3& p) const;
 			/// inverse transform a point
-			vec3 inverse_transform_point(const vec3& p) const;
+			virtual vec3 inverse_transform_point(const vec3& p) const;
 			/// transform a vector
-			vec3 transform_vector(const vec3& v) const;
+			virtual vec3 transform_vector(const vec3& v) const;
 			/// inverse transform a vector
-			vec3 inverse_transform_vector(const vec3& v) const;
+			virtual vec3 inverse_transform_vector(const vec3& v) const;
 			/// transform a normal
-			vec3 transform_normal(const vec3& n) const;
+			virtual vec3 transform_normal(const vec3& n) const;
 			/// inverse transform a normal
-			vec3 inverse_transform_normal(const vec3& n) const;
+			virtual vec3 inverse_transform_normal(const vec3& n) const;
 			/// Extract the translation, rotation and scale components of the given 4x4 tranformation matrix (assuming no shear, perspective or negative scale)
 			void extract_transform_components(const mat4& transform, vec3& translation, quat& rotation, vec3& scale) const;
 			/// Extract the translation, rotation and scale components of the given 4x4 tranformation matrix (assuming no shear, perspective or negative scale)
