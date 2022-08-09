a1f799ae0068d93e20b944127a02440120075113
David Groß <davebig@web.de>
2022-08-09 14:27:53 +0200

    extended generic render data

--- libs/cgv_glutil/generic_render_data.h
+++ libs/cgv_glutil/generic_render_data.h
@@ -15,6 +15,7 @@
 #define GRD_VAR_TO_STRING(s) GRD_VAR_TO_STRING_(s)
 
 #define GRD_DECL_VEC_MEMBER(t, i) std::vector<t> i;
+#define GRD_DECL_VEC_MEMBER_CONST_REF(t, i) const std::vector<t>& ref_##i() const { return i; }
 #define GRD_CALL_CLEAR_FUNC(t, i) i.clear();
 #define GRD_CALL_SIZE_FUNC(t, i) i.size();
 #define GRD_CALL_PUSH_BACK_FUNC(t, i) this->i.push_back(i);
@@ -54,6 +55,14 @@
 	GRD_APPLY_FUNC1(f, s, t0, i0) s(0)\
 	GRD_APPLY_FUNC7(f, s, t1, i1, t2, i2, t3, i3, t4, i4, t5, i5, t6, i6, t7, i7)
 
+#define GRD_APPLY_FUNC9(f, s, t0, i0, t1, i1, t2, i2, t3, i3, t4, i4, t5, i5, t6, i6, t7, i7, t8, i8)\
+	GRD_APPLY_FUNC1(f, s, t0, i0) s(0)\
+	GRD_APPLY_FUNC8(f, s, t1, i1, t2, i2, t3, i3, t4, i4, t5, i5, t6, i6, t7, i7, t8, i8)
+
+#define GRD_APPLY_FUNC10(f, s, t0, i0, t1, i1, t2, i2, t3, i3, t4, i4, t5, i5, t6, i6, t7, i7, t8, i8, t9, i9)\
+	GRD_APPLY_FUNC1(f, s, t0, i0) s(0)\
+	GRD_APPLY_FUNC9(f, s, t1, i1, t2, i2, t3, i3, t4, i4, t5, i5, t6, i6, t7, i7, t8, i8, t9, i9)
+
 #define GRD_APPLY_FUNC_N(n, f, s, ...) GRD_EXPAND( GRD_APPLY_FUNC##n(f, s, __VA_ARGS__) )
 
 #define GRD_GET_FIRST_PAIR_(f, t, i, ...) f(t, i)
@@ -83,7 +92,7 @@ protected:
 		return false;
 	}
 
-	bool has_indices() {
+	bool has_indices() const {
 		return aam.has_index_buffer();
 	}
 
@@ -125,12 +134,13 @@ public:
 	void add_idx(const unsigned int i) { idx.push_back(i); }
 
 	std::vector<unsigned>& ref_idx() { return idx; }
+	const std::vector<unsigned>& ref_idx() const { return idx; }
 
 	void set_out_of_date() {
 		state_out_of_date = true;
 	}
 
-	virtual size_t get_render_count() = 0;
+	virtual size_t get_render_count() const = 0;
 };
 
 }
@@ -174,7 +184,7 @@ public:
 		std::vector<vec2> positions;
 		std::vector<rgb> colors;
 
-		size_t get_render_count() {
+		size_t get_render_count() const {
 			if(idx.empty() return positions.size();
 			else return idx.size();
 		};
@@ -205,7 +215,8 @@ protected:\
 	}\
 public:\
 	GRD_APPLY_FUNC_N(attrib_count, GRD_DECL_VEC_MEMBER, GRD_SEP_NULL, __VA_ARGS__)\
-	size_t get_render_count() {\
+	GRD_APPLY_FUNC_N(attrib_count, GRD_DECL_VEC_MEMBER_CONST_REF, GRD_SEP_NULL, __VA_ARGS__)\
+	size_t get_render_count() const {\
 		if(idx.empty()) return GRD_GET_FIRST_PAIR(GRD_CALL_SIZE_FUNC, __VA_ARGS__)\
 		else return idx.size();\
 	}\
