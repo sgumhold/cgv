dbc7a5625a36ee77da555a2737270affb702e635
Myzios <8514107+Myzios@users.noreply.github.com>
2022-09-05 18:09:40 +0200

    Add reusable handle highlight logic for gizmos

--- libs/cg_nui/reusable_gizmo_functionalities.h
+++ libs/cg_nui/reusable_gizmo_functionalities.h
@@ -44,6 +44,44 @@ public:
 	void set_axes_scale_independent_positions(std::vector<vec3> scale_independent_axis_positions);
 };
 
+class CGV_API gizmo_functionality_handle_states
+{
+protected:
+	// internal state
+	std::vector<rgb> handle_colors;
+	std::vector<bool> handles_highlighted;
+	std::vector<bool> handles_grabbed;
+
+	// configuration
+	std::vector<rgb> handle_base_colors{ rgb(0.11f, 0.35f, 0.62f) };
+	std::vector<rgb> handle_highlight_colors{ rgb(0.0f, 0.47f, 1.0f) };
+	std::vector<rgb> handle_grab_colors{ rgb(0.0f, 0.47f, 1.0f) };
+
+	/// Validate the configuration of the axes. Has to be called during validation at attach. Requires the number of handles that are supposed to exist.
+	bool validate_handles(int handle_count);
+
+	void grab_handle(int handle_idx);
+	void release_handle(int handle_idx);
+	void release_handles();
+	void highlight_handle(int handle_idx);
+	void dehighlight_handle(int handle_idx);
+	void dehighlight_handles();
+public:
+	// Configuration functions
+	// Set one base color to be used by all handles
+	void set_handle_base_color(rgb color) { handle_base_colors = { color }; }
+	// Set different base colors for each handle. If less colors then handles are given then the last color will be repeated.
+	void set_handle_base_colors(std::vector<rgb> colors) { handle_base_colors = colors; }
+	// Set one highlight color to be used by all handles
+	void set_handle_highlight_color(rgb color) { handle_highlight_colors = { color }; }
+	// Set different highlight colors for each handle. If less colors then handles are given then the last color will be repeated.
+	void set_handle_highlight_colors(std::vector<rgb> colors) { handle_highlight_colors = colors; }
+	// Set one grab color to be used by all handles
+	void set_handle_grab_color(rgb color) { handle_highlight_colors = { color }; }
+	// Set different grab colors for each handle. If less colors then handles are given then the last color will be repeated.
+	void set_handle_grab_colors(std::vector<rgb> colors) { handle_highlight_colors = colors; }
+};
+
 	}
 }
 
