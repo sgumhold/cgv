4923fbc0f4fd2a4eb4d6fb53a0ec2f606a7b886a
Mario Henze <mario.henze.94@gmail.com>
2022-09-15 18:59:53 +0200

    Begin spatial audio example

--- plugins/cg_audio/cg_audio_interactor.cxx
+++ plugins/cg_audio/cg_audio_interactor.cxx
@@ -1,13 +1,25 @@
 #include "cg_audio_interactor.h"
 
-//#include <3rd/>
+#include <algorithm>
+
+#include <libs/cgv_oal/al_context.h>
 
 AudioInteractor::AudioInteractor()
 {
 	
 }
 
-void AudioInteractor::create_gui()
-{
-	//if (begin_tree_node("Hardware configuration",))
+void AudioInteractor::create_menu() {
+	const auto device_name_strings = cgv::audio::OALContext::enumerate_devices();
+	std::transform(std::cbegin(device_name_strings), std::cend(device_name_strings), std::back_inserter(menu_btn_ptrs),
+				   [this](const std::string& dev_name) {
+					   constexpr auto menu_prefix = "Devices/";
+					   return add_menu_button(menu_prefix + dev_name);
+		});
+}
+
+void AudioInteractor::destroy_menu() {
+	for (const auto& btn : menu_btn_ptrs) {
+		remove_menu_element(btn);
+	}
 }
--- plugins/cg_audio/cg_audio_interactor.h
+++ plugins/cg_audio/cg_audio_interactor.h
@@ -1,15 +1,19 @@
+#include <list>
+
 #include <cgv/base/base.h>
 #include <cgv/gui/event_handler.h>
-#include <cgv/gui/provider.h>
+#include <cgv/gui/menu_provider.h>
 
 class AudioInteractor : 
 	public cgv::base::node,
 	public cgv::gui::event_handler,
-	public cgv::gui::provider
+	public cgv::gui::menu_provider
 {
   public:
 	AudioInteractor();
-	virtual void create_gui() override;
+	virtual void create_menu() override;
+	virtual void destroy_menu() override;
 
   private:
+	std::list<cgv::gui::button_ptr> menu_btn_ptrs;
 };
