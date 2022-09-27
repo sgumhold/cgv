f25bd54642ee4219434e75558d6f8b23103dc01b
David Groß <davebig@web.de>
2022-09-27 13:58:26 +0200

    changed glutil shader include for static builds

--- libs/cgv_glutil/shader_inc.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-/*! file use this header to include the shader files from cgv_glutil in static builds. */
-
-#ifdef REGISTER_SHADER_FILES
-#include <cgv/base/register.h>
-#include <cgv_glutil_shader_inc.h>
-#endif
