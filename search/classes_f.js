17ebcec5a60ebeb8b14df1bc85cb4ff45ece2271
1DaveBig <davebig@web.de>
2022-10-13 14:36:01 +0000

    Deploying to gh-pages from @ sgumhold/cgv@b5c33c9d51a6353a11196fd4f91dfd47f9dc2614 🚀

--- search/classes_f.js
+++ search/classes_f.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['qem_3810',['qem',['../classcgv_1_1math_1_1qem.html',1,'cgv::math']]],
-  ['quaternion_3811',['quaternion',['../classcgv_1_1math_1_1quaternion.html',1,'cgv::math']]],
-  ['quaternion_3c_20float_20_3e_3812',['quaternion&lt; float &gt;',['../classcgv_1_1math_1_1quaternion.html',1,'cgv::math']]]
+  ['qem_3811',['qem',['../classcgv_1_1math_1_1qem.html',1,'cgv::math']]],
+  ['quaternion_3812',['quaternion',['../classcgv_1_1math_1_1quaternion.html',1,'cgv::math']]],
+  ['quaternion_3c_20float_20_3e_3813',['quaternion&lt; float &gt;',['../classcgv_1_1math_1_1quaternion.html',1,'cgv::math']]]
 ];
--- search/groups_1.js
+++ search/groups_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['virtual_20reality_7295',['Virtual Reality',['../group__VR.html',1,'']]]
+  ['virtual_20reality_7297',['Virtual Reality',['../group__VR.html',1,'']]]
 ];
