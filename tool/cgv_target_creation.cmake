sgumhold/cgv

commit d8c7e4d0cfee8a013907a02f95c24ac0051a770a

Fix non-Makefile generators not working with computed command line

tool/cgv_target_creation.cmake
@@ -165,7 +165,7 @@ function(cgv_do_deferred_ops TARGET_NAME)
 		if (GUI_PROVIDER_PLUGIN)
 			set(CMD_LINE_ARGS "plugin:${GUI_PROVIDER_PLUGIN}")
 			if (SHADER_PATHS)
-				set(CMD_LINE_ARGS "${CMD_LINE_ARGS} \"type(shader_config):shader_path='${SHADER_PATHS}'\"")
+				set(CMD_LINE_ARGS "${CMD_LINE_ARGS} type(shader_config):shader_path=\"${SHADER_PATHS}\"")
 			endif()
 		endif()
 		# --- append remaining plugins
@@ -198,7 +198,7 @@ function(cgv_do_deferred_ops TARGET_NAME)
 		)
 	else()
 		# try to set relevant options for all known generators in the hopes of ending up with a valid launch/debug configuration
-		set_plugin_execution_params(${NAME} "${CMD_LINE_ARGS}")
+		set_plugin_execution_params(${TARGET_NAME} "${CMD_LINE_ARGS}")
 	endif()
 endfunction()
 

commit 4a63f92953ad710ff0999be85d3a136ba67d81a3

Fix regression on Visual Studio generators introduced in d8c7e4d0cfee8a013907a02f95c24ac0051a770a

tool/cgv_target_creation.cmake
@@ -165,7 +165,7 @@ function(cgv_do_deferred_ops TARGET_NAME)
 		if (GUI_PROVIDER_PLUGIN)
 			set(CMD_LINE_ARGS "plugin:${GUI_PROVIDER_PLUGIN}")
 			if (SHADER_PATHS)
-				set(CMD_LINE_ARGS "${CMD_LINE_ARGS} type(shader_config):shader_path=\"${SHADER_PATHS}\"")
+				set(CMD_LINE_ARGS "${CMD_LINE_ARGS} \"type(shader_config):shader_path=\'${SHADER_PATHS}'\"")
 			endif()
 		endif()
 		# --- append remaining plugins