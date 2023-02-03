140574aeff7c039db6ae045408a3e0d94f451bb7
Benjamin Russig <benjamin.russig@tu-dresden.de>
2023-02-03 21:50:21 +0100

    Prevent issues with _USE_MATH_DEFINES with Clang on Linux

--- cgv/math/fvec.h
+++ cgv/math/fvec.h
@@ -1,6 +1,10 @@
 #pragma once
 
-#define _USE_MATH_DEFINES
+// Make sure this is the first thing the compiler sees, while preventing warnings if
+// it happened to already be defined by something else including this header
+#ifndef _USE_MATH_DEFINES
+	#define _USE_MATH_DEFINES 1
+#endif
 #include <array>
 #include <limits>
 #include <algorithm>
