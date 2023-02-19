sgumhold/cgv

commit 526c9a54f3689976ff2bad09ccaaf87a97a1715b

enabled usage of shader file cache through context flag; enabled usage of shader includes

cgv/render/shader_code.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <cgv/render/context.h>
+#include <set>
 
 #include "lib_begin.h"
 
@@ -89,7 +90,7 @@ public:
 
 protected:
 	/// map used to cache shader file contents indexed by their file name
-	static std::map<std::string, std::string> shader_code_cache;
+	static std::map<std::string, std::string> code_cache;
 	/// store the shader type
 	ShaderType st;
 
@@ -111,14 +112,18 @@ public:
 	static std::string get_last_error(const std::string& file_name, const std::string& last_error);
 	/// read shader code from file and return string with content or empty string if read failed
 	static std::string read_code_file(const std::string &file_name, std::string* _last_error = 0);
+	/// retreive shader code either by reading the file from disk or from the cache if enabled
+	static std::string retrieve_code(const std::string& file_name, bool use_cache, std::string* _last_error);
 	/** detect the shader type from the extension of the given
-		 file_name, i.e.		 
+		 file_name, i.e.
 		 - glvs ... ST_VERTEX
 		 - glgs ... ST_GEOMETRY
 		 - glfs ... ST_FRAGMENT
 		 - glcs ... ST_COMPUTE
 	*/
 	static ShaderType detect_shader_type(const std::string& file_name);
+	/// search for include directives in the given source code, replace them by the included file contents and return the full source code
+	static std::string resolve_includes(const std::string& source, bool use_cache, std::set<std::string>& included_file_names = std::set<std::string>(), std::string* _last_error = 0);
 	/// destruct shader code
 	void destruct(const context& ctx);
 	/** read shader code from file that is searched for with find_file.

cgv/render/context.cxx
@@ -162,6 +162,7 @@ context::context()
 	do_screen_shot = false;
 	light_source_handle = 1;
 
+	use_shader_file_cache = true;
 	auto_set_view_in_current_shader_program = true;
 	auto_set_gamma_in_current_shader_program = true;
 	auto_set_lights_in_current_shader_program = true;
@@ -400,6 +401,24 @@ shader_program_base* context::get_current_program() const
 	}
 	shader_program_base& prog = *shader_program_stack.top();
 	return &prog;
+}
+
+/// enable the usage of the shader file caches
+void context::enable_shader_file_cache()
+{
+	use_shader_file_cache = true;
+}
+
+/// disable the usage of the shader file caches
+void context::disable_shader_file_cache()
+{
+	use_shader_file_cache = false;
+}
+
+/// whether the shader file caches are enabled
+bool context::is_shader_file_cache_enabled() const
+{
+	return use_shader_file_cache;
 }
 
 /// return the number of light sources

cgv/render/context.h
@@ -582,6 +582,8 @@ public:
 protected:
 	friend class shader_program_base;
 
+	/// whether to use the caching facilities of shader_program and shader_code to store loaded shader file contents as strings for faster loading
+	bool use_shader_file_cache;
 	/// whether to automatically set viewing matrixes in current shader program, defaults to true 
 	bool auto_set_view_in_current_shader_program;
 	/// whether to automatically set lights in current shader program, defaults to true 
@@ -615,6 +617,12 @@ protected:
 public:
 	/// check for current program, prepare it for rendering and return pointer to it
 	shader_program_base* get_current_program() const;
+	/// enable the usage of the shader file caches
+	void enable_shader_file_cache();
+	/// disable the usage of the shader file caches
+	void disable_shader_file_cache();
+	/// whether the shader file caches are enabled
+	bool is_shader_file_cache_enabled() const;
 protected:
 	/// stack of currently enabled attribute array binding
 	std::stack<attribute_array_binding_base*> attribute_array_binding_stack;

cgv/render/shader_code.cxx
@@ -20,8 +20,7 @@ using namespace cgv::base;
 namespace cgv {
 	namespace render {
 
-static bool use_cache = false;
-std::map<std::string, std::string> shader_code::shader_code_cache;
+std::map<std::string, std::string> shader_code::code_cache;
 
 shader_config::shader_config()
 {
@@ -221,6 +220,26 @@ std::string shader_code::find_file(const std::string& file_name)
 	return find_in_paths(file_name, get_shader_config()->shader_path, true);
 }
 
+std::string shader_code::retrieve_code(const std::string& file_name, bool use_cache, std::string* _last_error) {
+
+	std::string source = "";
+
+	if(use_cache) {
+		auto it = code_cache.find(file_name);
+		if(it != code_cache.end()) {
+			source = it->second;
+		}
+	}
+
+	if(source.empty())
+		source = read_code_file(file_name, _last_error);
+
+	if(use_cache)
+		code_cache.emplace(file_name, source);
+
+	return source;
+}
+
 ShaderType shader_code::detect_shader_type(const std::string& file_name)
 {
 	std::string ext = to_lower(get_extension(file_name));
@@ -236,6 +255,52 @@ ShaderType shader_code::detect_shader_type(const std::string& file_name)
 	else if (ext == "glte" || ext == "pglte")
 		st = ST_TESS_EVALUATION;
 	return st;
+}
+
+std::string shader_code::resolve_includes(const std::string& source, bool use_cache, std::set<std::string>& included_file_names, std::string* _last_error)
+{
+	const std::string identifier = "#include ";
+
+	std::string resolved_source = "";
+
+	std::vector<cgv::utils::line> lines;
+	cgv::utils::split_to_lines(source, lines);
+
+	for(size_t i = 0; i < lines.size(); ++i) {
+		std::string line = cgv::utils::to_string(lines[i]);
+
+		// search for the include identifier
+		size_t identifier_pos = line.find(identifier);
+		if(identifier_pos != std::string::npos) {
+			// remove identifier and all content before; an include directive must be the first and only statement on a line
+			line.erase(0, identifier_pos + identifier.length());
+			
+			// trim whitespace
+			cgv::utils::trim(line);
+
+			if(line.length() > 0) {
+				// remove quotation marks, leaving only the include path
+				std::string include_file_name = line.substr(1, line.length() - 2);
+				std::string include_source = retrieve_code(include_file_name, use_cache, _last_error);
+
+				// check whether this file was already included and skip if this is the case
+				if(included_file_names.find(include_file_name) == included_file_names.end()) {
+					included_file_names.insert(include_file_name);
+					line = resolve_includes(include_source, use_cache, included_file_names, _last_error);
+				} else {
+					line = "";
+				}
+			}
+
+			// skip this line if nothing needs to be included (removes the include statement from the code)
+			if(line == "")
+				continue;
+		}
+
+		resolved_source += line + '\n';
+	}
+
+	return resolved_source;
 }
 
 /// return the shader type of this code
@@ -293,31 +358,20 @@ bool shader_code::read_code(const context& ctx, const std::string &file_name, Sh
 	if (st == ST_DETECT)
 		st = detect_shader_type(file_name);
 
-	std::string source = "";
+	// get source code from cache or read file
+	std::string source = retrieve_code(file_name, ctx.is_shader_file_cache_enabled(), &last_error);
 
-	if(use_cache) {
-		auto it = shader_code_cache.find(file_name);
+	source = resolve_includes(source, ctx.is_shader_file_cache_enabled());
 
-		if(it != shader_code_cache.end()) {
-			source = it->second;
-		} else {
-			source = read_code_file(file_name, &last_error);
-			shader_code_cache.emplace(file_name, source);
-		}
-	} else {
-		source = read_code_file(file_name, &last_error);
-	}
-
-	if(!defines.empty()) {
+	if(!defines.empty())
 		set_defines(source, defines);
-	}
 	
-	if (st == ST_VERTEX && ctx.get_gpu_vendor_id() == GPUVendorID::GPU_VENDOR_AMD) {
+	if (st == ST_VERTEX && ctx.get_gpu_vendor_id() == GPUVendorID::GPU_VENDOR_AMD)
 		set_vertex_attrib_locations(source);
-	}
 
 	if (source.empty())
 		return false;
+
 	return set_code(ctx, source, st);
 }
 

cgv/render/shader_program.cxx
@@ -12,7 +12,6 @@ using namespace cgv::utils;
 namespace cgv {
 	namespace render {
 
-static bool use_cache = false;
 std::map<std::string, std::string> shader_program::program_file_cache;
 std::map<std::string, std::vector<std::string>> shader_program::files_cache;
 
@@ -28,17 +27,38 @@ bool shader_program::attach_files(const context& ctx, const std::vector<std::str
 	return no_error;
 }
 
-bool shader_program::collect_file(const std::string& file_name, std::vector<std::string>& file_names)
+bool shader_program::collect_files_from_cache(const std::string& name, std::vector<std::string>& file_names, bool& added_files) {
+
+	auto it = files_cache.find(name);
+	if(it != files_cache.end()) {
+		const std::vector<std::string>& cached_file_names = it->second;
+		for(size_t i = 0; i < cached_file_names.size(); ++i)
+			file_names.push_back(cached_file_names[i]);
+
+		 added_files = !cached_file_names.empty();
+		 return true;
+	}
+
+	added_files = false;
+	return false;
+}
+
+bool shader_program::collect_file(const std::string& file_name, bool use_cache, std::vector<std::string>& file_names)
 {
 	if(use_cache) {
-		auto it = files_cache.find(file_name);
-		if(it != files_cache.end()) {
-			const std::vector<std::string>& cached_file_names = it->second;
-			for(size_t i = 0; i < cached_file_names.size(); ++i)
-				file_names.push_back(cached_file_names[i]);
 
-			return !cached_file_names.empty();
-		}
+		bool added_files = false;
+		if(collect_files_from_cache(file_name, file_names, added_files))
+			return added_files;
+
+		//auto it = files_cache.find(file_name);
+		//if(it != files_cache.end()) {
+		//	const std::vector<std::string>& cached_file_names = it->second;
+		//	for(size_t i = 0; i < cached_file_names.size(); ++i)
+		//		file_names.push_back(cached_file_names[i]);
+		//
+		//	return !cached_file_names.empty();
+		//}
 	}
 
 	std::vector<std::string> collected_file_names;
@@ -48,30 +68,33 @@ bool shader_program::collect_file(const std::string& file_name, std::vector<std:
 
 	if (!fn.empty()) {
 		file_names.push_back(fn);
-		if(use_cache) {
+		if(use_cache)
 			collected_file_names.push_back(fn);
-		}
 		found = true;
 	}
 
-	if(use_cache) {
+	if(use_cache)
 		files_cache.emplace(file_name, collected_file_names);
-	}
 
 	return found;
 }
 
-bool shader_program::collect_files(const std::string& base_name, std::vector<std::string>& file_names)
+bool shader_program::collect_files(const std::string& base_name, bool use_cache, std::vector<std::string>& file_names)
 {
 	if(use_cache) {
-		auto it = files_cache.find(base_name);
-		if(it != files_cache.end()) {
-			const std::vector<std::string>& cached_file_names = it->second;
-			for(size_t i = 0; i < cached_file_names.size(); ++i)
-				file_names.push_back(cached_file_names[i]);
 
-			return !cached_file_names.empty();
-		}
+		bool added_files = false;
+		if(collect_files_from_cache(base_name, file_names, added_files))
+			return added_files;
+
+		//auto it = files_cache.find(base_name);
+		//if(it != files_cache.end()) {
+		//	const std::vector<std::string>& cached_file_names = it->second;
+		//	for(size_t i = 0; i < cached_file_names.size(); ++i)
+		//		file_names.push_back(cached_file_names[i]);
+		//
+		//	return !cached_file_names.empty();
+		//}
 	}
 
 	std::vector<std::string> collected_file_names;
@@ -84,9 +107,8 @@ bool shader_program::collect_files(const std::string& base_name, std::vector<std
 		if (!fn.empty()) {
 			file_names.push_back(fn);
 			
-			if(use_cache) {
+			if(use_cache)
 				collected_file_names.push_back(fn);
-			}
 
 			added_file = true;
 		}
@@ -95,9 +117,8 @@ bool shader_program::collect_files(const std::string& base_name, std::vector<std
 	if(!added_file)
 		std::cerr << "could not find shader file " << base_name.c_str() << std::endl;
 
-	if(use_cache) {
+	if(use_cache)
 		files_cache.emplace(base_name, collected_file_names);
-	}
 
 	return added_file;
 }
@@ -124,7 +145,7 @@ bool shader_program::collect_dir(const std::string& dir_name, bool recursive, st
 	return true;
 }
 
-bool shader_program::collect_program(const std::string& file_name, std::vector<std::string>& file_names)
+bool shader_program::collect_program(const std::string& file_name, bool use_cache, std::vector<std::string>& file_names)
 {
 	std::string fn = shader_code::find_file(file_name);
 	if (fn.empty())
@@ -146,25 +167,25 @@ bool shader_program::collect_program(const std::string& file_name, std::vector<s
 	for (auto line : lines) {
 		std::string l = to_string((const token&)line);
 		if (l.substr(0,5) == "file:")
-			added_file = collect_file(l.substr(5), file_names) || added_file;
+			added_file = collect_file(l.substr(5), use_cache, file_names) || added_file;
 		else if (l.substr(0,12) == "vertex_file:")
-			added_file = collect_file(l.substr(12), file_names) || added_file;
+			added_file = collect_file(l.substr(12), use_cache, file_names) || added_file;
 		else if (l.substr(0, 14) == "geometry_file:")
-			added_file = collect_file(l.substr(14), file_names) || added_file;
+			added_file = collect_file(l.substr(14), use_cache, file_names) || added_file;
 		else if (l.substr(0, 26) == "tessellation_control_file:")
-			added_file = collect_file(l.substr(26), file_names) || added_file;
+			added_file = collect_file(l.substr(26), use_cache, file_names) || added_file;
 		else if (l.substr(0, 29) == "tessellation_evaluation_file:")
-			added_file = collect_file(l.substr(29), file_names) || added_file;
+			added_file = collect_file(l.substr(29), use_cache, file_names) || added_file;
 		else if (l.substr(0,14) == "fragment_file:")
-			added_file = collect_file(l.substr(14), file_names) || added_file;
+			added_file = collect_file(l.substr(14), use_cache, file_names) || added_file;
 		else if (l.substr(0,6) == "files:")
-			added_file = collect_files(l.substr(6), file_names) || added_file;
+			added_file = collect_files(l.substr(6), use_cache, file_names) || added_file;
 		else if (l.substr(0,4) == "dir:")
 			added_file = collect_dir(l.substr(4), false, file_names) || added_file;
 		else if (l.substr(0,8) == "rec_dir:")
 			added_file = collect_dir(l.substr(8), true, file_names) || added_file;
 		else if (l.substr(0,8) == "program:")
-			added_file = collect_program(l.substr(8), file_names) || added_file;
+			added_file = collect_program(l.substr(8), use_cache, file_names) || added_file;
 	}
 
 	get_shader_config()->shader_path = old_shader_path;
@@ -270,7 +291,7 @@ bool shader_program::attach_file(const context& ctx, const std::string& file_nam
 /// read shader code from files with the given base name, compile and attach them
 bool shader_program::attach_files(const context& ctx, const std::string& base_name, const shader_define_map& defines) {
 	std::vector<std::string> file_names;
-	if(!collect_files(base_name, file_names))
+	if(!collect_files(base_name, ctx.is_shader_file_cache_enabled(), file_names))
 		return false;
 	return attach_files(ctx, file_names, defines);
 }
@@ -281,7 +302,7 @@ bool shader_program::attach_dir(const context& ctx, const std::string& dir_name,
 		return false;
 	return attach_files(ctx, file_names);
 }
-bool shader_program::open_program_file(std::string& file_name, std::string& content, std::vector<line>& lines, std::string* last_error_ptr)
+bool shader_program::open_program_file(std::string& file_name, bool use_cache, std::string& content, std::vector<line>& lines, std::string* last_error_ptr)
 {
 	std::string fn = "";
 
@@ -289,13 +310,14 @@ bool shader_program::open_program_file(std::string& file_name, std::string& cont
 		auto it = program_file_cache.find(file_name);
 		if(it != program_file_cache.end()) {	
 			fn = it->second;
-		} else {
-			fn = shader_code::find_file(file_name);
-			program_file_cache.emplace(file_name, fn);
 		}
-	} else {
-		fn = shader_code::find_file(file_name);
 	}
+
+	if(fn.empty())
+		fn = shader_code::find_file(file_name);
+
+	if(use_cache)
+		program_file_cache.emplace(file_name, fn);
 
 	if (fn.empty()) {
 		if (last_error_ptr)
@@ -319,7 +341,7 @@ std::vector<shader_define_map> shader_program::extract_instances(std::string fil
 	std::string content;
 	std::vector<line> lines;
 	std::vector<shader_define_map> result;
-	if (!open_program_file(file_name, content, lines))
+	if (!open_program_file(file_name, false, content, lines))
 		return result;
 	for (unsigned int i = 0; i < lines.size(); ++i) {
 		token tok = lines[i];
@@ -358,7 +380,7 @@ bool shader_program::attach_program(const context& ctx, std::string file_name, b
 {
 	std::string content;
 	std::vector<line> lines;
-	if (!open_program_file(file_name, content, lines, &last_error)) {
+	if (!open_program_file(file_name, ctx.is_shader_file_cache_enabled(), content, lines, &last_error)) {
 		std::cerr << last_error << std::endl;
 		return false;
 	}

cgv/render/shader_program.h
@@ -42,13 +42,19 @@ protected:
 	/// ensure that the state has been set in the context
 	void update_state(const context& ctx);
 	/// common code necessary to open file
-	static bool open_program_file(std::string& file_name, std::string& content, std::vector<cgv::utils::line>& lines, std::string* last_error_ptr = 0);
+	static bool open_program_file(std::string& file_name, bool use_cache, std::string& content, std::vector<cgv::utils::line>& lines, std::string* last_error_ptr = 0);
 public:
+	
+	
 	/// resolve file name with shader_code::find_file and add file to list if found
-	static bool collect_file(const std::string& file_name, std::vector<std::string>& file_names);
+	static bool collect_files_from_cache(const std::string& name, std::vector<std::string>& file_names, bool& added_files);
+
+
+	/// resolve file name with shader_code::find_file and add file to list if found
+	static bool collect_file(const std::string& file_name, bool use_cache, std::vector<std::string>& file_names);
 	/** collect shader code files that extent the given base name. 
 	    Returns true if at least one shader code file has been collected.*/
-	static bool collect_files(const std::string& base_name, std::vector<std::string>& file_names);
+	static bool collect_files(const std::string& base_name, bool use_cache, std::vector<std::string>& file_names);
 	/** collect shader code files from directory. If the directory does not exist
 		 search it in the shader path of the shader configuration returned
 		 by get_shader_config(). Returns true if at least one shader code 
@@ -72,7 +78,7 @@ public:
 					  triangles,triangles_adjacency,triangle_strip,triangle_strip_adjacency,triangle_fan,
 					  quads,quad_strip,polygon.
 		 Returns true if at least one shader code file has been collected.*/
-	static bool collect_program(const std::string& file_name, std::vector<std::string>& file_names);
+	static bool collect_program(const std::string& file_name, bool use_cache, std::vector<std::string>& file_names);
 	/// return the maximum number of output vertices of a geometry shader
 	static unsigned int get_max_nr_geometry_shader_output_vertices(const context& ctx);
 	/** create empty shader program and set the option whether errors during 

commit 87ca90b033ef8ca2556c0e5818b009508560856b

Fix GCC/Clang compilation errors introduced in 526c9a54f3689976ff2bad09ccaaf87a97a1715b

cgv/render/shader_code.h
@@ -122,8 +122,13 @@ public:
 		 - glcs ... ST_COMPUTE
 	*/
 	static ShaderType detect_shader_type(const std::string& file_name);
+	/// search for include directives in the given source code, replace them by the included file contents and return the full source code as well as the set of all included files
+	static std::string resolve_includes(const std::string& source, bool use_cache, std::set<std::string>& included_file_names, std::string* _last_error = 0);
 	/// search for include directives in the given source code, replace them by the included file contents and return the full source code
-	static std::string resolve_includes(const std::string& source, bool use_cache, std::set<std::string>& included_file_names = std::set<std::string>(), std::string* _last_error = 0);
+	inline static std::string resolve_includes(const std::string& source, bool use_cache, std::string* _last_error = 0) {
+		std::set<std::string> dummy;
+		return resolve_includes(source, use_cache, dummy, _last_error);
+	}
 	/// destruct shader code
 	void destruct(const context& ctx);
 	/** read shader code from file that is searched for with find_file.