sgumhold/cgv

commit f8bee18c03bc930c3da59f48e6429ec91ddc8ac7

Fix stopwatch incorrectly measuring process CPU time on *nix instead of actual elapsed time

cgv/utils/stopwatch.cxx
@@ -14,22 +14,24 @@ namespace cgv {
 
 #ifdef _WIN32
 static long long frequency;
-static BOOL perfcounter_available;
 static bool queried_perfcounter = false;
+#else
+constexpr auto S_TO_NS=1000000000LL;
+constexpr auto NS_TO_S=(1.0/double(S_TO_NS));
 #endif
 
 void stopwatch::init()
 {
 #ifdef _WIN32
 	if (!queried_perfcounter) {
-		perfcounter_available = QueryPerformanceFrequency((LARGE_INTEGER*) &frequency);
+		QueryPerformanceFrequency((LARGE_INTEGER*) &frequency);
 		queried_perfcounter = true;
 	}
-	if (perfcounter_available)
-		QueryPerformanceCounter((LARGE_INTEGER*) &start);
-	else
-#endif			
-		(std::clock_t&) start = std::clock();
+	QueryPerformanceCounter((LARGE_INTEGER*) &start);
+#endif
+	timespec ts;
+	clock_gettime(CLOCK_MONOTONIC, &ts);
+	start = ((long long)ts.tv_sec)*S_TO_NS + (long long)ts.tv_nsec;
 }
 
 //standard constructor starts time measurement
@@ -50,21 +52,15 @@ double stopwatch::get_current_time(long long& end) const
 {
 	double time;
 #ifdef _WIN32
-	if(perfcounter_available)
-	{
-		QueryPerformanceCounter((LARGE_INTEGER*) &end);
-		time =(end-start)/ (double)frequency;
-	}
-	else
-	{
+	QueryPerformanceCounter((LARGE_INTEGER*) &end);
+	time =(end-start)/(double)frequency;
+#else
+	timespec ts;
+	clock_gettime(CLOCK_MONOTONIC, &ts);
+	end = ((long long)ts.tv_sec)*S_TO_NS + (long long)ts.tv_nsec;
+	time = double(end - start)*NS_TO_S;
 #endif
-		end = clock();
-		std::clock_t total = (std::clock_t)(end - start); //get elapsed time
-		time =  double(total)/CLOCKS_PER_SEC;
-#ifdef _WIN32
-	}
-#endif
-	return time;	
+	return time;
 }
 
 /// return time elpased thus far

cgv/utils/stopwatch.h
@@ -10,8 +10,6 @@ namespace cgv {
 * On Win32 the perfomance counter with a resolution of 0,313 microseconds is used, if available.
 * If the performance counter is not available and on other systems the std::clock() function with 
 * a resolution of 0,055 milliseconds is used.
-*
-* TODO: Better implementation for Linux (with higher resolution)
 * 
 * Example 1:
 *

commit 49bc428899dc5a3f67ebed839a9d3af2a88349cc

Fix compilation error on Windows caused by f8bee18c03bc930c3da59f48e6429ec91ddc8ac7

cgv/utils/stopwatch.cxx
@@ -28,10 +28,11 @@ void stopwatch::init()
 		queried_perfcounter = true;
 	}
 	QueryPerformanceCounter((LARGE_INTEGER*) &start);
-#endif
+#else
 	timespec ts;
 	clock_gettime(CLOCK_MONOTONIC, &ts);
 	start = ((long long)ts.tv_sec)*S_TO_NS + (long long)ts.tv_nsec;
+#endif
 }
 
 //standard constructor starts time measurement