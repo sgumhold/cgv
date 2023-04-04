6f026dba234cf5cee7d9cdea496314232fd198c5
David Groß <davebig@web.de>
2023-04-04 15:31:12 +0200

    refactored holographic raycast example and added matrix interpolation

--- plugins/examples/holo_raycast.cpp
+++ plugins/examples/holo_raycast.cpp
@@ -16,7 +16,10 @@ class holo_raycast :
 	public cgv::gui::provider,
 	public cgv::gui::event_handler {
 protected:
-	cgv::render::shader_library shaders;
+	cgv::render::view* view_ptr = nullptr;
+	cgv::render::stereo_view* stereo_view_ptr = nullptr;
+
+	cgv::render::shader_program raycast_prog;
 	
 	struct Calibration {
 		float pitch;
@@ -24,12 +27,13 @@ protected:
 		float center;
 	} calibration;
 
-	int scene_index = 1;
-
+	bool interpolate_view_matrix = true;
 	float eye_separation_factor = 1.0f;
 	vec3 light_direction;
 	bool enable_shadows = true;
-	vec4 test = vec4(0.0f);
+
+	bool debug_matrices = false;
+	float view_test = 0.0f;
 
 public:
 	holo_raycast() : cgv::base::node("Holographic Raycast Demo")
@@ -234,18 +238,6 @@ public:
 					on_set(&eye_separation_factor);
 					return true;
 				}
-				/*case '.': if (ka != cgv::gui::KeyAction::KA_RELEASE) {
-					test.y() = std::min(1.f, test.y()+0.0625f);
-					std::cout << "frustum_shear_factor: "<<test.y() << std::endl;
-					on_set(&test.y());
-					return true;
-				}
-				case ',': if (ka != cgv::gui::KeyAction::KA_RELEASE) {
-					test.y() = std::max(0.f, test.y()-0.0625f);
-					std::cout << "frustum_shear_factor: "<<test.y() << std::endl;
-					on_set(&test.y());
-					return true;
-				}*/
 				default:
 					/* DoNothing() */;
 			}
@@ -259,91 +251,209 @@ public:
 	}
 
 	void clear (cgv::render::context &ctx) {
-		shaders.clear(ctx);
+		raycast_prog.destruct(ctx);
 	}
 
 	bool init (cgv::render::context &ctx) {
-		shaders.add("raycast_scene0", "holo_raycast.glpr");
-		shaders.add("raycast_scene1", "holo_raycast2.glpr");
-
-		return shaders.load_all(ctx, "holo_raycast::init()");
+		return cgv::render::shader_library::load(ctx, raycast_prog, "holo_raycast.glpr", false, "holo_raycast::init()");
 	}
 
-	mat4 compute_stereo_frustum_screen(float eye, float eye_separation, vec2 screen_dim, vec2 z_planes, float parallax_zero_depth) {
-
-		float aspect = screen_dim.x() / screen_dim.y();
-		float top = 0.5*screen_dim.y()*z_planes.x() / parallax_zero_depth;
-		float bottom = -top;
-		float delta = 0.5*eye_separation*eye*screen_dim.x()*z_planes.x() / parallax_zero_depth;
-		float left = bottom * aspect - delta;
-		float right = top * aspect - delta;
-
-		return cgv::math::frustum4(left, right, bottom, top, z_planes.x(), z_planes.y());
-	}
-
-	void stereo_translate_modelview_matrix(float eye, float eye_separation, float screen_width, mat4& M) {
-		M(3, 0) += -0.5 * eye_separation * eye * screen_width;
+	void init_frame(cgv::render::context &ctx) {
+		if(!view_ptr) {
+			view_ptr = find_view_as_node();
+			stereo_view_ptr = reinterpret_cast<cgv::render::stereo_view*>(view_ptr);
+		}
 	}
 
 	void draw (cgv::render::context &ctx)
 	{
-		cgv::render::shader_program& raycast_prog = shaders.get(scene_index == 0 ? "raycast_scene0" : "raycast_scene1");
+		if(!stereo_view_ptr)
+			return;
+
+		if(debug_matrices)
+			test_matrix_interpolation(ctx, stereo_view_ptr);
+
 		raycast_prog.enable(ctx);
+		// set Looking Glass calibration parameters
 		raycast_prog.set_uniform(ctx, "pitch", calibration.pitch);
 		raycast_prog.set_uniform(ctx, "slope", calibration.slope);
 		raycast_prog.set_uniform(ctx, "center", calibration.center);
 
 		vec2 viewport_dims = vec2(static_cast<float>(ctx.get_width()), static_cast<float>(ctx.get_height()));
 
-		cgv::render::view* view_ptr = find_view_as_node();
-		cgv::render::stereo_view*  sview_ptr = reinterpret_cast<cgv::render::stereo_view*>(view_ptr);
-
 		vec3 eye_pos = view_ptr->get_eye();
 
 		const ivec4& current_vp = ctx.get_window_transformation_array().front().viewport;
 		float aspect = static_cast<float>(current_vp[2]) / static_cast<float>(current_vp[3]);
-		float y_extent_at_focus = static_cast<float>(sview_ptr->get_y_extent_at_focus());
-		float eye_separation = static_cast<float>(sview_ptr->get_eye_distance());
-		float parallax_zero_depth = static_cast<float>(sview_ptr->get_parallax_zero_depth());
-		float z_near = static_cast<float>(sview_ptr->get_z_near());
-		float z_far = static_cast<float>(sview_ptr->get_z_far());
-
+		float y_extent_at_focus = static_cast<float>(stereo_view_ptr->get_y_extent_at_focus());
+		float eye_separation = static_cast<float>(stereo_view_ptr->get_eye_distance());
+		float parallax_zero_depth = static_cast<float>(stereo_view_ptr->get_parallax_zero_depth());
+		float z_near = static_cast<float>(stereo_view_ptr->get_z_near());
+		float z_far = static_cast<float>(stereo_view_ptr->get_z_far());
 
 		float screen_width = y_extent_at_focus * aspect;
-		mat4 P0 = compute_stereo_frustum_screen(-1.0f, eye_separation, vec2(y_extent_at_focus * aspect, y_extent_at_focus), vec2(z_near, z_far), parallax_zero_depth);
-		mat4 P1 = compute_stereo_frustum_screen(1.0f, eye_separation, vec2(y_extent_at_focus * aspect, y_extent_at_focus), vec2(z_near, z_far), parallax_zero_depth);
-		
-		mat4 MV0 = ctx.get_modelview_matrix();
-		mat4 MV1 = ctx.get_modelview_matrix();
-		stereo_translate_modelview_matrix(-1.0f, eye_separation, screen_width, MV0);
-		stereo_translate_modelview_matrix(1.0f, eye_separation, screen_width, MV1);
 
-		mat4 MVP0 = P0 * MV0;
-		mat4 MVP1 = P1 * MV1;
+		eye_separation *= eye_separation_factor;
 
-
+		// general parameters needed to perform raycasting
 		raycast_prog.set_uniform(ctx, "viewport_dims", viewport_dims);
+		// variables needed for constructing matrices
 		raycast_prog.set_uniform(ctx, "eye_pos", eye_pos);
-		raycast_prog.set_uniform(ctx, "eye_separation", eye_separation_factor * eye_separation);
-		raycast_prog.set_uniform(ctx, "screen_width", y_extent_at_focus * aspect);
+		raycast_prog.set_uniform(ctx, "eye_separation", eye_separation);
+		raycast_prog.set_uniform(ctx, "screen_width", screen_width);
 		raycast_prog.set_uniform(ctx, "screen_height", y_extent_at_focus);
 		raycast_prog.set_uniform(ctx, "parallax_zero_depth", parallax_zero_depth);
 		raycast_prog.set_uniform(ctx, "z_near", z_near);
 		raycast_prog.set_uniform(ctx, "z_far", z_far);
+		
+		// projection matrices for left and right view
+		mat4 P_left = cgv::math::stereo_frustum_screen4(-1.0f, eye_separation, y_extent_at_focus * aspect, y_extent_at_focus, parallax_zero_depth, z_near, z_far);
+		mat4 P_right = cgv::math::stereo_frustum_screen4(1.0f, eye_separation, y_extent_at_focus * aspect, y_extent_at_focus, parallax_zero_depth, z_near, z_far);
 
+		// modelview matrices for left and right view
+		mat4 MV_left = ctx.get_modelview_matrix();
+		mat4 MV_right = MV_left;
+		stereo_translate_modelview_matrix(-1.0f, eye_separation, screen_width, MV_left);
+		stereo_translate_modelview_matrix(1.0f, eye_separation, screen_width, MV_right);
+
+		// get world-space eye position from inverse modelview matrix per view
+		vec3 eye_left = vec3(inv(MV_left).col(3));
+		vec3 eye_right = vec3(inv(MV_right).col(3));
+
+		// compute inverse modelview projection matrices for left and right view
+		mat4 iMVP_left = inv(P_left * MV_left);
+		mat4 iMVP_right = inv(P_right * MV_right);
+		// acquire view-dependent varying elements (last two columns)
+		mat4 stereo_view_params;
+		stereo_view_params.set_col(0, iMVP_left.col(2));
+		stereo_view_params.set_col(1, iMVP_right.col(2));
+		stereo_view_params.set_col(2, iMVP_left.col(3));
+		stereo_view_params.set_col(3, iMVP_right.col(3));
+
+		// variables needed for interpolating left and right view matrices
+		raycast_prog.set_uniform(ctx, "stereo_view_params", stereo_view_params);
+		raycast_prog.set_uniform(ctx, "eye_pos_left", eye_left);
+		raycast_prog.set_uniform(ctx, "eye_pos_right", eye_right);
+
+		// general appearance and test parameters
 		raycast_prog.set_uniform(ctx, "light_direction", light_direction);
 		raycast_prog.set_uniform(ctx, "enable_shadows", enable_shadows);
-		raycast_prog.set_uniform(ctx, "test", test);
+		raycast_prog.set_uniform(ctx, "interpolate_view_matrix", interpolate_view_matrix);
 
 		glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 		raycast_prog.disable(ctx);
 	}
 
+	void stereo_translate_modelview_matrix(float eye, float eye_separation, float screen_width, mat4& M) {
+		M(0, 3) += -0.5 * eye_separation * eye * screen_width;
+	}
+
+	void test_matrix_interpolation(cgv::render::context& ctx, cgv::render::stereo_view* sview_ptr) {
+
+		const ivec4& current_vp = ctx.get_window_transformation_array().front().viewport;
+		float aspect = static_cast<float>(current_vp[2]) / static_cast<float>(current_vp[3]);
+		float y_extent_at_focus = static_cast<float>(stereo_view_ptr->get_y_extent_at_focus());
+		float eye_separation = static_cast<float>(stereo_view_ptr->get_eye_distance());
+		float parallax_zero_depth = static_cast<float>(stereo_view_ptr->get_parallax_zero_depth());
+		float z_near = static_cast<float>(stereo_view_ptr->get_z_near());
+		float z_far = static_cast<float>(stereo_view_ptr->get_z_far());
+
+		float screen_width = y_extent_at_focus * aspect;
+
+		eye_separation *= eye_separation_factor;
+
+		mat4 P0 = cgv::math::stereo_frustum_screen4(-1.0f, eye_separation, y_extent_at_focus * aspect, y_extent_at_focus, parallax_zero_depth, z_near, z_far);
+		mat4 P1 = cgv::math::stereo_frustum_screen4(1.0f, eye_separation, y_extent_at_focus * aspect, y_extent_at_focus, parallax_zero_depth, z_near, z_far);
+
+		mat4 MV0 = ctx.get_modelview_matrix();
+		mat4 MV1 = ctx.get_modelview_matrix();
+		stereo_translate_modelview_matrix(-1.0f, eye_separation, screen_width, MV0);
+		stereo_translate_modelview_matrix(1.0f, eye_separation, screen_width, MV1);
+
+		mat4 iP0 = inv(P0);
+		mat4 iP1 = inv(P1);
+
+		mat4 iMV0 = inv(MV0);
+		mat4 iMV1 = inv(MV1);
+
+		mat4 MVP0 = P0 * MV0;
+		mat4 MVP1 = P1 * MV1;
+
+		mat4 iMVP0 = inv(MVP0);
+		mat4 iMVP1 = inv(MVP1);
+
+		float eye_value = view_test - 1.0f;
+		float interpolation_param = 0.5f * view_test;
+
+		mat4 Pmid = cgv::math::stereo_frustum_screen4(eye_value, eye_separation, y_extent_at_focus * aspect, y_extent_at_focus, parallax_zero_depth, z_near, z_far);
+		mat4 MVmid = ctx.get_modelview_matrix();
+		stereo_translate_modelview_matrix(eye_value, eye_separation, screen_width, MVmid);
+
+		mat4 iPmid = inv(Pmid);
+		mat4 iMVmid = inv(MVmid);
+
+		mat4 MVPmid = Pmid * MVmid;
+		mat4 iMVPmid = inv(MVPmid);
+
+		vec3 col20(iMVP0.col(2));
+		vec3 col30(iMVP0.col(3));
+
+		vec3 col21(iMVP1.col(2));
+		vec3 col31(iMVP1.col(3));
+
+		vec3 col2int = cgv::math::lerp(col20, col21, interpolation_param);
+		vec3 col3int = cgv::math::lerp(col30, col31, interpolation_param);
+
+		vec3 col2mid(iMVPmid.col(2));
+		vec3 col3mid(iMVPmid.col(3));
+
+		mat4 Pint(0.0f);
+		mat4 MVint(0.0f);
+		mat4 MVPint(0.0f);
+		mat4 iPint(0.0f);
+		mat4 iMVint(0.0f);
+		mat4 iMVPint(0.0f);
+		for(int i = 0; i < 4; ++i) {
+			for(int j = 0; j < 4; ++j) {
+				Pint(i, j) = cgv::math::lerp(P0(i, j), P1(i, j), interpolation_param);
+				MVint(i, j) = cgv::math::lerp(MV0(i, j), MV1(i, j), interpolation_param);
+				MVPint(i, j) = cgv::math::lerp(MVP0(i, j), MVP1(i, j), interpolation_param);
+				iPint(i, j) = cgv::math::lerp(iP0(i, j), iP1(i, j), interpolation_param);
+				iMVint(i, j) = cgv::math::lerp(iMV0(i, j), iMV1(i, j), interpolation_param);
+				iMVPint(i, j) = cgv::math::lerp(iMVP0(i, j), iMVP1(i, j), interpolation_param);
+			}
+		}
+
+		mat4 D(0.0f);
+		for(int i = 0; i < 4; ++i) {
+			for(int j = 0; j < 4; ++j) {
+				D(i, j) = iMVPmid(i, j) - iMVPint(i, j);
+			}
+		}
+
+		std::cout << "Eye: " << eye_value << ", Interpoaltion t: " << interpolation_param << std::endl;
+		//std::cout << "Left Eye: " << std::endl;
+		//std::cout << iMVP0 << std::endl;
+		//std::cout << "Right Eye: " << std::endl;
+		//std::cout << iMVP1 << std::endl;
+		std::cout << "Center: " << std::endl;
+		std::cout << iMVPmid << std::endl;
+		std::cout << "Diff: " << std::endl;
+		vec3 d2 = col2int - col2mid;
+		vec3 d3 = col3int - col3mid;
+		std::cout << d2 << std::endl;
+		std::cout << d3 << std::endl;
+		//std::cout << "Interpolated: " << std::endl;
+		//std::cout << MVint << std::endl;*/
+		//std::cout << "Difference Original vs Interpolated: " << std::endl;
+		//std::cout << D << std::endl;
+	}
+
 	void create_gui() {
 		add_decorator(name, "heading");
 
-		add_member_control(this, "Scene", reinterpret_cast<cgv::type::DummyEnum&>(scene_index), "dropdown", "enums='Coordinate System Spheres, Hanging Object'");
-		
+		add_member_control(this, "Interpolate View Matrix", interpolate_view_matrix, "check");
+
 		add_member_control(this, "Eye Separation Factor", eye_separation_factor, "value_slider", "min=0;max=2;step=0.0625");
 
 		add_decorator("Light Direction", "separator");
@@ -353,17 +463,15 @@ public:
 
 		add_member_control(this, "Shadows", enable_shadows, "check");
 
-		add_member_control(this, "A", test[0], "value_slider", "min=0;max=2;step=0.0625");
-		add_member_control(this, "B", test[1], "value_slider", "min=0;max=2;step=0.0625");
-		add_member_control(this, "C", test[2], "value_slider", "min=0;max=2;step=0.0625");
-		add_member_control(this, "D", test[3], "value_slider", "min=0;max=2;step=0.0625");
+		add_member_control(this, "Debug Matrices", debug_matrices, "check");
+		add_member_control(this, "View Test", view_test, "value_slider", "min=-1;max=1;step=0.0625");
 	}
 
 	void reload_shaders() {
 		if(auto ctx_ptr = get_context()) {
 			auto& ctx = *ctx_ptr;
 			ctx.disable_shader_file_cache();
-			shaders.reload_all(ctx, "holo_raycast::init()");
+			cgv::render::shader_library::load(ctx, raycast_prog, "holo_raycast.glpr", true, "holo_raycast::init()");
 			ctx.enable_shader_file_cache();
 		}
 	}
