c018a1f3482a5a44895aec7605ff913aeefe5ce3
David Groß <davebig@web.de>
2023-06-28 17:27:37 +0200

    added circle draggable class

--- libs/cgv_g2d/shape.h
+++ libs/cgv_g2d/shape.h
@@ -2,12 +2,17 @@
 
 #include <cgv/render/render_types.h>
 
+#include "rect.h"
+
 #include "lib_begin.h"
 
 namespace cgv {
 namespace g2d {
 
 struct CGV_API shape_base : public frect {
+
+	using frect::frect;
+
 	bool position_is_center = false;
 
 	inline cgv::render::ivec2 int_position() const { return static_cast<cgv::render::ivec2>(position); }
@@ -17,7 +22,7 @@ struct CGV_API shape_base : public frect {
 
 	inline cgv::render::vec2 center() const { return position_is_center ? position : frect::center(); }
 
-	virtual bool is_inside(const cgv::render::vec2& query_pos) const {
+	inline virtual bool is_inside(const cgv::render::vec2& query_pos) const {
 
 		return frect::is_inside(position_is_center ? query_pos + frect::center() - position : query_pos);
 	}
