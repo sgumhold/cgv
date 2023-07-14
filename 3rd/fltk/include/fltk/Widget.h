sgumhold/cgv

commit 064962538e77839d78d79c3baa38da4ff812cbab

Eliminate annoying fltk/Widget.h warnings

3rd/fltk/include/fltk/Widget.h
@@ -118,7 +118,7 @@ public:
   void	callback(Callback1*c, long p=0) { callback_=(Callback*)c; user_data_=(void*)p; }
   void*	user_data() const	{ return user_data_; }
   void	user_data(void* v)	{ user_data_ = v; }
-  long	argument() const	{ return (long)user_data_; }
+  long	argument() const	{ return (long)(size_t)user_data_; }
   void	argument(long v)	{ user_data_ = (void*)v; }
   uchar when() const		{ return when_; }
   void	when(uchar i)		{ when_ = i; }

commit bb98f54a9c9feef883174be581c24a1b4ab31e03

Fix GCC/Clang build errors introduced in 064962538e77839d78d79c3baa38da4ff812cbab

3rd/fltk/include/fltk/Widget.h
@@ -22,6 +22,7 @@
 #ifndef fltk_Widget_h
 #define fltk_Widget_h
 
+#include "stddef.h"
 #include "Style.h"
 #include "Rectangle.h"
 