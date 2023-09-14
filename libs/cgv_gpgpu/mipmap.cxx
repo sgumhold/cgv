bf801d794ab500217c8be7ec05da36809c7f2dd1
David Groß <davebig@web.de>
2023-09-14 13:54:59 +0200

    Update mipmap gpgpu algorithm to support 1D textures and remove need to specify texture format.

--- libs/cgv_gpgpu/mipmap.cxx
+++ libs/cgv_gpgpu/mipmap.cxx
@@ -15,96 +15,11 @@ bool mipmap::load_shader_programs(cgv::render::context& ctx) {
 	bool res = true;
 	std::string where = "cgv::gpgpu::mipmap::load_shader_programs()";
 
-	cgv::render::shader_define_map defines;
-	cgv::render::shader_code::set_define(defines, "IMAGE_FORMAT", image_format, "");
-	cgv::render::shader_code::set_define(defines, "NUM_COMPONENTS", component_count, 1);
-	
-	res = res && cgv::render::shader_library::load(ctx, mipmap3d_prog, "gpgpu_mipmap2d", defines, true, where);
-	res = res && cgv::render::shader_library::load(ctx, mipmap3d_prog, "gpgpu_mipmap3d", defines, true, where);
+	res = res && cgv::render::shader_library::load(ctx, mipmap1d_prog, "gpgpu_mipmap1d", true, where);
+	res = res && cgv::render::shader_library::load(ctx, mipmap2d_prog, "gpgpu_mipmap2d", true, where);
+	res = res && cgv::render::shader_library::load(ctx, mipmap3d_prog, "gpgpu_mipmap3d", true, where);
 
 	return res;
-}
-
-void mipmap::compute_mipmaps2d(cgv::render::context& ctx, cgv::render::texture& source_texture) {
-
-	const unsigned texture_handle = (const unsigned&)source_texture.handle - 1;
-
-	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_2D, texture_handle);
-
-	mipmap2d_prog.enable(ctx);
-
-	uvec2 size(source_texture.get_width(), source_texture.get_height());
-
-	unsigned max_size = cgv::math::max_value(size);
-	unsigned num_levels = 1 + static_cast<unsigned>(log2(static_cast<float>(max_size)));
-
-	uvec2 input_size = size;
-
-	for(unsigned level = 0; level < num_levels - 1; ++level) {
-		glBindImageTexture(1, texture_handle, level + 1, GL_TRUE, 0, GL_WRITE_ONLY, GL_R8);
-
-		uvec2 output_size = size;
-		float divisor = static_cast<float>(pow(2, level + 1));
-
-		output_size = static_cast<uvec2>(static_cast<vec2>(output_size) / divisor);
-		output_size = cgv::math::max(output_size, uvec2(1u));
-
-		mipmap2d_prog.set_uniform(ctx, "level", level);
-		mipmap2d_prog.set_uniform(ctx, "output_size", output_size);
-
-		GLuint work_groups_x = calculate_num_groups(output_size.x(), group_size);
-		GLuint work_groups_y = calculate_num_groups(output_size.y(), group_size);
-
-		glDispatchCompute(work_groups_x, work_groups_y, 1);
-		glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
-
-		input_size = output_size;
-	}
-
-	mipmap2d_prog.disable(ctx);
-}
-
-void mipmap::compute_mipmaps3d(cgv::render::context& ctx, cgv::render::texture& source_texture) {
-
-	const unsigned texture_handle = (const unsigned&)source_texture.handle - 1;
-
-	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_3D, texture_handle);
-
-	mipmap3d_prog.enable(ctx);
-
-	uvec3 size(source_texture.get_width(), source_texture.get_height(), source_texture.get_depth());
-
-	unsigned max_size = cgv::math::max_value(size);
-	unsigned num_levels = 1 + static_cast<unsigned>(log2(static_cast<float>(max_size)));
-
-	uvec3 input_size = size;
-
-	for(unsigned level = 0; level < num_levels - 1; ++level) {
-		// TODO: use correct image format!
-		glBindImageTexture(1, texture_handle, level + 1, GL_TRUE, 0, GL_WRITE_ONLY, GL_R8);
-
-		uvec3 output_size = size;
-		float divisor = static_cast<float>(pow(2, level + 1));
-
-		output_size = static_cast<uvec3>(static_cast<vec3>(output_size) / divisor);
-		output_size = cgv::math::max(output_size, uvec3(1u));
-		
-		mipmap3d_prog.set_uniform(ctx, "level", level);
-		mipmap3d_prog.set_uniform(ctx, "output_size", output_size);
-
-		GLuint work_groups_x = calculate_num_groups(output_size.x(), group_size);
-		GLuint work_groups_y = calculate_num_groups(output_size.y(), group_size);
-		GLuint work_groups_z = calculate_num_groups(output_size.z(), group_size);
-
-		glDispatchCompute(work_groups_x, work_groups_y, work_groups_z);
-		glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
-
-		input_size = output_size;
-	}
-
-	mipmap3d_prog.disable(ctx);
 }
 
 bool mipmap::init(cgv::render::context& ctx) {
@@ -120,20 +35,87 @@ bool mipmap::init(cgv::render::context& ctx) {
 	return true;
 }
 
-void mipmap::execute(cgv::render::context& ctx, cgv::render::texture& source_texture) {
+bool mipmap::execute(cgv::render::context& ctx, cgv::render::texture& source_texture) {
+
+	if(!(source_texture.tt == cgv::render::TextureType::TT_1D ||
+	   source_texture.tt == cgv::render::TextureType::TT_2D ||
+	   source_texture.tt == cgv::render::TextureType::TT_3D))
+		return false;
 
 	if(!source_texture.have_mipmaps)
-		source_texture.generate_mipmaps(ctx);
+		source_texture.create_mipmaps(ctx);
 
-	if(source_texture.get_nr_dimensions() < 2)
-		return;
+	unsigned num_dimensions = source_texture.get_nr_dimensions();
 
-	if(source_texture.get_nr_dimensions() == 2)
-		compute_mipmaps2d(ctx, source_texture);
-	else if(source_texture.get_nr_dimensions() == 3)
-		compute_mipmaps3d(ctx, source_texture);
-	else
-		return;
+	GLenum texture_target = 0;
+	cgv::render::shader_program* mipmap_prog = nullptr;
+
+	switch(num_dimensions) {
+	case 1:
+		texture_target = GL_TEXTURE_1D;
+		mipmap_prog = &mipmap1d_prog;
+		break;
+	case 2:
+		texture_target = GL_TEXTURE_2D;
+		mipmap_prog = &mipmap2d_prog;
+		break;
+	case 3:
+		texture_target = GL_TEXTURE_3D;
+		mipmap_prog = &mipmap3d_prog;
+		break;
+	default: break;
+	}
+
+	const unsigned texture_handle = (const unsigned&)source_texture.handle - 1;
+	GLuint gl_format = (const GLuint&)source_texture.internal_format;
+
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(texture_target, texture_handle);
+
+	mipmap_prog->enable(ctx);
+
+	uvec3 size(source_texture.get_width(), source_texture.get_height(), source_texture.get_depth());
+
+	unsigned max_size = cgv::math::max_value(size);
+	unsigned num_levels = 1 + static_cast<unsigned>(log2(static_cast<float>(max_size)));
+
+	uvec3 input_size = size;
+
+	for(unsigned level = 0; level < num_levels - 1; ++level) {
+		glBindImageTexture(1, texture_handle, level + 1, GL_TRUE, 0, GL_WRITE_ONLY, gl_format);
+		
+		uvec3 output_size = size;
+		float divisor = static_cast<float>(pow(2, level + 1));
+
+		output_size = static_cast<uvec3>(static_cast<vec3>(output_size) / divisor);
+		output_size = cgv::math::max(output_size, uvec3(1u));
+		
+		mipmap_prog->set_uniform(ctx, "level", level);
+
+		GLuint work_groups_x = calculate_num_groups(output_size.x(), group_size);
+		GLuint work_groups_y = calculate_num_groups(output_size.y(), group_size);
+		GLuint work_groups_z = calculate_num_groups(output_size.z(), group_size);
+
+		if(num_dimensions == 1) {
+			mipmap_prog->set_uniform(ctx, "output_size", output_size.x());
+			work_groups_y = 1;
+			work_groups_z = 1;
+		} else if(num_dimensions == 2) {
+			mipmap_prog->set_uniform(ctx, "output_size", uvec2(output_size));
+			work_groups_z = 1;
+		} else if(num_dimensions == 3) {
+			mipmap_prog->set_uniform(ctx, "output_size", output_size);
+		}
+
+		glDispatchCompute(work_groups_x, work_groups_y, work_groups_z);
+		glMemoryBarrier(GL_SHADER_IMAGE_ACCESS_BARRIER_BIT);
+
+		input_size = output_size;
+	}
+
+	mipmap_prog->disable(ctx);
+
+	glBindTexture(texture_target, 0);
 }
 
 } // namespace gpgpu
