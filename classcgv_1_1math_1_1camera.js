284c58d951aa4825d2e132c5de2c013b5e5b4ec8
sgumhold <stefan.gumhold@tu-dresden.de>
2023-09-28 15:40:01 +0000

    Deploying to gh-pages from @ sgumhold/cgv@bc30e3b920a5992602842163a38dc50eef00db49 🚀

--- classcgv_1_1math_1_1camera.js
+++ classcgv_1_1math_1_1camera.js
@@ -5,7 +5,7 @@ var classcgv_1_1math_1_1camera =
     [ "camera", "classcgv_1_1math_1_1camera.html#a1200d2a1a175cb139d277b7f084ad2b8", null ],
     [ "camera", "classcgv_1_1math_1_1camera.html#a676ce2d90d6a679cb280a648f39f5703", null ],
     [ "apply_distortion_model", "classcgv_1_1math_1_1camera.html#aff51ec684c3612bb1ed2ce518c0294bd", null ],
-    [ "compute_undistortion_map", "classcgv_1_1math_1_1camera.html#a1e24e1dd969197d17970e8d5f168265d", null ],
+    [ "compute_undistortion_map", "classcgv_1_1math_1_1camera.html#aebc1fa366129a263e2189b100d44690a", null ],
     [ "get_standard_max_nr_iterations", "classcgv_1_1math_1_1camera.html#a026e015e0ea5d823ee4f8ff4a99709e7", null ],
     [ "get_standard_slow_down", "classcgv_1_1math_1_1camera.html#a1a5d09eb25a7e4f55798cf9c48937947", null ],
     [ "invert_distortion_model", "classcgv_1_1math_1_1camera.html#a9d873eb461f9f4a7075aabea65f52d7d", null ],
