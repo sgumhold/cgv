e021d81f11ec4426d1c7c993399bc87ae92dafeb
David Groß <davebig@web.de>
2023-11-03 14:29:08 +0100

    Add bresenham traverser and fix bug in previous implementation

--- cgv/math/bresenham.h
+++ cgv/math/bresenham.h
@@ -8,7 +8,7 @@
 namespace cgv {
 namespace math {
 
-/// @brief Computes a list of points along a rasterized line according to Bresenham_s line drawing algorithm.
+/// @brief Compute a list of points along a rasterized line according to Bresenham's line drawing algorithm.
 /// @param start The line start point.
 /// @param end The line end point.
 /// @return The list of rasterized points.
@@ -37,7 +37,7 @@ static std::vector<fvec<int, 2>> bresenham(fvec<int, 2> start, fvec<int, 2> end)
 	std::vector<fvec<int, 2>> points;
 
 	for(int x = start.x(); x <= end.x(); ++x) {
-		flip ? points.emplace_back(x, y) : points.emplace_back(y, x);
+		flip ? points.emplace_back(y, x) : points.emplace_back(x, y);
 		if(D > 0) {
 			y = y + yi;
 			D = D + (2 * (d.y() - d.x()));
@@ -47,6 +47,79 @@ static std::vector<fvec<int, 2>> bresenham(fvec<int, 2> start, fvec<int, 2> end)
 	}
 
 	return points;
+};
+
+class bresenham_traverser {
+private:
+	fvec<int, 2> _origin = fvec<int, 2>(0);
+	fvec<int, 2> _destination = fvec<int, 2>(0);
+	fvec<int, 2> _position = fvec<int, 2>(0);
+	bool _done = false;
+
+	int dx = 0;
+	int sx = 0;
+	int dy = 0;
+	int sy = 0;
+	int error = 0;
+
+public:
+
+	bresenham_traverser(fvec<int, 2> origin, fvec<int, 2> destination) : _origin(origin), _destination(destination) {
+		
+		_position = _origin;
+
+		if(_origin == _destination)
+			_done = true;
+
+		dx = std::abs(_destination.x() - _origin.x());
+		sx = _origin.x() < _destination.x() ? 1 : -1;
+		dy = -std::abs(_destination.y() - _origin.y());
+		sy = _origin.y() < _destination.y() ? 1 : -1;
+		error = dx + dy;
+	}
+
+	bool done() const {
+
+		return _done;
+	}
+
+	fvec<int, 2> position() const {
+
+		return _position;
+	}
+
+	fvec<int, 2> origin() const {
+
+		return _origin;
+	}
+
+	fvec<int, 2> destination() const {
+
+		return _destination;
+	}
+
+	void step() {
+		
+		if(_position == _destination)
+			_done = true;
+
+		int e2 = 2 * error;
+		
+		if(e2 >= dy) {
+			error += dy;
+			_position.x() += sx;
+		}
+
+		if(e2 <= dx) {
+			error += dx;
+			_position.y() += sy;
+		}
+	}
+
+	void operator++() {
+
+		step();
+	}
 };
 
 } // namespace math
