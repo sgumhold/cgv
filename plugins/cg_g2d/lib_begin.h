22992e9bb53a25b98ad3d2eedaa0d08349959b1e
David Groß <davebig@web.de>
2024-01-04 10:57:16 +0100

    Temporarily remove cg_g2d plugin to merge branch

--- plugins/cg_g2d/lib_begin.h
+++
@@ -1,8 +0,0 @@
-#if defined(CGV_GUI_FORCE_STATIC) || defined(CGV_GUI_G2D_FORCE_STATIC)
-#	define CGV_FORCE_STATIC_LIB
-#endif
-#if defined(CGV_GUI_G2D_EXPORTS) || defined(CG_G2D_EXPORTS)
-#	define CGV_EXPORTS
-#endif
-
-#include <cgv/config/lib_begin.h>
