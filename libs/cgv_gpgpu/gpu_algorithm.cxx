e99cd6d1ab4a6e5b2825403132e0c54439da73dc
David Groß <davebig@web.de>
2024-02-08 15:50:32 +0100

    Reiterate on workgroup calculation code - Change in 851038d4f13f5092562cbf7ba7aec82ef37d6b53
    - The previous change prevented using the methods from outside the cgv_gpgpu project
      as they were defined in a source file and therefore this would result in undefined
      symbols.

--- libs/cgv_gpgpu/gpu_algorithm.cxx
+++ libs/cgv_gpgpu/gpu_algorithm.cxx
@@ -19,16 +19,5 @@ void gpu_algorithm::delete_buffer(const cgv::render::context& ctx, cgv::render::
 	buffer.destruct(ctx);
 }
 
-template <typename T> T gpu_algorithm::calculate_num_groups(T num_elements, T num_elements_per_group)
-{
-	assert(T(0) != num_elements_per_group);
-	assert(T(0) != num_elements);
-	return (num_elements + num_elements_per_group - T(1)) / num_elements_per_group;
-}
-
-template unsigned gpu_algorithm::calculate_num_groups(unsigned, unsigned);
-template uvec2 gpu_algorithm::calculate_num_groups(uvec2, uvec2);
-template uvec3 gpu_algorithm::calculate_num_groups(uvec3, uvec3);
-
 }
 }
