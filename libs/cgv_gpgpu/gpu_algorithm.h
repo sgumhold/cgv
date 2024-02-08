128655c881bfbc8481a9a4aaf779ba1157be0196
David Groß <davebig@web.de>
2024-02-08 15:50:32 +0100

    Reiterate on workgroup calculation code - Change in af48b5e12284f419139aabeaa0428666f1761ab0
    - The previous change prevented using the methods from outside the cgv_gpgpu project
      as they were defined in a source file and therefore this would result in undefined
      symbols.

--- libs/cgv_gpgpu/gpu_algorithm.h
+++ libs/cgv_gpgpu/gpu_algorithm.h
@@ -14,6 +14,14 @@ namespace gpgpu {
 
 /** Definition of base functionality for highly parallel gpu algorithms. */
 class CGV_API gpu_algorithm {
+private:
+	template<typename T>
+	T calculate_num_groups_void(T num_elements, T num_elements_per_group) const {
+		assert(T(0) != num_elements_per_group);
+		assert(T(0) != num_elements);
+		return (num_elements + num_elements_per_group - T(1)) / num_elements_per_group;
+	}
+
 protected:
 	bool is_initialized_ = false;
 
@@ -23,14 +31,24 @@ protected:
 
 	virtual bool load_shader_programs(cgv::render::context& ctx) = 0;
 
-	unsigned calculate_padding(unsigned num_elements, unsigned num_elements_per_group) {
+	unsigned calculate_padding(unsigned num_elements, unsigned num_elements_per_group) const {
 		unsigned n_pad = num_elements_per_group - (num_elements % num_elements_per_group);
 		if(num_elements % num_elements_per_group == 0)
 			n_pad = 0;
 		return n_pad;
 	}
 
-	template<typename T> T calculate_num_groups(T num_elements, T num_elements_per_group);
+	unsigned calculate_num_groups(unsigned num_elements, unsigned num_elements_per_group) const {
+		return calculate_num_groups_void(num_elements, num_elements_per_group);
+	}
+
+	uvec2 calculate_num_groups(uvec2 num_elements, uvec2 num_elements_per_group) const {
+		return calculate_num_groups_void(num_elements, num_elements_per_group);
+	}
+
+	uvec3 calculate_num_groups(uvec3 num_elements, uvec3 num_elements_per_group) const {
+		return calculate_num_groups_void(num_elements, num_elements_per_group);
+	}
 
 	void dispatch_compute1d(unsigned num_groups) {
 		glDispatchCompute(num_groups, 1u, 1u);
