e673af863314180b4129380f60231fc910c2a9b7
David Groß <davebig@web.de>
2024-02-12 17:27:49 +0100

    Increase height of GUI tabs to match menu bar

--- plugins/cg_fltk/fltk_tab_group.cxx
+++ plugins/cg_fltk/fltk_tab_group.cxx
@@ -126,7 +126,7 @@ void* fltk_tab_group::get_user_data() const
 void fltk_tab_group::prepare_new_element(cgv::gui::gui_group_ptr ggp, int& x, int& y, int& w, int& h)
 {
 	bool tabs_bottom = fltk::theme_idx_ < 0;
-	int tab_height = 24;
+	int tab_height = 25;
 	x = 0;
 	y = tabs_bottom ? 0 : tab_height;
 	w = tab_group->w();
