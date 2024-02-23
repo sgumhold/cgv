a46897bdfc7565756b77a0f83ccf6daf9e149352
sgumhold <stefan.gumhold@tu-dresden.de>
2024-02-24 00:27:13 +0100

    added missing std:: prefixes

--- cgv/data/image_view.h
+++ cgv/data/image_view.h
@@ -19,7 +19,7 @@ public:
 	size_t get_width() const { return df.get_width(); }
 	size_t get_height() const { return df.get_height(); }
 	data_view& ref_data_view() { return dv; }
-	void create(const std::string& fmt_dcr, ptrdiff_t w=-1, ptrdiff_t h=-1);
+	void create(const std::string& fmt_dcr, std::ptrdiff_t w=-1, std::ptrdiff_t h=-1);
 	void clear(double v);
 	template <typename T>
 	T& ref(size_t i, size_t j) { return *((T*)ref_void(i,j)); }
@@ -27,7 +27,7 @@ public:
 	const T& get(size_t i, size_t j) const { return *((const T*)get_void(i,j)); }
 	template <typename T>
 	T* ptr(size_t i, size_t j) { return (T*)ref_void(i,j); }
-	void copy_rectangle(const image_view& src, ptrdiff_t X = 0, ptrdiff_t Y = 0, ptrdiff_t x = 0, ptrdiff_t y = 0, ptrdiff_t w = -1, ptrdiff_t h = -1);
+	void copy_rectangle(const image_view& src, std::ptrdiff_t X = 0, std::ptrdiff_t Y = 0, std::ptrdiff_t x = 0, std::ptrdiff_t y = 0, std::ptrdiff_t w = -1, std::ptrdiff_t h = -1);
 	void sub_sample();
 };
 
