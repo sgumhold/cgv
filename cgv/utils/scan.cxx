sgumhold/cgv

commit 2d4b53be16050cb26a66fe6b8b8e86249da4980a

changed implementation of is_integer and is_double to std::from_char in case of C++17 and later also added the compiler switch /Zc:__cplusplus to the pj make file generation process such that __cplusplus is defined correctly

cgv/utils/scan.cxx
@@ -2,6 +2,9 @@
 
 #include <stdlib.h>
 #include <algorithm>
+#if __cplusplus >= 201703L
+#include <charconv>
+#endif
 
 namespace cgv {
 	namespace utils {
@@ -354,6 +357,10 @@ bool is_integer(const char* begin, const char* end, int& value)
 {
 	if (begin == end)
 		return false;
+
+#if __cplusplus >= 201703L
+	return std::from_chars(begin, end, value).ec == std::errc();
+#else
 	// skip trailing spaces
 	while (begin < end && *begin == ' ')
 		++begin;
@@ -397,6 +404,7 @@ bool is_integer(const char* begin, const char* end, int& value)
 		return true;
 	}
 	return false;
+#endif
 }
 
 bool is_integer(const std::string& s, int& value)
@@ -408,6 +416,9 @@ bool is_double(const char* begin, const char* end, double& value)
 {
 	if (begin == end)
 		return false;
+#if __cplusplus >= 201703L
+	return std::from_chars(begin, end, value).ec == std::errc();
+#else
 	bool found_digit = false;
 	int nr_dots = 0;
 	int nr_exp = 0;
@@ -455,6 +466,7 @@ bool is_double(const char* begin, const char* end, double& value)
 		return false;
 	value = atof(std::string(begin,end-begin).c_str());
 	return true; 
+#endif
 }
 
 bool is_double(const std::string& s, double& value)
@@ -742,7 +754,7 @@ unsigned int levenshtein_distance(const std::string& s1, const std::string& s2)
 	// this row is A[0][i]: edit distance from an empty s to t;
 	// that distance is the number of characters to append to  s to make t.
 	for (size_t i = 0; i <= s2.length(); ++i) {
-		v0[i] = i;
+		v0[i] = int(i);
 	}
 
 	for (size_t i = 0; i < s1.length(); ++i) {
@@ -750,7 +762,7 @@ unsigned int levenshtein_distance(const std::string& s1, const std::string& s2)
 
 		// first element of v1 is A[i + 1][0]
 		//   edit distance is delete (i + 1) chars from s to match empty t
-		v1[0] = i + 1;
+		v1[0] = int(i + 1);
 
 		// use formula to fill in the rest of the row
 		for (size_t j = 0; j < s2.length(); ++j) {

make/ppp/templates/vs10_vcxproj.tm
@@ -148,6 +148,7 @@
 @}
 @if(pj::cppLanguageStandard != "")@{@//
       <LanguageStandard>@(pj::cppLanguageStandard)</LanguageStandard>
+      <AdditionalOptions>/Zc:__cplusplus %(AdditionalOptions)</AdditionalOptions>
 @}@//
 	</ClCompile>
     <@(pj::linker_tool_vs10[cj])>

commit a4239e4db74cac730eed21f8aa05d2701214eef7

Fix potentially crash inducing logic bug introduced in 2d4b53be16050cb26a66fe6b8b8e86249da4980a
To check whether a sequence of bytes represents an integer, once should not just check if no parsing error occurred, but also where exactly in the sequence the parser stopped. The C++17 standard states that parses should return success even if they could not continue through the whole sequence as long as the bytes already consumed can be successfully parsed. The code from 2d4b53be16050cb26a66fe6b8b8e86249da4980a would thus conclude that almost all floating point numbers are integers, since the parser just stops at the decimal point and reports whatever the integer part of the number was, breaking (among many other things) large parts of the GUI infrastructure handling floating point numbers.

cgv/utils/scan.cxx
@@ -353,13 +353,21 @@ int get_element_index(const std::string& e, const std::string& s, char sep)
 	return -1;
 }
 
+#if __cplusplus >= 201703L
+	// We will be using std::from chars below for parsing, this provides the whitespace check we need there for correct logic
+	inline const bool char_is_whitespace_or_zero (const char ch) {
+		return ch == 0 || ch == '\r' || ch == '\n' || ch == ' ' || ch == '\t';
+	}
+#endif
+
 bool is_integer(const char* begin, const char* end, int& value)
 {
 	if (begin == end)
 		return false;
 
 #if __cplusplus >= 201703L
-	return std::from_chars(begin, end, value).ec == std::errc();
+	const auto fc = std::from_chars(begin, end, value);
+	return fc.ec == std::errc() && char_is_whitespace_or_zero(*fc.ptr);
 #else
 	// skip trailing spaces
 	while (begin < end && *begin == ' ')