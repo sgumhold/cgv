sgumhold/cgv

commit 4c599d3f4442e3ceea597105dc9601ec175b34c2

Fix compilation errors on 32bit builds

cgv/render/texture.cxx
@@ -247,7 +247,7 @@ bool is_power_of_two(unsigned int i)
 	while (true);
 	return false;
 }
-bool is_power_of_two(size_t i)
+bool is_power_of_two(unsigned long long i)
 {
 	do {
 		if (i == 1)
@@ -267,7 +267,7 @@ unsigned int power_of_two_ub(unsigned int i)
 		res *= 2;
 	return res;
 }
-size_t power_of_two_ub(size_t i)
+size_t power_of_two_ub(unsigned long long i)
 {
 	size_t res = 2;
 	while (res < i)

commit b322bb4d45bc333207cb1fa79e0df680208ee510

Fix Clang build errors introduced in 4c599d3f4442e3ceea597105dc9601ec175b34c2

cgv/render/texture.cxx
@@ -235,19 +235,8 @@ void texture::set_fixed_sample_locations(bool use)
 	fixed_sample_locations = use; 
 }
 
-bool is_power_of_two(unsigned int i)
-{
-	do {
-		if (i == 1)
-			return true;
-		if ((i & 1) != 0)
-			return false;
-		i /= 2;
-	} 
-	while (true);
-	return false;
-}
-bool is_power_of_two(unsigned long long i)
+template <class int_type>
+bool is_power_of_two(int_type i)
 {
 	do {
 		if (i == 1)
@@ -260,16 +249,10 @@ bool is_power_of_two(unsigned long long i)
 	return false;
 }
 
-unsigned int power_of_two_ub(unsigned int i)
+template <class int_type>
+int_type power_of_two_ub(int_type i)
 {
-	unsigned int res = 2;
-	while (res < i)
-		res *= 2;
-	return res;
-}
-size_t power_of_two_ub(unsigned long long i)
-{
-	size_t res = 2;
+	int_type res = 2;
 	while (res < i)
 		res *= 2;
 	return res;