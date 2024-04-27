sgumhold/cgv

commit 3931a57fd2f8992d46dab2a99ff49f43325c49c0

CMake: make launch configs build type-aware for first-class support of JetBrains IDEs

tool/set_plugin_execution_params.cmake
@@ -190,24 +190,34 @@ function(create_idea_run_entry CONTENT_VAR TARGET_NAME)
 
 	# Generate plugin build launch config
 	set(IDEA_WORKING_DIR "file://${CGVARG__WORKING_DIR}")
+	if(CMAKE_BUILD_TYPE MATCHES "Release")
+		set(IDEA_CONFIG_NAME "${TARGET_NAME}")
+	else()
+		set(IDEA_CONFIG_NAME "${TARGET_NAME} (${CMAKE_BUILD_TYPE})")
+	endif()
 	set(IDEA_TARGET_NAME "${TARGET_NAME}")
 	set(IDEA_LAUNCH_CMD "${LAUNCH_PROGRAM_PLUGIN}")
 	format_idea_launch_args(IDEA_LAUNCH_ARGS ARGUMENT_LIST ${CMD_ARGS_PLUGIN})
-	configure_file("${CGV_DIR}/make/cmake/idea_launch.xml.in" "${CMAKE_SOURCE_DIR}/.run/${TARGET_NAME}.run.xml" @ONLY)
+	configure_file("${CGV_DIR}/make/cmake/idea_launch.xml.in" "${CMAKE_SOURCE_DIR}/.run/${IDEA_CONFIG_NAME}.run.xml" @ONLY)
 	file(
-		GENERATE OUTPUT "${CMAKE_SOURCE_DIR}/.run/${TARGET_NAME}.run.xml"
-		INPUT "${CMAKE_SOURCE_DIR}/.run/${TARGET_NAME}.run.xml"
+		GENERATE OUTPUT "${CMAKE_SOURCE_DIR}/.run/${IDEA_CONFIG_NAME}.run.xml"
+		INPUT "${CMAKE_SOURCE_DIR}/.run/${IDEA_CONFIG_NAME}.run.xml"
 		USE_SOURCE_PERMISSIONS
 	)
 	if (NOT CGVARG__NO_EXECUTABLE)
 		# If required, generate single-exec build launch config
+		if(CMAKE_BUILD_TYPE MATCHES "Release")
+			set(IDEA_CONFIG_NAME "${NAME_EXE}")
+		else()
+			set(IDEA_CONFIG_NAME "${NAME_EXE} (${CMAKE_BUILD_TYPE})")
+		endif()
 		set(IDEA_TARGET_NAME "${NAME_EXE}")
 		set(IDEA_LAUNCH_CMD "${LAUNCH_PROGRAM_EXE}")
 		format_idea_launch_args(IDEA_LAUNCH_ARGS ARGUMENT_LIST ${CMD_ARGS_EXE})
-		configure_file("${CGV_DIR}/make/cmake/idea_launch.xml.in" "${CMAKE_SOURCE_DIR}/.run/${NAME_EXE}.run.xml" @ONLY)
+		configure_file("${CGV_DIR}/make/cmake/idea_launch.xml.in" "${CMAKE_SOURCE_DIR}/.run/${IDEA_CONFIG_NAME}.run.xml" @ONLY)
 		file(
-			GENERATE OUTPUT "${CMAKE_SOURCE_DIR}/.run/${NAME_EXE}.run.xml"
-			INPUT "${CMAKE_SOURCE_DIR}/.run/${NAME_EXE}.run.xml"
+			GENERATE OUTPUT "${CMAKE_SOURCE_DIR}/.run/${IDEA_CONFIG_NAME}.run.xml"
+			INPUT "${CMAKE_SOURCE_DIR}/.run/${IDEA_CONFIG_NAME}.run.xml"
 			USE_SOURCE_PERMISSIONS
 		)
 	endif()

make/cmake/idea_launch.xml.in
@@ -1,5 +1,5 @@
 <component name="ProjectRunConfigurationManager">
-  <configuration default="false" name="@IDEA_TARGET_NAME@" type="CMakeRunConfiguration" factoryName="Application" PROGRAM_PARAMS="@IDEA_LAUNCH_ARGS@" REDIRECT_INPUT="false" ELEVATE="false" USE_EXTERNAL_CONSOLE="false" EMULATE_TERMINAL="true" WORKING_DIR="@IDEA_WORKING_DIR@" PASS_PARENT_ENVS_2="true" PROJECT_NAME="@CMAKE_PROJECT_NAME@" TARGET_NAME="@IDEA_TARGET_NAME@" CONFIG_NAME="Debug" RUN_PATH="@IDEA_LAUNCH_CMD@">
+  <configuration default="false" name="@IDEA_CONFIG_NAME@" type="CMakeRunConfiguration" factoryName="Application" PROGRAM_PARAMS="@IDEA_LAUNCH_ARGS@" REDIRECT_INPUT="false" ELEVATE="false" USE_EXTERNAL_CONSOLE="false" EMULATE_TERMINAL="true" WORKING_DIR="@IDEA_WORKING_DIR@" PASS_PARENT_ENVS_2="true" PROJECT_NAME="@CMAKE_PROJECT_NAME@" TARGET_NAME="@IDEA_TARGET_NAME@" CONFIG_NAME="Debug" RUN_PATH="@IDEA_LAUNCH_CMD@">
     <envs>
       <env name="CGV_DIR" value="@CGV_DIR@" />
       <env name="CGV_OPTIONS" value="@CGV_OPTIONS@" />

commit 6571628f99e811692822022f1e305c51c52eea22

CMake: fix multi-config builds broken by 3931a57fd2f8992d46dab2a99ff49f43325c49c0

3rd/openal-soft/CMakeLists.txt
@@ -25,9 +25,9 @@ foreach(OAL_TGT ${OAL_TARGETS})
 	# check wether we can add flags to the target
 	get_target_property(TGT_TYPE ${OAL_TGT} TYPE)
 	if (NOT TGT_TYPE STREQUAL "UTILITY")
-		if (CMAKE_C_COMPILER_ID MATCHES "GNU" OR MAKE_CXX_COMPILER_ID MATCHES "GNU")
+		if (CMAKE_C_COMPILER_ID MATCHES "GNU" OR CMAKE_CXX_COMPILER_ID MATCHES "GNU")
 			target_compile_options(${OAL_TGT} PRIVATE -Wno-stringop-overflow -Wno-inline)
-		elseif(CMAKE_C_COMPILER_ID MATCHES "Clang" OR MAKE_CXX_COMPILER_ID MATCHES "Clang")
+		elseif(CMAKE_C_COMPILER_ID MATCHES "Clang" OR CMAKE_CXX_COMPILER_ID MATCHES "Clang")
 			target_compile_options(${OAL_TGT} PRIVATE -Wno-everything)
 		endif()
 	endif()

CMakeLists.txt
@@ -16,6 +16,12 @@ set(CGV_IS_CONFIGURING TRUE)
 # detect whether we're being included as a subproject
 get_directory_property(CGV_HAS_PARENT_PROJ PARENT_DIRECTORY)
 
+# detect whether we're configuring for a multi-config generator
+get_property(CGV_USING_MULTI_CONFIG GLOBAL PROPERTY GENERATOR_IS_MULTI_CONFIG)
+if(CGV_HAS_PARENT_PROJ)
+	set(CGV_USING_MULTI_CONFIG ${CGV_USING_MULTI_CONFIG} PARENT_SCOPE)
+endif()
+
 # setup relevant directories
 # - "the" CGV directory
 set(CGV_DIR ${PROJECT_SOURCE_DIR})

tool/cgv_target_creation.cmake
@@ -333,12 +333,18 @@ function(cgv_do_deferred_ops TARGET_NAME CONFIGURING_CGV)
 				USE_SOURCE_PERMISSIONS
 			)
 
-			# (2) IntelliJ IDEA (CLion etc.)
-			create_idea_run_entry(
-				VSCODE_TARGET_LAUNCH_JSON_CONFIGS ${TARGET_NAME} ${NO_EXE_FLAG} WORKING_DIR ${WORKING_DIR}
-				PLUGIN_ARGS ${AUTOGEN_CMD_LINE_ARGS};${ADDITIONAL_ARGS} EXE_ARGS ${ADDITIONAL_ARGS}
-				INVOCATION_PROXY ${INVOCATION_PROXY}
-			)
+			# (2) JetBrains IDEs (IDEA, CLion etc.)
+			#   Multi-config generators are not properly supported in these IDEs (also unneccesary). Generating launch configs for them
+			#   becomes problematic with multi-config generators due to the way CMake invokes file generation, so the easiest workaround
+			#   is to just not generate launch configs when a multi-config generator is used.
+			#   TODO: proper handling of this is possible and should be implemented at some point
+			if (NOT CGV_USING_MULTI_CONFIG)
+				create_idea_run_entry(
+					${TARGET_NAME} ${NO_EXE_FLAG} WORKING_DIR ${WORKING_DIR}
+					PLUGIN_ARGS ${AUTOGEN_CMD_LINE_ARGS};${ADDITIONAL_ARGS} EXE_ARGS ${ADDITIONAL_ARGS}
+					INVOCATION_PROXY ${INVOCATION_PROXY}
+				)
+			endif()
 
 			# (3) Visual Studio Code
 			concat_vscode_launch_json_content(

tool/set_plugin_execution_params.cmake
@@ -167,9 +167,9 @@ function(concat_vscode_launch_json_content LAUNCH_JSON_CONFIG_VAR TARGET_NAME)
 	set(${LAUNCH_JSON_CONFIG_VAR} ${JSON_LIST_STRING} PARENT_SCOPE)
 endfunction()
 
-function(create_idea_run_entry CONTENT_VAR TARGET_NAME)
+function(create_idea_run_entry TARGET_NAME)
 	cmake_parse_arguments(
-		PARSE_ARGV 2 CGVARG_ "NO_EXECUTABLE" "INVOCATION_PROXY;WORKING_DIR" "PLUGIN_ARGS;EXE_ARGS"
+		PARSE_ARGV 1 CGVARG_ "NO_EXECUTABLE" "INVOCATION_PROXY;WORKING_DIR" "PLUGIN_ARGS;EXE_ARGS"
 	)
 
 	# determine target names