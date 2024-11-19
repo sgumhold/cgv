2feb832cc11084577d2a85058d44f420c2d2bdc3
David Groß <davebig@web.de>
2024-11-19 17:20:58 +0100

    Restructure and rename

--- libs/cgv_gpgpu/gpu_algorithm.cxx
+++ libs/cgv_gpgpu/gpu_algorithm.cxx
@@ -3,6 +3,33 @@
 namespace cgv {
 namespace gpgpu {
 
+std::map<std::string, int> gpu_algorithm::get_program_uniforms(cgv::render::context& ctx, cgv::render::shader_program& prog) const {
+	GLuint id = reinterpret_cast<GLuint>(prog.handle) - 1;
+
+	std::map<std::string, int> map;
+
+	if(id == 0)
+		return map;
+	
+	GLint num_active_uniforms = 0;
+	glGetProgramiv(id, GL_ACTIVE_UNIFORMS, &num_active_uniforms);
+	
+	std::vector<GLchar> buffer(256);
+	for(int i = 0; i < num_active_uniforms; ++i) {
+		GLint array_size = 0;
+		GLenum type = 0;
+		GLsizei actual_length = 0;
+		glGetActiveUniform(id, i, buffer.size(), &actual_length, &array_size, &type, buffer.data());
+		std::string name(static_cast<char*>(buffer.data()), actual_length);
+
+		int location = prog.get_uniform_location(ctx, name);
+		if(location > -1)
+			map[name] = location;
+	}
+
+	return map;
+}
+
 void gpu_algorithm::ensure_buffer(const cgv::render::context& ctx, cgv::render::vertex_buffer& buffer, size_t size, cgv::render::VertexBufferType type, cgv::render::VertexBufferUsage usage) {
 
 	if(buffer.is_created() && buffer.type == type && buffer.usage == usage) {
--- libs/cgv_gpgpu/gpu_algorithm.h
+++ libs/cgv_gpgpu/gpu_algorithm.h
@@ -14,22 +14,36 @@ namespace gpgpu {
 
 /** Definition of base functionality for highly parallel gpu algorithms. */
 class CGV_API gpu_algorithm {
-private:
-	template<typename T>
-	T calculate_num_groups_void(T num_elements, T num_elements_per_group) const {
-		assert(T(0) != num_elements_per_group);
-		assert(T(0) != num_elements);
-		return (num_elements + num_elements_per_group - T(1)) / num_elements_per_group;
-	}
+public:
+	gpu_algorithm() {}
+	~gpu_algorithm() {}
+
+	virtual bool init(cgv::render::context& ctx, size_t count) = 0;
+
+	virtual void destruct(const cgv::render::context& ctx) = 0;
+
+	bool is_initialized() const { return is_initialized_; }
 
 protected:
 	bool is_initialized_ = false;
+
+	// TODO: Move this functionality to the shader_program class.
+	std::map<std::string, int> get_program_uniforms(cgv::render::context& ctx, cgv::render::shader_program& prog) const;
 
 	void ensure_buffer(const cgv::render::context& ctx, cgv::render::vertex_buffer& buffer, size_t size, cgv::render::VertexBufferType type = cgv::render::VertexBufferType::VBT_STORAGE, cgv::render::VertexBufferUsage usage = cgv::render::VertexBufferUsage::VBU_STREAM_COPY);
 
 	void delete_buffer(const cgv::render::context& ctx, cgv::render::vertex_buffer& buffer);
 
 	virtual bool load_shader_programs(cgv::render::context& ctx) = 0;
+
+	// TODO: Move to utils or math.
+	/// @brief Return the closest power of two larger than x
+	uint32_t next_power_of_two(uint32_t x) const {
+		uint32_t pot = 1;
+		while(x > pot)
+			pot <<= 1;
+		return pot;
+	}
 
 	unsigned calculate_padding(unsigned num_elements, unsigned num_elements_per_group) const {
 		unsigned n_pad = num_elements_per_group - (num_elements % num_elements_per_group);
@@ -62,15 +76,32 @@ protected:
 		glDispatchCompute(num_groups[0], num_groups[1], num_groups[2]);
 	}
 
+	void dispatch_compute(unsigned num_groups_x, unsigned num_groups_y, unsigned num_groups_z) {
+		glDispatchCompute(num_groups_x, num_groups_y, num_groups_z);
+	}
+
+private:
+	template<typename T>
+	T calculate_num_groups_void(T num_elements, T num_elements_per_group) const {
+		assert(T(0) != num_elements_per_group);
+		assert(T(0) != num_elements);
+		return (num_elements + num_elements_per_group - T(1)) / num_elements_per_group;
+	}
+};
+
+// TODO: Move to utility.
+template<typename T>
+class double_buffer {
 public:
-	gpu_algorithm() {}
-	~gpu_algorithm() {}
+	double_buffer(T* first, T* second) : _first(first), _second(second) {}
 
-	virtual bool init(cgv::render::context& ctx, size_t count) = 0;
+	T* first() { return _first; }
+	T* second() { return _second; }
 
-	virtual void destruct(const cgv::render::context& ctx) = 0;
-
-	bool is_initialized() const { return is_initialized_; }
+	void swap() { std::swap(_first, _second); }
+private:
+	T* _first = nullptr;
+	T* _second = nullptr;
 };
 
 }
