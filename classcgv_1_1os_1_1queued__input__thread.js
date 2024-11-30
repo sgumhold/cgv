0ec8c28c5f9f7aaa5129d11c2907e99ebf05d928
domi-commits <76519246+domi-commits@users.noreply.github.com>
2024-11-30 15:21:46 +0000

    Deploying to gh-pages from @ sgumhold/cgv@b79f377ecfebc6331843c0fcb99283959a1786b8 🚀

--- classcgv_1_1os_1_1queued__input__thread.js
+++ classcgv_1_1os_1_1queued__input__thread.js
@@ -1,18 +1,22 @@
 var classcgv_1_1os_1_1queued__input__thread =
 [
-    [ "queued_input_thread", "classcgv_1_1os_1_1queued__input__thread.html#a81b43a638f12e02bb3a79c6e9e20d83d", null ],
+    [ "queued_input_thread", "classcgv_1_1os_1_1queued__input__thread.html#a0a9b6c9fdfecfa0597e845165783f52e", null ],
     [ "close", "classcgv_1_1os_1_1queued__input__thread.html#a32619322e0c6826ff62fa879e9e726fa", null ],
     [ "connect_to_source", "classcgv_1_1os_1_1queued__input__thread.html#a394ccd58255c66dff228182c96045c81", null ],
     [ "done", "classcgv_1_1os_1_1queued__input__thread.html#ad68eec45f64157dc31e4794f06310d66", null ],
     [ "get_nr_blocks", "classcgv_1_1os_1_1queued__input__thread.html#a771d403912fc60d99c1c5764396d1976", null ],
-    [ "read_block_from_pipe", "classcgv_1_1os_1_1queued__input__thread.html#a625fd2d8adb5b9ee775ff0e62866713f", null ],
-    [ "receive_block", "classcgv_1_1os_1_1queued__input__thread.html#ad33f8a3d405d32536e65127e95748acb", null ],
+    [ "move_packages_to_data_blocks", "classcgv_1_1os_1_1queued__input__thread.html#affdfc42f4014c6bcf8fd98f5c4c7193f", null ],
+    [ "pop_data_block", "classcgv_1_1os_1_1queued__input__thread.html#a2fecff057c2b089963552b1fe097c013", null ],
+    [ "read_package_from_pipe", "classcgv_1_1os_1_1queued__input__thread.html#a3405c9bca673dfc072c36619b79f06f9", null ],
     [ "run", "classcgv_1_1os_1_1queued__input__thread.html#a7c2ea30ef95b610381e42bc295c35c8a", null ],
-    [ "set_block_buffer_size", "classcgv_1_1os_1_1queued__input__thread.html#ab2e7485b22f23d0c901ec5baaf30f650", null ],
     [ "all_data_received", "classcgv_1_1os_1_1queued__input__thread.html#af13de54985e39dcc135e3228d9ee04e3", null ],
-    [ "block_buffer_size", "classcgv_1_1os_1_1queued__input__thread.html#a6a4d3a885e340db19667b9a952526d60", null ],
-    [ "blocks", "classcgv_1_1os_1_1queued__input__thread.html#a833f8f4e9226b23eedf65b21a3c61271", null ],
+    [ "data_blocks", "classcgv_1_1os_1_1queued__input__thread.html#a20551523e5f04ac157417ba586bf57ca", null ],
     [ "is_binary", "classcgv_1_1os_1_1queued__input__thread.html#ac3f928c6c36249f779d28779bb0e69d0", null ],
-    [ "m", "classcgv_1_1os_1_1queued__input__thread.html#a617edc393242424d79382cb6f4ab9b8b", null ],
-    [ "ms_to_wait", "classcgv_1_1os_1_1queued__input__thread.html#acd4dd1019a1dc07dabbd5e457cc0ab9f", null ]
+    [ "ms_to_wait", "classcgv_1_1os_1_1queued__input__thread.html#acd4dd1019a1dc07dabbd5e457cc0ab9f", null ],
+    [ "mutex_data_blocks", "classcgv_1_1os_1_1queued__input__thread.html#ac717919093dec155495a4bd3011feaf7", null ],
+    [ "mutex_packages", "classcgv_1_1os_1_1queued__input__thread.html#a93ffd9c2b6cd55fb2c9dcc1124e9df6d", null ],
+    [ "package_index", "classcgv_1_1os_1_1queued__input__thread.html#a8a9de6723e560d31df20a61b397315c7", null ],
+    [ "package_size", "classcgv_1_1os_1_1queued__input__thread.html#a1194608605933ccc4b3cb10031dd2332", null ],
+    [ "packages", "classcgv_1_1os_1_1queued__input__thread.html#a3b540ca8eeb2b63d70a2d9d948c7c71e", null ],
+    [ "packages_per_block", "classcgv_1_1os_1_1queued__input__thread.html#a2e2679eadd5fd4cf501d26d475bed2f5", null ]
 ];
--- search/files_4.js
+++ search/files_4.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['scan_2eh_5391',['scan.h',['../scan_8h.html',1,'']]],
-  ['scan_5fenum_2eh_5392',['scan_enum.h',['../scan__enum_8h.html',1,'']]],
-  ['stl_5freader_2eh_5393',['stl_reader.h',['../stl__reader_8h.html',1,'']]]
+  ['scan_2eh_5397',['scan.h',['../scan_8h.html',1,'']]],
+  ['scan_5fenum_2eh_5398',['scan_enum.h',['../scan__enum_8h.html',1,'']]],
+  ['stl_5freader_2eh_5399',['stl_reader.h',['../stl__reader_8h.html',1,'']]]
 ];
