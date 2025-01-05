b0696fe91d1356cd22b318878ec842ef74d861b2
brussig-tud <39052884+brussig-tud@users.noreply.github.com>
2025-01-05 14:01:41 +0100

    Maptiles plugin: fix types and start WIP on improved interfacing

--- plugins/maptiles_plugin/maptiles_interfacer.h
+++ plugins/maptiles_plugin/maptiles_interfacer.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <cgv/render/context.h>
+
+
 class maptiles;
 
 class maptiles_interfacer
@@ -10,4 +13,5 @@ private:
 public:
 	static void set_pointer(maptiles* _ptr) { ptr = _ptr; }
 	static maptiles* get_pointer() { return ptr; }
+	static void force_draw (cgv::render::context &ctx);
 };
