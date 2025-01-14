ff5f1d52a8e6654c2df6c2be9c53275663c00b70
shalin_ <shalindahiya@gmail.com>
2025-01-14 11:16:31 +0100

    move more functions from spline tube fragment shader to spline_tube_tools.glsl

--- plugins/spline_tubes/glsl/spline_tube_tools.glsl
+++ plugins/spline_tubes/glsl/spline_tube_tools.glsl
@@ -5,11 +5,20 @@
 // Shader module interface definition
 //
 
-///***** begin interface of curve_tools.glsl **********************************/
-// --- link dependencies ---------------
-/* transform.glsl */
+///***** begin interface of spline_tube_tools.glsl **********************************/
+
+// --- defines -----------------------
+#define POS_INF 3e+37
 
 // --- functions -----------------------
+
+float Pow2(float x);
+float Pow3(float x);
+// Raises a quadratic polynomial to the power of 2
+void Pow2(in float c[3], out float o_c[5]);
+void Sub(in float a[5], in float b[5], out float o_c[5]);
+
+void SplinePointsToPolyCoeffs(float p0, float h, float p1, out float o_c[3]);
 
 float EvalPoly(float x, float c0, float c1, float c2, float c3, float c4, float c5, float c6); 
 float EvalPoly(float x, float c0, float c1, float c2, float c3, float c4, float c5);
@@ -27,13 +36,72 @@ float EvalPolyD1(float x, float c[5]);
 float EvalPolyD2(float x, float c[5]);
 float EvalPolyD3(float x, float c[5]);
 
+// Evaluate a quadratic spline(and it's first, second and third dervatives) at parameter t
+float qSplineIDistEval(float t, float curveX[3], float polyB_C[5]);
+float qSplineD1Eval(float t, float curveX[3], float polyB_C[5]);
+float qSplineD2Eval(float t, float curveX[3], float polyB_C[5]);
+float qSplineD3Eval(float t, float polyB_C[5]);
+
+// Bisection method for finding roots for 'f(t)' and it's first, second, and third derivatives
+float qSplineIDistEval_BinRootFinder_Eval(float n, float p, float curveX[3], float polyB_C[5], int max_iter);
+float qSplineD1_BinRootFinder_Eval(float n, float p, float curveX[3], float polyB_C[5], int max_iter);
+float qSplineD2_BinRootFinder_Eval(float n, float p, float curveX[3], float polyB_C[5], int max_iter);
+float qSplineD3_BinRootFinder_Eval(float n, float p, float polyB_C[5], int max_iter);
+
+// Bisection method for finding roots of a polynomial
+// c: coefficients of the polynomial
+// n: parameter for which function evaluated to a negative value
+// p: parameter for which function evaluated to a positive value
+// max_iter: maximum number of iterations
 float BisectionMethodPolyD0(float c[5], float n, float p, int max_iter);
 float BisectionMethodPolyD1(float c[5], float n, float p, int max_iter);
 
+// Newton's method for finding roots of a polynomial
+// c: coefficients of the polynomial
+// x: initial guess
+// max_iter: maximum number of iterations
 float NewtonsMethodPolyD0(float c[5], float x, int max_iter);
 float NewtonsMethodPolyD1(float c[5], float x, int max_iter);
+// Similar to above, but with an additional epsilon parameter
+// epsilon: maximum error
+float NewtonsMethodPolyD0(float c[5], float x, float epsilon, int max_iter);
+float NewtonsMethodPolyD1(float c[5], float x, float epsilon, int max_iter);
 ///***** end interface of curve_tools.glsl ************************************/
 
+
+
+//****************************************************************************/
+//******** Function Definitions***********************************************/
+//****************************************************************************/
+float Pow2(float x) { 
+	return x * x; 
+}
+
+float Pow3(float x) { 
+	return x * x * x; 
+}
+
+void Pow2(in float c[3], out float o_c[5]) {
+	o_c[0] = c[0] * c[0]; 
+	o_c[1] =  2.0 * c[0] * c[1];
+	o_c[2] = c[1] * c[1] +  2.0 * c[0] * c[2];
+	o_c[3] =  2.0 * c[2] * c[1];
+	o_c[4] = c[2] * c[2];
+}
+
+void Sub(in float a[5], in float b[5], out float o_c[5]) {
+	o_c[0] = a[0] - b[0];
+	o_c[1] = a[1] - b[1];
+	o_c[2] = a[2] - b[2];
+	o_c[3] = a[3] - b[3];
+	o_c[4] = a[4] - b[4];
+}
+
+void SplinePointsToPolyCoeffs(float p0, float h, float p1, out float o_c[3]) {
+	o_c[0] = p0;
+	o_c[1] = -2.0 * p0 + 2.0 * h;
+	o_c[2] =   p0 + p1 - 2.0 * h;
+}
 
 float EvalPoly(float x, float c0, float c1, float c2, float c3, float c4, float c5, float c6) { return x * (x * (x * (x * (x * (x * c6 + c5) + c4) + c3) + c2) + c1) + c0; }
 float EvalPoly(float x, float c0, float c1, float c2, float c3, float c4, float c5) { return EvalPoly(x, c0,c1,c2,c3,c4,c5,0.0); }
@@ -59,6 +127,95 @@ float EvalPolyD0(float x, float c[5]) { return EvalPoly(x, c[0], c[1], c[2], c[3
 float EvalPolyD1(float x, float c[5]) { return EvalPoly(x, c[1], c[2] + c[2], c[3] + c[3] + c[3], c[4] + c[4] + c[4] + c[4]); }
 float EvalPolyD2(float x, float c[5]) { return EvalPoly(x, c[2] + c[2], c[3] * 6.0, c[4] * 12.0); }
 float EvalPolyD3(float x, float c[5]) { return EvalPoly(x, c[3] * 6.0, c[4] * 24.0); }
+
+float qSplineIDistEval(float t, float curveX[3], float polyB_C[5]) {		
+	float term  = EvalPolyD0(t, curveX);
+	float discr = EvalPolyD0(t, polyB_C);
+	if(discr < 0.0) return POS_INF;
+	else return term - sqrt(discr);
+}
+
+float qSplineD1Eval(float t, float curveX[3], float polyB_C[5]) {	 		
+	float f1D1 = EvalPolyD1(t, curveX);	
+	float f2D0 = EvalPolyD0(t, polyB_C);
+	float f2D1 = EvalPolyD1(t, polyB_C);
+	return f1D1 - f2D1 * 0.5 * inversesqrt(max(0.0, f2D0));
+}
+
+float qSplineD2Eval(float t, float curveX[3], float polyB_C[5]) {		
+	float f1D1 = EvalPolyD1(t, curveX);
+	float f1D2 = EvalPolyD2(t, curveX);
+	float f2D0 = EvalPolyD0(t, polyB_C);
+	float f2D1 = EvalPolyD1(t, polyB_C);
+	float f2D2 = EvalPolyD2(t, polyB_C);
+	f2D0 = max(0.0, f2D0);
+	return (Pow2(f2D1) / f2D0 * 0.25 - f2D2 * 0.5) * inversesqrt(f2D0) + f1D2;
+}
+
+float qSplineD3Eval(float t, float polyB_C[5]) {
+	float f2D0 = EvalPolyD0(t, polyB_C);
+	float f2D1 = EvalPolyD1(t, polyB_C);
+	float f2D2 = EvalPolyD2(t, polyB_C);
+	float f2D3 = EvalPolyD3(t, polyB_C);
+	f2D0 = max(0.0, f2D0);
+	return (-3.0 * Pow3(f2D1) + 6.0 * f2D0 * f2D1 * f2D2 - 4.0 * Pow2(f2D0) * f2D3) / Pow2(f2D0) * inversesqrt(f2D0);
+}
+
+float qSplineIDistEval_BinRootFinder_Eval(float n, float p, float curveX[3], float polyB_C[5], int max_iter) {
+	if(qSplineIDistEval(n, curveX, polyB_C) > 0.0) return n;
+	if(qSplineIDistEval(p, curveX, polyB_C) < 0.0) return p;
+
+	for(int i = 0; i < max_iter; ++i) {
+		float m = (n + p) * 0.5;
+		float f = qSplineIDistEval(m, curveX, polyB_C);
+		if(f < 0.0) n = m;
+		else p = m;
+	}
+
+	return (n + p) * 0.5;
+}
+
+float qSplineD1_BinRootFinder_Eval(float n, float p, float curveX[3], float polyB_C[5], int max_iter) {
+	if(qSplineD1Eval(n, curveX, polyB_C) > 0.0) return n;
+    if(qSplineD1Eval(p, curveX, polyB_C) < 0.0) return p;
+
+    for(int i = 0; i < max_iter; ++i) {
+        float m = (n + p) * 0.5;
+        float f = qSplineD1Eval(m, curveX, polyB_C);
+        if(f < 0.0) n = m;
+        else p = m;
+    }
+
+    return (n + p) * 0.5;
+}
+
+float qSplineD2_BinRootFinder_Eval(float n, float p, float curveX[3], float polyB_C[5], int max_iter) {
+	if(qSplineD2Eval(n, curveX, polyB_C) > 0.0) return n;
+	if(qSplineD2Eval(p, curveX, polyB_C) < 0.0) return p;
+
+	for(int i = 0; i < max_iter; ++i) {
+		float m = (n + p) * 0.5;
+		float f = qSplineD2Eval(m, curveX, polyB_C);
+		if(f < 0.0) n = m;
+		else p = m;
+	}
+
+	return (n + p) * 0.5;
+}
+
+float qSplineD3_BinRootFinder_Eval(float n, float p, float polyB_C[5], int max_iter) {
+	if(qSplineD3Eval(n, polyB_C) > 0.0) return n;
+	if(qSplineD3Eval(p, polyB_C) < 0.0) return p;
+
+	for(int i = 0; i < max_iter; ++i) {
+		float m = (n + p) * 0.5;
+		float f = qSplineD3Eval(m, polyB_C);
+		if(f < 0.0) n = m;
+		else p = m;
+	}
+
+	return (n + p) * 0.5;
+}
 
 float BisectionMethodPolyD0(float c[5], float n, float p, int max_iter) {
     for(int j = 0; j < max_iter; ++j) {                                           
@@ -99,3 +256,26 @@ float NewtonsMethodPolyD1(float c[5], float x, int max_iter) {
     }
     return x0;
 }
+
+float NewtonsMethodPolyD0(float c[5], float x, float epsilon, int max_iter) {
+    float x0 = x;
+    for (int i = 0; i < max_iter; i++) {
+        float f = EvalPolyD0(x0, c);
+        if (abs(f) < epsilon) break;
+        float df = EvalPolyD1(x0, c);
+        x0 -= f / df;
+    }
+    return x0;
+}
+
+float NewtonsMethodPolyD1(float c[5], float x, float epsilon, int max_iter) {
+    float x0 = x;
+    for (int i = 0; i < max_iter; i++) {
+        float f = EvalPolyD1(x0, c);
+        if (abs(f) < epsilon) break;
+        float df = EvalPolyD2(x0, c);
+        x0 -= f / df;
+    }
+    return x0;
+}
+
