8277d7f7c2e5813decdfe162cdab25edb5c040b8
shalin_ <shalindahiya@gmail.com>
2025-01-25 12:51:52 +0100

    add move sematics to RasterTileData and fix memory leak

--- libs/maptiles/src/RasterTileData.cpp
+++ libs/maptiles/src/RasterTileData.cpp
@@ -1,17 +1,26 @@
 #include "RasterTileData.h"
 #include "utils.h"
 
-RasterTileData::RasterTileData()
-	: m_zoom(0), m_x(0), m_y(0), m_height(0), m_width(0), m_image(nullptr)
-{
-}
-
 RasterTileData::RasterTileData(unsigned char* image, int height, int width, int zoom, int x, int y)
 	: m_zoom(zoom), m_x(x), m_y(y), m_height(height), m_width(width), m_image(image)
 {
 }
 
+RasterTileData::RasterTileData(RasterTileData&& other) noexcept 
+{
+	m_zoom = other.m_zoom;
+	m_x = other.m_x;
+	m_y = other.m_y;
+	m_height = other.m_height;
+	m_width = other.m_width;
+	m_image = other.m_image;
+	other.m_image = nullptr;
+}
+
 RasterTileData::~RasterTileData() 
 {
-	
+	if (m_image != nullptr) {
+		delete[] m_image;
+		m_image = nullptr;
+	}
 }
