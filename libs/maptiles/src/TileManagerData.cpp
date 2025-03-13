4ba48aeb66f3358bf939deec1b6252e04749c18b
shalin_ <shalindahiya@gmail.com>
2025-03-13 09:10:41 +0100

    improve thread safety of TileManagerData

--- libs/maptiles/src/TileManagerData.cpp
+++ libs/maptiles/src/TileManagerData.cpp
@@ -33,10 +33,11 @@ RasterTileData& TileManagerData::GetRasterTile(int zoom, int x, int y)
 {
 	RasterTileIndex index = { (unsigned)zoom, x, y };
 	
-
-	if (m_RasterTileCache.find(index) != m_RasterTileCache.end())
 	{
-		return m_RasterTileCache.at(index);
+		std::lock_guard<std::mutex> lockActive(m_MutexRasterTiles);
+		if (m_RasterTileCache.find(index) != m_RasterTileCache.end()) {
+			return m_RasterTileCache.at(index);
+		}
 	}
 
 	OSMRasterTileLoader loader(zoom, x, y);
@@ -69,19 +70,22 @@ Tile3DData& TileManagerData::GetTile3D(double lat, double lon)
 {
 	Tile3DIndex index = { lat, lon };
 	
-
-	if (m_Tile3DCache.find(index) != m_Tile3DCache.end())
 	{
-		Tile3DData& tile = m_Tile3DCache.at(index);
-		if (tile.ref_lat == m_config->ReferencePoint.lat && tile.ref_lon == m_config->ReferencePoint.lon)
+		std::lock_guard<std::mutex> lockActive(m_MutexTile3Ds);
+		if (m_Tile3DCache.find(index) != m_Tile3DCache.end())
 		{
+			Tile3DData& tile = m_Tile3DCache.at(index);
+			if (tile.ref_lat == m_config->ReferencePoint.lat && tile.ref_lon == m_config->ReferencePoint.lon)
+			{
+				return tile;
+			}
+			tile.ConvertTo3DCoordinates(m_config->ReferencePoint.lat, m_config->ReferencePoint.lon);
+			std::cout << "Changing ref point for tile3D to (" << m_config->ReferencePoint.lat << ", "
+					  << m_config->ReferencePoint.lon << ")\n";
 			return tile;
 		}
-		tile.ConvertTo3DCoordinates(m_config->ReferencePoint.lat, m_config->ReferencePoint.lon);
-		std::cout << "Changing ref point for tile3D to (" << m_config->ReferencePoint.lat << ", "
-				  << m_config->ReferencePoint.lon << ")\n";
-		return tile;
 	}
+
 
 	double size = m_config->Tile3DSize;
 	OSMDataLoader loader(lat, lon, lat + size, lon + size);
@@ -120,22 +124,28 @@ void TileManagerData::TrimCache()
 	std::set<RasterTileIndex> to_be_removed_raster_tile;
 	std::set<Tile3DIndex> to_be_removed_tile3D;
 
-	for (const auto& element : m_RasterTileCache) {
-		// Get the position of the tile in degrees
-		double lat = tiley2lat(element.first.y, element.first.zoom);
-		double lon = tilex2long(element.first.x, element.first.zoom);
-		if (std::abs(lat - m_config->ReferencePoint.lat) > m_config->RasterTileRenderCacheTrimDistance ||
-			std::abs(lon - m_config->ReferencePoint.lon) > m_config->RasterTileRenderCacheTrimDistance)
-			to_be_removed_raster_tile.insert(element.first);
+	{
+		std::lock_guard<std::mutex> lockRasterTiles(m_MutexRasterTiles);
+		for (const auto& element : m_RasterTileCache) {
+			// Get the position of the tile in degrees
+			double lat = tiley2lat(element.first.y, element.first.zoom);
+			double lon = tilex2long(element.first.x, element.first.zoom);
+			if (std::abs(lat - m_config->ReferencePoint.lat) > m_config->RasterTileRenderCacheTrimDistance ||
+				std::abs(lon - m_config->ReferencePoint.lon) > m_config->RasterTileRenderCacheTrimDistance)
+				to_be_removed_raster_tile.insert(element.first);
+		}
 	}
 
-	for (const auto& element : m_Tile3DCache) {
-		// Get the position of the tile in degrees
-		double lat = element.first.lat;
-		double lon = element.first.lon;
-		if (std::abs(lat - m_config->ReferencePoint.lat) > m_config->Tile3DRenderCacheTrimDistance ||
-			std::abs(lon - m_config->ReferencePoint.lon) > m_config->Tile3DRenderCacheTrimDistance)
-			to_be_removed_tile3D.insert(element.first);
+	{
+		std::lock_guard<std::mutex> lockTile3D(m_MutexTile3Ds);
+		for (const auto& element : m_Tile3DCache) {
+			// Get the position of the tile in degrees
+			double lat = element.first.lat;
+			double lon = element.first.lon;
+			if (std::abs(lat - m_config->ReferencePoint.lat) > m_config->Tile3DRenderCacheTrimDistance ||
+				std::abs(lon - m_config->ReferencePoint.lon) > m_config->Tile3DRenderCacheTrimDistance)
+				to_be_removed_tile3D.insert(element.first);
+		}
 	}
 
 	std::lock_guard<std::mutex> lockRasterTiles(m_MutexRasterTiles);
