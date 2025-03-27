a9e7509ae1570abadfdd07666c7c401026450ceb
sgumhold <stefan.gumhold@tu-dresden.de>
2025-03-28 00:33:29 +0100

    extended clipboard functions (windows only)

--- cgv/os/clipboard.h
+++ cgv/os/clipboard.h
@@ -1,5 +1,6 @@
 #pragma once
-
+#include <string>
+#include <vector>
 #include "lib_begin.h"
 
 namespace cgv {
@@ -7,9 +8,12 @@ namespace cgv {
 
 /// copies the given text to the clipboard
 extern CGV_API bool copy_text_to_clipboard(const char* text);
-
 /// copies an image of width w and height h given as packed rgb pixels of type unsigned char to the clipboard
 extern CGV_API bool copy_rgb_image_to_clipboard(int w, int h, const unsigned char* image_buffer);
+/// if clipboard contains text (return true), copy text to \c text and optionally clear clipboard
+extern CGV_API bool get_text_from_clipboard(std::string& text, bool clear_clipboard = false);
+/// if clipboard contains image (return true), copy image dims to \c w, \c h and pixel data in rgb24 format to \c data; optionally clear clipboard
+extern CGV_API bool get_rgb_image_from_clipboard(int& w, int& h, std::vector<uint8_t>& data, bool clear_clipboard = false);
 
 	}
 }
