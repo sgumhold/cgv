ade3bfdc902e9ec00e6e6d413d8616fa0c327137
1DaveBig <david.gross.de@gmail.com>
2025-04-14 14:18:10 +0200

    Move uniform_argument to own header

--- libs/cgv_gpgpu/compute_kernel.cxx
+++ libs/cgv_gpgpu/compute_kernel.cxx
@@ -6,15 +6,11 @@ namespace cgv {
 namespace gpgpu {
 
 bool compute_kernel::init(cgv::render::context& ctx, const std::string& name, const cgv::render::shader_compile_options& config, const std::string& where) {
-	bool res = cgv::render::shader_library::load(ctx, _prog, name, config, where);
-	if(res)
-		_uniforms = get_program_uniforms(ctx, _prog);
-	return res;
+	return cgv::render::shader_library::load(ctx, _prog, name, config, where);
 }
 
 void compute_kernel::destruct(const cgv::render::context& ctx) {
 	_prog.destruct(ctx);
-	_uniforms.clear();
 }
 
 bool compute_kernel::enable(cgv::render::context& ctx) {
@@ -30,13 +26,8 @@ void compute_kernel::set_arguments(cgv::render::context& ctx, const uniform_argu
 	if(!was_enabled)
 		_prog.enable(ctx);
 
-	for(const uniform_argument& arg : arguments) {
-		auto it = _uniforms.find(arg.name);
-		if(it != uniforms.end()) {
-			int loc = it->second;
-			_prog.set_uniform(ctx, loc, arg.desc, arg.addr);
-		}
-	}
+	for(const uniform_argument& arg : arguments)
+		_prog.set_uniform(ctx, _prog.get_uniform_location(ctx, arg.name), arg.desc, arg.addr);
 
 	if(!was_enabled)
 		_prog.disable(ctx);
