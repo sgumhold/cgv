664bbb8c1127439f53417c3a61927cdaedc85e0b
1DaveBig <david.gross.de@gmail.com>
2025-04-15 13:59:22 +0200

    Refactor cgv_gpgpu mipmap algorithm

--- libs/cgv_gpgpu/mipmap.h
+++ libs/cgv_gpgpu/mipmap.h
@@ -9,29 +9,22 @@
 namespace cgv {
 namespace gpgpu {
 
-/** GPU compute shader implementation for computing texture mipmaps. */
+/// GPU compute shader implementation for computing texture mipmaps.
 class CGV_API mipmap : public algorithm {
-protected:
-	unsigned group_size = 0;
-	
-	/// shader programs
-	cgv::render::shader_program mipmap1d_prog;
-	cgv::render::shader_program mipmap2d_prog;
-	cgv::render::shader_program mipmap3d_prog;
-	
-	bool load_shader_programs(cgv::render::context& ctx);
-
 public:
-	void destruct(const cgv::render::context& ctx);
-
-	bool init(cgv::render::context& ctx, size_t count) { return init(ctx); }
+	mipmap();
 
 	bool init(cgv::render::context& ctx);
 
-	bool execute(cgv::render::context& ctx, cgv::render::texture& source_texture);
+	bool dispatch(cgv::render::context& ctx, cgv::render::texture& texture);
+
+private:
+	compute_kernel kernel_1d;
+	compute_kernel kernel_2d;
+	compute_kernel kernel_3d;
 };
 
-}
-}
+} // namespace gpgpu
+} // namespace cgv
 
 #include <cgv/config/lib_end.h>
