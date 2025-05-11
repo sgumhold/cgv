1208453cedc37b27becd073baf4a917e95e07f1a
1DaveBig <david.gross.de@gmail.com>
2025-05-11 11:41:43 +0200

    Refactor gpgpu radix sort implementations

--- libs/cgv_gpgpu/radix_sort.h
+++ libs/cgv_gpgpu/radix_sort.h
@@ -1,42 +1,46 @@
 #pragma once
 
-#include "sort.h"
+#include "algorithm.h"
 
 #include "lib_begin.h"
 
 namespace cgv {
 namespace gpgpu {
 
-class CGV_API radix_sort : public sort {
+class CGV_API radix_sort : public algorithm {
 public:
-	radix_sort();
+	enum class Order {
+		kAscending,
+		kDescending
+	};
 
-	void destruct(const cgv::render::context& ctx);
+	radix_sort(const std::string& type_name, size_t radix);
+
+	bool init(cgv::render::context& ctx, const sl::data_type& key_type, Order order, size_t size);
+	bool init(cgv::render::context& ctx, const sl::data_type& key_type, const sl::data_type& value_type, Order order, size_t size);
+	
+	void dispatch(cgv::render::context& ctx, const cgv::render::vertex_buffer& keys_buffer);
+	void dispatch(cgv::render::context& ctx, const cgv::render::vertex_buffer& keys_buffer, const cgv::render::vertex_buffer& values_buffer);
+
+protected:
+	virtual bool v_init(cgv::render::context& ctx, cgv::render::shader_compile_options& config) = 0;
+
+	virtual void v_dispatch(cgv::render::context& ctx, const cgv::render::vertex_buffer* keys_buffer, const cgv::render::vertex_buffer* values_buffer) = 0;
+
+	// TODO: Make configurable.
+	const uint32_t _key_width = 32;
+	uint32_t _radix = 2;
+	uint32_t _radix_mask = 1;
+	uint32_t _radix_log = 1;
+	uint32_t _radix_passes = _key_width;
+
+	sl::data_type _key_type;
+	sl::data_type _value_type;
+	Order _order = Order::kAscending;
+	uint32_t _num_keys = 0;
 
 private:
-	bool v_init(cgv::render::context& ctx) override;
-
-	void v_dispatch(cgv::render::context& ctx, const cgv::render::vertex_buffer* keys_buffer, const cgv::render::vertex_buffer* values_buffer) override;
-
-	const uint32_t _radix = 4;
-	const uint32_t _radix_passes = 16;
-	const uint32_t _group_size = 64;
-	const uint32_t _block_size = 4 * _group_size;
-
-	uint32_t _num_block_sums = 0;
-	uint32_t _num_groups = 0;
-	uint32_t _num_scan_groups = 0;
-
-	compute_kernel _scan_local_kernel;
-	compute_kernel _scan_global_kernel;
-	compute_kernel _scatter_kernel;
-
-	/// GPU buffers
-	cgv::render::vertex_buffer _keys_out_buffer;
-	cgv::render::vertex_buffer _values_out_buffer;
-	cgv::render::vertex_buffer _prefix_sums_buffer;
-	cgv::render::vertex_buffer _block_sums_buffer;
-	cgv::render::vertex_buffer _last_sum_buffer;
+	bool is_type_supported(const sl::data_type& type) const;	
 };
 
 } // namespace gpgpu
---
+++ libs/cgv_gpgpu/radix_sort_4x.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "radix_sort.h"
+
+#include "lib_begin.h"
+
+namespace cgv {
+namespace gpgpu {
+
+class CGV_API radix_sort_4x : public radix_sort {
+public:
+	radix_sort_4x();
+
+	void destruct(const cgv::render::context& ctx);
+
+private:
+	bool v_init(cgv::render::context& ctx, cgv::render::shader_compile_options& config) override;
+
+	void v_dispatch(cgv::render::context& ctx, const cgv::render::vertex_buffer* keys_buffer, const cgv::render::vertex_buffer* values_buffer) override;
+
+	//const uint32_t _radix = 4;
+	//const uint32_t _radix_passes = 16;
+	const uint32_t _group_size = 64;
+	const uint32_t _block_size = 4 * _group_size;
+
+	uint32_t _num_block_sums = 0;
+	uint32_t _num_groups = 0;
+	uint32_t _num_scan_groups = 0;
+
+	compute_kernel _scan_local_kernel;
+	compute_kernel _scan_global_kernel;
+	compute_kernel _scatter_kernel;
+
+	/// GPU buffers
+	cgv::render::vertex_buffer _keys_out_buffer;
+	cgv::render::vertex_buffer _values_out_buffer;
+	cgv::render::vertex_buffer _prefix_sums_buffer;
+	cgv::render::vertex_buffer _block_sums_buffer;
+	cgv::render::vertex_buffer _last_sum_buffer;
+};
+
+} // namespace gpgpu
+} // namespace cgv
+
+#include <cgv/config/lib_end.h>
