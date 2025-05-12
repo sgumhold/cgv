a78e0f9b61cb861af4ae18c184853fc911a07f54
1DaveBig <david.gross.de@gmail.com>
2025-05-12 14:36:21 +0200

    Expose success of cgv::gpgpu::ensure_buffer through boolean return type

--- libs/cgv_gpgpu/utils.cxx
+++ libs/cgv_gpgpu/utils.cxx
@@ -3,13 +3,13 @@
 namespace cgv {
 namespace gpgpu {
 
-void ensure_buffer(const cgv::render::context& ctx, cgv::render::vertex_buffer& buffer, size_t size, cgv::render::VertexBufferType type, cgv::render::VertexBufferUsage usage) {
+bool ensure_buffer(const cgv::render::context& ctx, cgv::render::vertex_buffer& buffer, size_t size_in_bytes, cgv::render::VertexBufferType type, cgv::render::VertexBufferUsage usage) {
 	if(buffer.is_created() && buffer.type == type && buffer.usage == usage) {
-		buffer.resize(ctx, size);
+		return buffer.resize(ctx, size_in_bytes);
 	} else {
 		buffer.destruct(ctx);
 		buffer = cgv::render::vertex_buffer(type, usage);
-		buffer.create(ctx, size);
+		return buffer.create(ctx, size_in_bytes);
 	}
 }
 
