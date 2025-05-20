6c6ff46d9dbafc12de6f1607e9425606afe75083
1DaveBig <david.gross.de@gmail.com>
2025-05-20 16:09:34 +0200

    Remove automatic shader program enablement in compute_kernel

--- libs/cgv_gpgpu/compute_kernel.cxx
+++ libs/cgv_gpgpu/compute_kernel.cxx
@@ -22,21 +22,12 @@ bool compute_kernel::disable(cgv::render::context& ctx) {
 }
 
 void compute_kernel::set_arguments(cgv::render::context& ctx, const argument_bindings& arguments) {
-	bool was_enabled = false;
-	if(_prog.is_enabled())
-		was_enabled = true;
-	else
-		_prog.enable(ctx);
-
 	for(size_t i = 0; i < arguments.get_uniform_count(); ++i) {
 		const uniform_binding* binding = arguments.get_uniform(i);
 		int loc = _prog.get_uniform_location(ctx, binding->_name);
 		if(loc > -1)
 			_prog.set_uniform(ctx, loc, binding->_desc, binding->_addr);
 	}
-
-	if(!was_enabled)
-		_prog.disable(ctx);
 }
 
 } // namespace gpgpu
