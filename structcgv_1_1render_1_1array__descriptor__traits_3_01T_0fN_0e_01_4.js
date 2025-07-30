c63261157f242d87d49142b8981f5ca6cec714d8
sgumhold <stefan.gumhold@tu-dresden.de>
2025-07-30 13:02:02 +0000

    Deploying to gh-pages from @ sgumhold/cgv@849ea57a5263a3df1c9f0398341065452571dad2 🚀

---
+++ structcgv_1_1render_1_1array__descriptor__traits_3_01T_0fN_0e_01_4.js
@@ -0,0 +1,8 @@
+var structcgv_1_1render_1_1array__descriptor__traits_3_01T_0fN_0e_01_4 =
+[
+    [ "get_address", "structcgv_1_1render_1_1array__descriptor__traits_3_01T_0fN_0e_01_4.html#acfc596fefe0629a040519df3950d224c", null ],
+    [ "get_address", "structcgv_1_1render_1_1array__descriptor__traits_3_01T_0fN_0e_01_4.html#a85f021eff1ed81ede2d5628b387142d3", null ],
+    [ "get_nr_elements", "structcgv_1_1render_1_1array__descriptor__traits_3_01T_0fN_0e_01_4.html#ab2ccf0961c67a78dec6dda53fe34eb61", null ],
+    [ "get_size", "structcgv_1_1render_1_1array__descriptor__traits_3_01T_0fN_0e_01_4.html#ab307f6cb99b9b3536a2dfbc4cf92e274", null ],
+    [ "get_type_descriptor", "structcgv_1_1render_1_1array__descriptor__traits_3_01T_0fN_0e_01_4.html#aca2e4e882e2f6b82898effc775cfc4d8", null ]
+];
