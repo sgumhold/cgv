sgumhold/cgv

commit 7e0607ce8cec4fcafc1f741b9da4af7b829f7c37

improved context creation and core profile support

libs/cgv_gl/gl/gl_context.cxx
@@ -35,7 +35,57 @@ using namespace cgv::media::illum;
 namespace cgv {
 	namespace render {
 		namespace gl {
-			
+
+std::vector<int> get_context_creation_attrib_list(cgv::render::context_config& cc)
+{
+#ifdef _WIN32
+	const int CONTEXT_FLAGS = WGL_CONTEXT_FLAGS_ARB;
+	const int CONTEXT_FORWARD_COMPATIBLE_BIT = WGL_CONTEXT_FORWARD_COMPATIBLE_BIT_ARB;
+	const int CONTEXT_DEBUG_BIT = WGL_CONTEXT_DEBUG_BIT_ARB;
+	const int CONTEXT_MAJOR_VERSION = WGL_CONTEXT_MAJOR_VERSION_ARB;
+	const int CONTEXT_MINOR_VERSION = WGL_CONTEXT_MINOR_VERSION_ARB;
+	const int CONTEXT_PROFILE_MASK = WGL_CONTEXT_PROFILE_MASK_ARB;
+	const int CONTEXT_CORE_PROFILE_BIT = WGL_CONTEXT_CORE_PROFILE_BIT_ARB;
+	const int CONTEXT_COMPATIBILITY_PROFILE_BIT = WGL_CONTEXT_COMPATIBILITY_PROFILE_BIT_ARB;
+#else
+	const int CONTEXT_FLAGS = XGL_CONTEXT_FLAGS_ARB;
+	const int CONTEXT_FORWARD_COMPATIBLE_BIT = XGL_CONTEXT_FORWARD_COMPATIBLE_BIT_ARB;
+	const int CONTEXT_DEBUG_BIT = XGL_CONTEXT_DEBUG_BIT_ARB;
+	const int CONTEXT_MAJOR_VERSION = XGL_CONTEXT_MAJOR_VERSION_ARB;
+	const int CONTEXT_MINOR_VERSION = XGL_CONTEXT_MINOR_VERSION_ARB;
+	const int CONTEXT_PROFILE_MASK = XGL_CONTEXT_PROFILE_MASK_ARB;
+	const int CONTEXT_CORE_PROFILE_BIT = XGL_CONTEXT_CORE_PROFILE_BIT_ARB;
+	const int CONTEXT_COMPATIBILITY_PROFILE_BIT = XGL_CONTEXT_COMPATIBILITY_PROFILE_BIT_ARB;
+#endif
+	std::vector<int> attrib_list;
+	int version = 0;
+	if (cc.version_major < 1)
+		version = 46;
+	else
+		version = 10 * cc.version_major + (cc.version_minor >= 0 ? cc.version_minor : 0);
+	if ((version > 30 && cc.forward_compatible) || cc.debug) {
+		attrib_list.push_back(CONTEXT_FLAGS);
+		attrib_list.push_back(
+			(cc.forward_compatible ? CONTEXT_FORWARD_COMPATIBLE_BIT : 0) +
+			(cc.debug ? CONTEXT_DEBUG_BIT : 0));
+	}
+	if (cc.version_major > 0) {
+		attrib_list.push_back(CONTEXT_MAJOR_VERSION);
+		attrib_list.push_back(cc.version_major);
+	}
+	if (cc.version_minor >= 0) {
+		attrib_list.push_back(CONTEXT_MINOR_VERSION);
+		attrib_list.push_back(cc.version_minor);
+	}
+	if (version > 31) {
+		attrib_list.push_back(CONTEXT_PROFILE_MASK);
+		attrib_list.push_back(
+			cc.core_profile ? CONTEXT_CORE_PROFILE_BIT : CONTEXT_COMPATIBILITY_PROFILE_BIT);
+	}
+	attrib_list.push_back(0);
+	return attrib_list;
+}
+
 GLenum map_to_gl(PrimitiveType primitive_type)
 {
 	static const GLenum gl_primitive_type[] = {
@@ -271,6 +321,22 @@ void gl_set_material(const cgv::media::illum::phong_material& mat, MaterialSide 
 	glMaterialf(side, GL_SHININESS, mat.get_shininess());
 }
 
+/// return info font size
+float gl_context::get_info_font_size() const
+{
+	return info_font_size;
+}
+/// return info font face and ensure that it is created
+cgv::media::font::font_face_ptr gl_context::get_info_font_face() const
+{
+	if (info_font_face.empty()) {
+		font_ptr info_font = default_font(true);
+		if (!info_font.empty())
+			info_font_face = info_font->get_font_face(FFA_REGULAR);
+	}
+	return info_font_face;
+}
+
 /// construct gl_context and attach signals
 gl_context::gl_context()
 {
@@ -364,14 +430,9 @@ bool gl_context::configure_gl()
 	if (version_major >= 3) {
 		GLint context_flags;
 		glGetIntegerv(GL_CONTEXT_FLAGS, &context_flags);
-#ifdef WIN32
 		// weird behavior under windows or just nvidia or just my laptop (Stefan)??
-		debug = (context_flags & WGL_CONTEXT_FORWARD_COMPATIBLE_BIT_ARB) != 0;
-		forward_compatible = (context_flags & WGL_CONTEXT_DEBUG_BIT_ARB) != 0;
-#else
-		debug = (context_flags & GLX_CONTEXT_DEBUG_BIT_ARB) != 0;
-		forward_compatible = (context_flags & GLX_CONTEXT_FORWARD_COMPATIBLE_BIT_ARB) != 0;
-#endif
+		debug = (context_flags & GL_CONTEXT_FLAG_DEBUG_BIT) != 0;
+		forward_compatible = (context_flags & GL_CONTEXT_FLAG_FORWARD_COMPATIBLE_BIT) != 0;
 	}
 	else {
 		debug = false;
@@ -381,15 +442,22 @@ bool gl_context::configure_gl()
 	if (version >= 32) {
 		GLint context_profile;
 		glGetIntegerv(GL_CONTEXT_PROFILE_MASK, &context_profile);
-#ifdef WIN32
-		core_profile = (context_profile & WGL_CONTEXT_CORE_PROFILE_BIT_ARB) != 0;
-#else
-		core_profile = (context_profile & GLX_CONTEXT_CORE_PROFILE_BIT_ARB) != 0;
+#ifdef _DEBUG
+		if (context_profile != 0) {
+			if (core_profile != ((context_profile & GL_CONTEXT_COMPATIBILITY_PROFILE_BIT) == 0)) {
+				if (core_profile)
+					std::cerr << "WARNING: Ignoring that GL_CONTEXT_PROFILE_MASK yields compatibility profile for context created as core." << std::endl;
+				else
+					std::cerr << "WARNING: Ignoring that GL_CONTEXT_PROFILE_MASK does not yield compatibility profile for context created as such." << std::endl;
+			}
+		}
+		else {
+			std::cerr << "WARNING: Ignoring that GL_CONTEXT_PROFILE_MASK yields zero." << std::endl;
+		}
 #endif
 	}
 	else
 		core_profile = false;
-
 	const GLubyte* vendor_c_string = glGetString(GL_VENDOR);
 	std::string vendor_string(reinterpret_cast<const char*>(vendor_c_string));
 	vendor_string = cgv::utils::to_upper(vendor_string);
@@ -402,7 +470,7 @@ bool gl_context::configure_gl()
 		gpu_vendor = GPU_VENDOR_AMD;
 	
 #ifdef _DEBUG
-	std::cout << "OpenGL version " << version_major << "." << version_minor << (core_profile?" (core)":"") << (debug?" (debug)":"") << (forward_compatible?" (forward_compatible)":"") << std::endl;
+	std::cout << "OpenGL version " << version_major << "." << version_minor << (core_profile?" (core)":" (compatibility)") << (debug?" (debug)":"") << (forward_compatible?" (forward_compatible)":"") << std::endl;
 	const GLubyte* renderer_c_string = glGetString(GL_RENDERER);
 	const GLubyte* glslversion_c_string = glGetString(GL_SHADING_LANGUAGE_VERSION);
 	if (vendor_c_string)
@@ -420,7 +488,6 @@ bool gl_context::configure_gl()
 				glDebugMessageCallback(debug_callback, this);
 		}
 	}
-	//enable_font_face(info_font_face, info_font_size);
 	// use the eye location to compute the specular lighting
 	if (!core_profile) {
 		glLightModelf(GL_LIGHT_MODEL_LOCAL_VIEWER, 0);
@@ -495,28 +562,19 @@ void gl_context::clear_background(bool color_flag, bool depth_flag, bool stencil
 float gl_context::get_current_font_size() const
 {
 	if (current_font_size == 0)
-		return info_font_size;
+		return get_info_font_size();
 	return current_font_size;
 }
 /// overwrite function to return info font face in case no font is currently selected
 media::font::font_face_ptr gl_context::get_current_font_face() const
 {
 	if (current_font_face.empty())
-		return info_font_face;
+		return get_info_font_face();
 	return current_font_face;
 }
 
 void gl_context::init_render_pass()
 {
-	if (info_font_face.empty()) {
-		font_ptr info_font = default_font(true);
-		if (!info_font.empty()) {
-			info_font_face = info_font->get_font_face(FFA_REGULAR);
-			info_font_face->enable(this, info_font_size);
-			if (current_font_face.empty())
-				enable_font_face(info_font_face, info_font_size);
-		}
-	}
 #ifdef WIN32
 	wglSwapIntervalEXT(enable_vsync ? 1 : 0);
 #else
@@ -632,7 +690,7 @@ void gl_context::draw_textual_info()
 		disable_depth_test();
 
 		push_pixel_coords();
-		enable_font_face(info_font_face, info_font_size);
+		enable_font_face(get_info_font_face(), get_info_font_size());
 
 		set_cursor(20, get_height()-1-20);
 
@@ -879,7 +937,7 @@ void gl_context::on_lights_changed()
 			if (light.get_type() == cgv::media::illum::LT_SPOT) {
 				glLightf(GL_LIGHT0 + light_idx, GL_SPOT_CUTOFF, light.get_spot_cutoff());
 				glLightf(GL_LIGHT0 + light_idx, GL_SPOT_EXPONENT, light.get_spot_exponent());
-				glLightfv(GL_LIGHT0 + light_idx, GL_SPOT_DIRECTION, light.get_spot_direction());
+				glLightfv(GL_LIGHT0 + light_idx, GL_SPOT_DIRECTION, light.get_spot_direction().data());
 			}
 			else {
 				glLightf(GL_LIGHT0 + light_idx, GL_SPOT_CUTOFF, 180.0f);
@@ -1673,7 +1731,7 @@ void gl_context::set_modelview_matrix(const dmat4& V)
 		GLint mm;
 		glGetIntegerv(GL_MATRIX_MODE, &mm);
 		glMatrixMode(GL_MODELVIEW);
-		glLoadMatrixd(V);
+		glLoadMatrixd(V.data());
 		glMatrixMode(mm);
 	}
 	context::set_modelview_matrix(V);
@@ -1685,7 +1743,7 @@ void gl_context::set_projection_matrix(const dmat4& P)
 		GLint mm;
 		glGetIntegerv(GL_MATRIX_MODE, &mm);
 		glMatrixMode(GL_PROJECTION);
-		glLoadMatrixd(P);
+		glLoadMatrixd(P.data());
 		glMatrixMode(mm);
 	}
 	context::set_projection_matrix(P);

3rd/fltk/OpenGL/Fl_Gl_Choice.cxx
@@ -31,6 +31,7 @@
 #include <stdlib.h>
 #include <iostream>
 #include <vector>
+#include <map>
 using namespace fltk;
 
 #define DEBUG_PFD (0) // 1 = PFD selection debug output, 0 = no debug output
@@ -61,10 +62,13 @@ static void debug_print_pfd(int pfd_id, const PIXELFORMATDESCRIPTOR& pfd) {
 
 static GlChoice* first;
 
-int*& GlChoice::ref_attrib_list()
+std::vector<int>& GlChoice::ref_gl_context_attrib_list(const Window* window)
 {
-	static int* attrib_list = 0;
-	return attrib_list;
+    static std::vector<int> default_attrib_list;
+	static std::map<const Window*, std::vector<int>> attrib_list_map;
+    if (window == 0)
+        return default_attrib_list;
+    return attrib_list_map[window];
 }
 
 GlChoice* GlChoice::find(int mode) {
@@ -275,6 +279,36 @@ static GLContext first_context;
 
 #if USE_X11
 
+GLXFBConfig findFBConfigFromVisual(Display* display, XVisualInfo* visualInfo) 
+{
+    if (!display || !visualInfo) {
+        fprintf(stderr, "Invalid display or visualInfo\n");
+        return NULL;
+    }
+
+    int fbCount;
+    GLXFBConfig* fbConfigs = glXGetFBConfigs(display, DefaultScreen(display), &fbCount);
+    if (!fbConfigs || fbCount == 0) {
+        fprintf(stderr, "Failed to get framebuffer configurations\n");
+        return NULL;
+    }
+
+    GLXFBConfig matchingFBConfig = NULL;
+    for (int i = 0; i < fbCount; i++) {
+        XVisualInfo* vi = glXGetVisualFromFBConfig(display, fbConfigs[i]);
+        if (vi) {
+            if (vi->visualid == visualInfo->visualid) {
+                matchingFBConfig = fbConfigs[i];
+                XFree(vi);
+                break;
+            }
+            XFree(vi);
+        }
+    }
+    XFree(fbConfigs);
+    return matchingFBConfig;
+}
+
 // Define this to destroy all OpenGL contexts at exit to try to fix NVidia crashes
 #define DESTROY_ON_EXIT 0
 
@@ -298,16 +332,16 @@ static void destructor() {
 
 GLContext fltk::create_gl_context(XVisualInfo* vis) {
   GLContext context;
-#if 0 // enable OpenGL3 support if possible
+#if 1 // enable OpenGL3 support if possible
   // This is disabled because it does not work on SUSE11 with NVidia cards.
   // I tried all the visuals and none worked. Error is returned when attempts
   // are made to use the context:
   // XRequest.144: BadAlloc (insufficient resources for operation) 0x1e00006
   // XRequest.144: GLXBadContext 0x1e00006
   // XRequest.144: GLXBadDrawable 0x1e00004
-  typedef GLXFBConfig (*PFNGLXGETFBCONFIGFROMVISUALSGIXPROC)(
-		Display *dpy,
-		XVisualInfo *vis );
+  //typedef GLXFBConfig (*PFNGLXGETFBCONFIGFROMVISUALSGIXPROC)(
+	//	Display *dpy,
+	//	XVisualInfo *vis );
   typedef GLXContext (*PFNGLXCREATECONTEXTATTRIBSARB)(
 		Display *dpy,
 		GLXFBConfig config,
@@ -315,26 +349,26 @@ GLContext fltk::create_gl_context(XVisualInfo* vis) {
 		Bool direct,
 		const int *attrib_list);
 
-  PFNGLXGETFBCONFIGFROMVISUALSGIXPROC glXGetFBConfigFromVisualSGIX = (PFNGLXGETFBCONFIGFROMVISUALSGIXPROC)
-		glXGetProcAddress((const GLubyte *) "glXGetFBConfigFromVisualSGIX");
+  //PFNGLXGETFBCONFIGFROMVISUALSGIXPROC glXGetFBConfigFromVisualSGIX = (PFNGLXGETFBCONFIGFROMVISUALSGIXPROC)
+	//	glXGetProcAddress((const GLubyte *) "glXGetFBConfigFromVisualSGIX");
 
   PFNGLXCREATECONTEXTATTRIBSARB glXCreateContextAttribsARB =
 		(PFNGLXCREATECONTEXTATTRIBSARB)  glXGetProcAddress((const GLubyte *) "glXCreateContextAttribsARB");
 
-  if (glXGetFBConfigFromVisualSGIX && glXCreateContextAttribsARB) {
-    printf("A\n");
+  if (!GlChoice::ref_gl_context_attrib_list(window).empty() && glXCreateContextAttribsARB) {
+    //printf("A\n");
     // printf("Success!\n");
-    GLXFBConfig c = glXGetFBConfigFromVisualSGIX(xdisplay, vis);
-    printf("B\n");
-#   define GLX_CONTEXT_MAJOR_VERSION_ARB		0x2091
-#   define GLX_CONTEXT_MINOR_VERSION_ARB		0x2092
-    int arr2[] = {  GLX_CONTEXT_MAJOR_VERSION_ARB, 3,
-                    GLX_CONTEXT_MINOR_VERSION_ARB, 0, None };
-    context = glXCreateContextAttribsARB(xdisplay, c, first_context, True, arr2);
-    printf("C\n");
+    GLXFBConfig c = findFBConfigFromVisual(xdisplay, vis);
+//    printf("B\n");
+//#   define GLX_CONTEXT_MAJOR_VERSION_ARB		0x2091
+//#   define GLX_CONTEXT_MINOR_VERSION_ARB		0x2092
+//    int arr2[] = {  GLX_CONTEXT_MAJOR_VERSION_ARB, 3,
+//                    GLX_CONTEXT_MINOR_VERSION_ARB, 0, None };
+    context = glXCreateContextAttribsARB(xdisplay, c, first_context, True, GlChoice::ref_gl_context_attrib_list(window).data());
+//    printf("C\n");
   } else
 #endif
-    context = glXCreateContext(xdisplay, vis, first_context, 1);
+      context = glXCreateContext(xdisplay, vis, first_context, 1);
 #if DESTROY_ON_EXIT
   Contexts* p = new Contexts;
   p->context = context;
@@ -356,7 +390,7 @@ GLContext fltk::create_gl_context(const Window* window, const GlChoice* g, int l
   SetPixelFormat(i->dc, g->pixelFormat, &g->pfd);
 
   GLContext context = 0;
-  if (GlChoice::ref_attrib_list()) {
+  if (!GlChoice::ref_gl_context_attrib_list(window).empty()) {
 	  HGLRC tempRC = wglCreateContext(i->dc);
 	  wglMakeCurrent(i->dc, tempRC);
 	  GLenum err = glewInit();
@@ -431,21 +465,15 @@ GLContext fltk::create_gl_context(const Window* window, const GlChoice* g, int l
 		//	std::cerr << "FL_Gl_Choice::wglChoosePixelFormatARB failed" << std::endl;
 
 		  bool is_first_context = first_context == 0;
-		  if (layer != 0) {
-			  int n = 0;
-			  while (GlChoice::ref_attrib_list()[n] != 0)
-				  n += 2;
-			  int* new_attribs = new int[n + 3];
-			  for (int j = 0; j < n; ++j)
-				  new_attribs[j] = GlChoice::ref_attrib_list()[j];
-			  new_attribs[n] = WGL_CONTEXT_LAYER_PLANE_ARB;
-			  new_attribs[n + 1] = layer;
-			  new_attribs[n + 2] = 0;
-			  context = wglCreateContextAttribsARB(i->dc, first_context, new_attribs);
-			  delete[] new_attribs;
+          auto& attrib_list = GlChoice::ref_gl_context_attrib_list(window);
+          if (layer != 0) {
+              attrib_list.back() = WGL_CONTEXT_LAYER_PLANE_ARB;
+              attrib_list.push_back(layer);
+              attrib_list.push_back(0);
+			  context = wglCreateContextAttribsARB(i->dc, first_context, attrib_list.data());
 		  }
 		  else
-			  context = wglCreateContextAttribsARB(i->dc, first_context, GlChoice::ref_attrib_list());
+			  context = wglCreateContextAttribsARB(i->dc, first_context, attrib_list.data());
 		  if (!context) {
 			  int err = glGetError();
 			  std::cerr << "WGL error: " << glewGetErrorString(err) << std::endl;

3rd/fltk/OpenGL/GlChoice.h
@@ -79,6 +79,8 @@
 # define GLContext GLXContext
 #endif
 
+#include <vector>
+
 namespace fltk {
 
 // Describes crap needed to create a GLContext.
@@ -97,7 +99,7 @@ public:
 #endif
   // Return one of these structures for a given gl mode, or null if impossible:
   static GlChoice *find(int mode);
-  static int*& ref_attrib_list();
+  static std::vector<int>& ref_gl_context_attrib_list(const Window* window = 0);
   int get_mode() const { return mode; }
 };
 

3rd/fltk/OpenGL/gl_draw.cxx
@@ -135,7 +135,7 @@ void fltk::glsetfont(fltk::Font* font, float size) {
   drawn correctly, others draw as question marks.
 */
 void fltk::gldrawtext(const char* str) {
-  gldrawtext(str, strlen(str));
+  gldrawtext(str, int(strlen(str)));
 }
 
 /**
@@ -221,7 +221,7 @@ void fltk::gldrawtext(const char* text, int n) {
   Draw \a text at the given point in 3D space transformed to the screen.
 */
 void fltk::gldrawtext(const char* str, float x, float y, float z) {
-  gldrawtext(str, strlen(str), x, y, z);
+  gldrawtext(str, int(strlen(str)), x, y, z);
 }
 
 /**

cgv/render/context.cxx
@@ -48,7 +48,7 @@ context_config::context_config()
 #else
 	debug = false;
 #endif
-	core_profile = false;
+	core_profile = true;
 	accumulation_bits = 0;
 	version_major = -1;
 	version_minor = -1;

cgv/render/context.h
@@ -563,6 +563,8 @@ struct CGV_API context_config
 	context_config();
 	/// reflect the shader_path member
 	bool self_reflect(cgv::reflect::reflection_handler& srh);
+	/// provide integer vector to store context creation attribute list
+	std::vector<int> context_creation_attrib_list;
 };
 
 /// type of ref counted pointer to context creation configuration

libs/cgv_gl/box_wire_renderer.cxx
@@ -1,4 +1,5 @@
 #include "box_wire_renderer.h"
+#include <cgv_reflect_types/math/fvec.h>
 #include <cgv_gl/gl/gl.h>
 #include <cgv_gl/gl/gl_tools.h>
 

libs/cgv_gl/gl/gl_context.h
@@ -13,6 +13,9 @@ namespace cgv {
 		/// namespace for opengl specific GPU programming
 		namespace gl {
 
+/// construct a 0 terminated list of context creation attribute definitions
+extern CGV_API std::vector<int> get_context_creation_attrib_list(cgv::render::context_config& cc);
+
 /// set a very specific texture format from a gl-constant and a component_format description. This should be called after the texture is constructed and before it is created.
 extern CGV_API void set_gl_format(texture& tex, GLuint gl_format, const std::string& component_format_description);
 
@@ -129,7 +132,7 @@ protected:
 	bool check_shader_support(ShaderType st, const std::string& where, const cgv::render::render_component* rc = 0) const;
 	bool check_fbo_support(const std::string& where, const cgv::render::render_component* rc = 0) const;
 	/// font used to draw textual info
-	cgv::media::font::font_face_ptr info_font_face;
+	mutable cgv::media::font::font_face_ptr info_font_face;
 	/// font size to draw textual info
 	float info_font_size;
 	/// 
@@ -157,6 +160,10 @@ public:
 	/// clear the buffer contents of the flagged buffers to the set background colors
 	void clear_background(bool color_flag, bool depth_flag, bool stencil_flag = false, bool accum_flag = false) override;
 
+	/// return info font size
+	float get_info_font_size() const;
+	/// return info font face and ensure that it is created
+	cgv::media::font::font_face_ptr get_info_font_face() const;
 	/// overwrite function to return info font size in case no font is currently selected
 	float get_current_font_size() const override;
 	/// overwrite function to return info font face in case no font is currently selected

commit 16d72b96ae20ed66544bae3b68edd4ac25fd2bcd

Fix wrong normals for context-tesselated primitives introduced in 7e0607ce8cec4fcafc1f741b9da4af7b829f7c37 - The switch to core profile caused the use of a context-internal attribute array binding - Vertex position, normal and texcoord are stored consecutively in a vertex buffer - A wrong stride for the normals caused a wrong texcoord offset, partly overwriting the normal data

libs/cgv_gl/gl/gl_context.cxx
@@ -1312,15 +1312,15 @@ bool gl_context::prepare_attributes(std::vector<vec3>& P, std::vector<vec3>& N, 
 		else
 			vbo_ptr->destruct(*this);
 		vbo_ptr->create(*this, P.size() * sizeof(vec3) + N.size() * sizeof(vec3) + T.size() * sizeof(vec2));
-		vbo_ptr->replace(const_cast<gl_context&>(*this), 0, &P.front(), P.size());
+		vbo_ptr->replace(const_cast<gl_context&>(*this), 0, P.data(), P.size());
 		size_t nml_off = P.size() * sizeof(vec3);
 		size_t tex_off = nml_off;
 		if (!N.empty()) {
-			vbo_ptr->replace(const_cast<gl_context&>(*this), nml_off, &N.front(), N.size());
-			tex_off += N.size() * sizeof(vec2);
+			vbo_ptr->replace(const_cast<gl_context&>(*this), nml_off, N.data(), N.size());
+			tex_off += N.size() * sizeof(vec3);
 		}
 		if (!T.empty())
-			vbo_ptr->replace(const_cast<gl_context&>(*this), tex_off, &T.front(), T.size());
+			vbo_ptr->replace(const_cast<gl_context&>(*this), tex_off, T.data(), T.size());
 
 		type_descriptor td3 = element_descriptor_traits<vec3>::get_type_descriptor(P.front());
 		aab_ptr->set_attribute_array(*this, prog_ptr->get_position_index(), td3, *vbo_ptr, 0, P.size());