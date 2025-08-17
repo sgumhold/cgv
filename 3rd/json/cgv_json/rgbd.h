9e268cb8b908f730f64e7704e0e53f53085b1d15
Dominik Maier <maier-dominik@outlook.de>
2025-08-17 15:39:25 +0200

    Added JPG as rgdb format

--- 3rd/json/cgv_json/rgbd.h
+++ 3rd/json/cgv_json/rgbd.h
@@ -8,12 +8,12 @@
 #include "../nlohmann/json.hpp"
 
 namespace rgbd {
-	void to_json(nlohmann::json& j, const rgbd_calibration& c) {
+	inline void to_json(nlohmann::json& j, const rgbd_calibration& c) {
 		j["depth_scale"] = c.depth_scale;
 		j["depth"] = c.depth;
 		j["color"] = c.color;
 	}
-	void from_json(const nlohmann::json& j, rgbd_calibration& c) {
+	inline void from_json(const nlohmann::json& j, rgbd_calibration& c) {
 		j.at("depth_scale").get_to(c.depth_scale);
 		j.at("depth").get_to(c.depth);
 		j.at("color").get_to(c.color);
