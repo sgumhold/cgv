3355b8cc55e86cc0697e28cc64e3b51caa2fe3c7
1DaveBig <david.gross.de@gmail.com>
2025-10-16 15:42:20 +0200

    Fix bugs in copy_if

--- libs/cgv_gpgpu/copy_if.cxx
+++ libs/cgv_gpgpu/copy_if.cxx
@@ -15,9 +15,6 @@ bool copy_if::init(cgv::render::context& ctx, const sl::data_type& value_type, c
 	if(!value_type.is_valid())
 		return false;
 
-	//_value_type = value_type;
-	//_num_values = static_cast<uint32_t>(count);
-
 	algorithm_create_info info;
 	info.arguments = &arguments;
 	info.types.push_back(value_type);
@@ -33,13 +30,6 @@ bool copy_if::init(cgv::render::context& ctx, const sl::data_type& value_type, c
 	};
 
 	return algorithm::init(ctx, info, kernels);
-
-	/*
-	if(algorithm::init(ctx, info, kernels))
-		return resize(ctx);
-
-	return false;
-	*/
 }
 
 void copy_if::destruct(const cgv::render::context& ctx) {
@@ -54,33 +44,22 @@ void copy_if::destruct(const cgv::render::context& ctx) {
 }
 
 void copy_if::resize(cgv::render::context& ctx, uint32_t size) {
-	// Todo: Clean up num groups (currently unused) and num scan groups.
-
-	// Pad number of values to the next multiple of blocksize.
 	uint32_t num_values_padded = cgv::math::next_multiple_k_greater_than_n(_block_size, size);
-	_num_groups = cgv::math::div_round_up(num_values_padded, _group_size);
-	_num_scan_groups = cgv::math::div_round_up(num_values_padded, _block_size);
-	_num_block_sums = cgv::math::next_power_of_two(_num_scan_groups);
-	uint32_t block_sum_offset_shift = static_cast<uint32_t>(std::log2(_block_size));
 	uint32_t num_vote_ballots = cgv::math::div_round_up(num_values_padded, 32u);
+
+	_num_groups = cgv::math::div_round_up(num_values_padded, _block_size);
+	_num_block_sums = cgv::math::next_power_of_two(_num_groups);
 
 	_votes_buffer.create_or_resize<uint32_t>(ctx, num_vote_ballots);
 	_prefix_sums_buffer.create_or_resize<uint32_t>(ctx, num_values_padded);
-	_block_sums_buffer.create_or_resize<uint32_t>(ctx, 4ull * _num_block_sums);
+	_block_sums_buffer.create_or_resize<uint32_t>(ctx, _num_block_sums);
 
+	uint32_t block_sum_offset_shift = static_cast<uint32_t>(std::log2(_block_size));
 	uint32_t last_block_sum_idx = (num_values_padded >> block_sum_offset_shift) - 1;
-
-	// Todo: Is num values padded actually needed? It should work without.
-	// Todo: Remove uniforms that are not used by individual kernels.
 
 	_vote_kernel.enable(ctx);
 	_vote_kernel.set_argument(ctx, "u_num_values", size);
-	_vote_kernel.set_argument(ctx, "u_num_values_padded", num_values_padded);
 	_vote_kernel.disable(ctx);
-
-	_scan_local_kernel.enable(ctx);
-	_scan_local_kernel.set_argument(ctx, "u_num_scan_groups", _num_scan_groups);
-	_scan_local_kernel.disable(ctx);
 
 	_scan_global_kernel.enable(ctx);
 	_scan_global_kernel.set_argument(ctx, "u_num_block_sums", _num_block_sums);
@@ -103,22 +82,15 @@ bool copy_if::dispatch(cgv::render::context& ctx, device_buffer_iterator input_f
 	if(compatible(input_first, output_first))
 		return false;
 	
-
-	//const size_t max_count = 0xFFFFFFFF;
-	//if(count == 0 || count > max_count)
-	//	return false;
-
-	// Todo: Decide on exact vote kernel implementation based on available group count.
-	auto temp = ctx.get_device_capabilities().max_compute_work_group_count.x();
-
-
 	uint32_t count = static_cast<uint32_t>(cgv::gpgpu::distance(input_first, input_last));
-	resize(ctx, count);
+	uint32_t max_count = static_cast<uint32_t>(ctx.get_device_capabilities().max_compute_work_group_count.x()) * _block_size;
+	if(count > max_count)
+		return false;
 
-
-
-	// Todo: Currently, the input range size must exactly match _num_values!
-	// Todo: Implement actual support for iterators.
+	if(count != _last_size) {
+		resize(ctx, count);
+		_last_size = count;
+	}
 
 	input_first.buffer().bind(ctx, cgv::render::VertexBufferType::VBT_STORAGE, 0);
 	output_first.buffer().bind(ctx, cgv::render::VertexBufferType::VBT_STORAGE, 1);
@@ -127,21 +99,20 @@ bool copy_if::dispatch(cgv::render::context& ctx, device_buffer_iterator input_f
 	_vote_kernel.enable(ctx);
 	_vote_kernel.set_argument<uint32_t>(ctx, "u_input_begin", input_first.index());
 	_vote_kernel.set_argument<uint32_t>(ctx, "u_input_end", input_last.index());
-	_vote_kernel.set_argument<uint32_t>(ctx, "u_output_begin", output_first.index());
 	_vote_kernel.set_arguments(ctx, arguments);
 	bind_buffer_like_arguments(ctx, arguments);
 
-	dispatch_compute(_num_scan_groups, 1, 1);
+	dispatch_compute(_num_groups, 1, 1);
 	glMemoryBarrier(GL_SHADER_STORAGE_BARRIER_BIT);
 	
 	unbind_buffer_like_arguments(ctx, arguments);
 	_vote_kernel.disable(ctx);
-	
+
 	_prefix_sums_buffer.bind(ctx, 3);
 	_block_sums_buffer.bind(ctx, 4);
 
 	_scan_local_kernel.enable(ctx);
-	dispatch_compute(_num_scan_groups, 1, 1);
+	dispatch_compute(_num_groups, 1, 1);
 	glMemoryBarrier(GL_SHADER_STORAGE_BARRIER_BIT);
 	_scan_local_kernel.disable(ctx);
 
@@ -151,7 +122,10 @@ bool copy_if::dispatch(cgv::render::context& ctx, device_buffer_iterator input_f
 	_scan_global_kernel.disable(ctx);
 
 	_scatter_kernel.enable(ctx);
-	dispatch_compute(_num_scan_groups, 1, 1);
+	_scatter_kernel.set_argument<uint32_t>(ctx, "u_input_begin", input_first.index());
+	_scatter_kernel.set_argument<uint32_t>(ctx, "u_input_end", input_last.index());
+	_scatter_kernel.set_argument<uint32_t>(ctx, "u_output_begin", output_first.index());
+	dispatch_compute(_num_groups, 1, 1);
 	glMemoryBarrier(GL_SHADER_STORAGE_BARRIER_BIT);
 	_scatter_kernel.disable(ctx);
 
@@ -174,20 +148,13 @@ bool copy_if::dispatch(cgv::render::context& ctx, device_buffer_iterator input_f
 
 copy_if_atomic::copy_if_atomic() : algorithm("copy_if_atomic") {}
 
-bool copy_if_atomic::init(cgv::render::context& ctx, const sl::data_type& value_type, size_t count, const std::string& unary_predicate) {
-	return init(ctx, value_type, count, {}, unary_predicate);
+bool copy_if_atomic::init(cgv::render::context& ctx, const sl::data_type& value_type, const std::string& unary_predicate) {
+	return init(ctx, value_type, {}, unary_predicate);
 }
 
-bool copy_if_atomic::init(cgv::render::context& ctx, const sl::data_type& value_type, size_t count, const argument_definitions& arguments, const std::string& unary_predicate) {
+bool copy_if_atomic::init(cgv::render::context& ctx, const sl::data_type& value_type, const argument_definitions& arguments, const std::string& unary_predicate) {
 	if(!value_type.is_valid())
 		return false;
-
-	const size_t max_count = 0xFFFFFFFF;
-	if(count == 0 || count > max_count)
-		return false;
-
-	_value_type = value_type;
-	_num_values = static_cast<uint32_t>(count);
 
 	algorithm_create_info info;
 	info.arguments = &arguments;
@@ -196,32 +163,15 @@ bool copy_if_atomic::init(cgv::render::context& ctx, const sl::data_type& value_
 	info.default_buffer_count = 3;
 	info.options.define_snippet("predicate", unary_predicate);
 
-	if(algorithm::init(ctx, info, { { &_kernel, "gpgpu_copy_if_atomic" }, }))
-		return resize(ctx);
+	_atomic_counter_buffer.create_or_resize<int32_t>(ctx, 1);
 
-	return false;
+	algorithm::init(ctx, info, { { &_kernel, "gpgpu_copy_if_atomic" }, });
 }
 
 void copy_if_atomic::destruct(const cgv::render::context& ctx) {
 	_kernel.destruct(ctx);
 	_atomic_counter_buffer.destruct(ctx);
 	algorithm::destruct(ctx);
-}
-
-bool copy_if_atomic::resize(cgv::render::context& ctx) {
-	// Pad number of values to the next multiple of blocksize.
-	_num_groups = cgv::math::div_round_up(_num_values, _group_size);
-	
-	_atomic_counter_buffer.create_or_resize<int32_t>(ctx, 1);
-	
-	// Todo: Is num values padded actually needed? It should work without.
-	// Todo: Remove uniforms that are not used by individual kernels.
-
-	_kernel.enable(ctx);
-	_kernel.set_argument(ctx, "u_num_values", _num_values);
-	_kernel.disable(ctx);
-
-	return true;
 }
 
 bool copy_if_atomic::dispatch(cgv::render::context& ctx, const cgv::render::vertex_buffer& input_buffer, const cgv::render::vertex_buffer& output_buffer, size_t count, const argument_bindings& arguments) {
@@ -235,30 +185,29 @@ bool copy_if_atomic::dispatch(cgv::render::context& ctx, device_buffer_iterator 
 	if(compatible(input_first, output_first))
 		return false;
 
-	// Todo: Reset atomic_counter_buffer to zero.
-
+	// Reset atomic_counter_buffer to zero.
 	_atomic_counter_buffer.ctx_ptr = &ctx;
 	GLuint id = 0;
 	_atomic_counter_buffer.put_id(id);
 	int32_t zero = 0;
 	glNamedBufferSubData(id, 0, sizeof(int32_t), &zero);
 
-	
-	// Todo: Currently, the input range size must exactly match _num_values!
-	// Todo: Implement actual support for iterators.
+	uint32_t count = static_cast<uint32_t>(cgv::gpgpu::distance(input_first, input_last));
+	uint32_t num_groups = cgv::math::div_round_up(count, _group_size);
 
 	input_first.buffer().bind(ctx, cgv::render::VertexBufferType::VBT_STORAGE, 0);
 	output_first.buffer().bind(ctx, cgv::render::VertexBufferType::VBT_STORAGE, 1);
 	_atomic_counter_buffer.bind(ctx, 2);
 
 	_kernel.enable(ctx);
+	_kernel.set_argument(ctx, "u_num_values", count);
 	_kernel.set_argument<uint32_t>(ctx, "u_input_begin", input_first.index());
 	_kernel.set_argument<uint32_t>(ctx, "u_input_end", input_last.index());
 	_kernel.set_argument<uint32_t>(ctx, "u_output_begin", output_first.index());
 	_kernel.set_arguments(ctx, arguments);
 	bind_buffer_like_arguments(ctx, arguments);
 
-	dispatch_compute(_num_groups, 1, 1);
+	dispatch_compute(num_groups, 1, 1);
 	glMemoryBarrier(GL_SHADER_STORAGE_BARRIER_BIT);
 
 	unbind_buffer_like_arguments(ctx, arguments);
--- libs/cgv_gpgpu/copy_if.h
+++ libs/cgv_gpgpu/copy_if.h
@@ -14,6 +14,60 @@ namespace gpgpu {
 //	kIndex
 //};
 
+namespace detail {
+/*
+/// GPU compute shader implementation for copying values based on a boolean predicate.
+class CGV_API copy_if_base : public algorithm {
+public:
+	copy_if_base();
+
+	bool init(cgv::render::context& ctx, const sl::data_type& value_type, const std::string& unary_predicate);
+	bool init(cgv::render::context& ctx, const sl::data_type& value_type, const argument_definitions& arguments, const std::string& unary_predicate);
+
+	void destruct(const cgv::render::context& ctx);
+
+	bool dispatch(cgv::render::context& ctx, const cgv::render::vertex_buffer& input_buffer, const cgv::render::vertex_buffer& output_buffer, size_t count, const argument_bindings& arguments = {});
+	bool dispatch(cgv::render::context& ctx, device_buffer_iterator input_first, device_buffer_iterator input_last, device_buffer_iterator output_first, const argument_bindings& arguments = {});
+
+	bool read_count(cgv::render::context& ctx, size_t& out) {
+		uint32_t count = 0;
+		if(_prefix_sums_buffer.copy(ctx, 0, &count, 1)) {
+			out = count;
+			return true;
+		}
+		return false;
+	}
+
+private:
+	void resize(cgv::render::context& ctx, uint32_t size);
+
+	//CopyMode mode = CopyMode::kCopy;
+
+	const uint32_t _group_size = 64;
+	const uint32_t _block_size = 4 * _group_size;
+
+	uint32_t _num_block_sums = 0;
+	uint32_t _num_groups = 0;
+	uint32_t _num_scan_groups = 0;
+
+	compute_kernel _vote_kernel;
+	compute_kernel _scan_local_kernel;
+	compute_kernel _scan_global_kernel;
+	compute_kernel _scatter_kernel;
+
+	/// GPU buffers objects
+	storage_buffer _votes_buffer;
+	storage_buffer _prefix_sums_buffer;
+	storage_buffer _block_sums_buffer;
+};
+*/
+} // detail
+
+//enum class MemoryPolicy {
+//	kUnrestricted,
+//	kConservative
+//};
+
 /// GPU compute shader implementation for copying values based on a boolean predicate.
 class CGV_API copy_if : public algorithm {
 public:
@@ -37,6 +91,9 @@ public:
 	}
 
 private:
+
+	std::vector<uint32_t> votes;
+
 	void resize(cgv::render::context& ctx, uint32_t size);
 
 	//CopyMode mode = CopyMode::kCopy;
@@ -44,9 +101,9 @@ private:
 	const uint32_t _group_size = 64;
 	const uint32_t _block_size = 4 * _group_size;
 
+	uint32_t _last_size = 0;
 	uint32_t _num_block_sums = 0;
 	uint32_t _num_groups = 0;
-	uint32_t _num_scan_groups = 0;
 
 	compute_kernel _vote_kernel;
 	compute_kernel _scan_local_kernel;
@@ -63,8 +120,8 @@ class CGV_API copy_if_atomic : public algorithm {
 public:
 	copy_if_atomic();
 
-	bool init(cgv::render::context& ctx, const sl::data_type& value_type, size_t count, const std::string& unary_predicate);
-	bool init(cgv::render::context& ctx, const sl::data_type& value_type, size_t count, const argument_definitions& arguments, const std::string& unary_predicate);
+	bool init(cgv::render::context& ctx, const sl::data_type& value_type, const std::string& unary_predicate);
+	bool init(cgv::render::context& ctx, const sl::data_type& value_type, const argument_definitions& arguments, const std::string& unary_predicate);
 
 	void destruct(const cgv::render::context& ctx);
 
@@ -81,15 +138,13 @@ public:
 	}
 
 private:
-	bool resize(cgv::render::context& ctx);
-
-	const uint32_t _group_size = 64;
-	const uint32_t _block_size = 4 * _group_size;
-
-	uint32_t _num_groups = 0;
-
-	sl::data_type _value_type;
-	uint32_t _num_values = 0;
+	//const uint32_t _group_size = 64;
+	//const uint32_t _block_size = 4 * _group_size;
+	//
+	//uint32_t _num_groups = 0;
+	//
+	//sl::data_type _value_type;
+	//uint32_t _num_values = 0;
 
 	compute_kernel _kernel;
 	
