a7859d30efd8e90ed963f6851f68a78dfd45d97f
brussig-tud <39052884+brussig-tud@users.noreply.github.com>
2025-10-27 13:36:37 +0000

    Deploying to gh-pages from @ sgumhold/cgv@fe64e2bda43eddfa634b48df09bfd2bdce494dc0 🚀

--- classcgv_1_1render_1_1ellipsoid__renderer.js
+++ classcgv_1_1render_1_1ellipsoid__renderer.js
@@ -1,14 +1,14 @@
 var classcgv_1_1render_1_1ellipsoid__renderer =
 [
     [ "create_render_style", "classcgv_1_1render_1_1ellipsoid__renderer.html#a2b7d96eb3af78bcb8778a51f333fbe34", null ],
-    [ "disable", "classcgv_1_1render_1_1ellipsoid__renderer.html#a677130fd3a85ad2d659e79d7368b699e", null ],
-    [ "disable_attribute_array_manager", "classcgv_1_1render_1_1ellipsoid__renderer.html#aba6d9a1168ced0274f162219574676bf", null ],
-    [ "draw", "classcgv_1_1render_1_1ellipsoid__renderer.html#ab5189e0c48749b46cd144c8d04ea6429", null ],
-    [ "enable", "classcgv_1_1render_1_1ellipsoid__renderer.html#abebf977d756c9b63443e4ea7ad15addc", null ],
-    [ "enable_attribute_array_manager", "classcgv_1_1render_1_1ellipsoid__renderer.html#a5fac8d57fe16d92073966e505d93b576", null ],
+    [ "disable", "classcgv_1_1render_1_1ellipsoid__renderer.html#aefd36feace9e82f708cd8c468770a6a2", null ],
+    [ "disable_attribute_array_manager", "classcgv_1_1render_1_1ellipsoid__renderer.html#a783a61b8826c95361623e3712f3e297a", null ],
+    [ "draw", "classcgv_1_1render_1_1ellipsoid__renderer.html#a7fad8174aefc121c416398888be96076", null ],
+    [ "enable", "classcgv_1_1render_1_1ellipsoid__renderer.html#aafe8a05b14d035ad2d9981a45b5665df", null ],
+    [ "enable_attribute_array_manager", "classcgv_1_1render_1_1ellipsoid__renderer.html#adf4ae9d7bbfb71587b72f65f6d4f71f9", null ],
     [ "get_default_prog_name", "classcgv_1_1render_1_1ellipsoid__renderer.html#a8e2fd14f433b84468054efe483251da1", null ],
     [ "remove_orientation_array", "classcgv_1_1render_1_1ellipsoid__renderer.html#afc826294909e6660c981aa480c196324", null ],
     [ "remove_size_array", "classcgv_1_1render_1_1ellipsoid__renderer.html#a6308f835715bdbd831e6a82e2c560bfb", null ],
     [ "set_orientation_array", "classcgv_1_1render_1_1ellipsoid__renderer.html#a21b7fcde39798133c75373a6b64dbb8a", null ],
-    [ "validate_attributes", "classcgv_1_1render_1_1ellipsoid__renderer.html#af13cdf78092673eb2effc598906db81f", null ]
+    [ "validate_attributes", "classcgv_1_1render_1_1ellipsoid__renderer.html#a50adaefab0cc3a70dd55b693602bca7c", null ]
 ];
