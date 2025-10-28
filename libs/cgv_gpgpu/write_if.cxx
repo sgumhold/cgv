8ea06fda2f36ba0e2521e9ae5ed1fe28ce843923
1DaveBig <david.gross.de@gmail.com>
2025-10-28 16:07:57 +0100

    Refactor gpgpu copy_if - Rename copy_if_atomic to collect_if - Add select_if that is similar to copy_if but outputs indices instead of values - Add output iterator for copy_if result count

--- libs/cgv_gpgpu/write_if.cxx
+++ libs/cgv_gpgpu/write_if.cxx
@@ -4,14 +4,15 @@
 
 namespace cgv {
 namespace gpgpu {
+namespace detail {
 
-copy_if::copy_if() : algorithm("copy_if") {}
+write_if::write_if(const std::string& name, bool write_indices) : algorithm(name), _write_indices(write_indices) {}
 
-bool copy_if::init(cgv::render::context& ctx, const sl::data_type& value_type, const std::string& unary_predicate) {
+bool write_if::init(cgv::render::context& ctx, const sl::data_type& value_type, const std::string& unary_predicate) {
 	return init(ctx, value_type, {}, unary_predicate);
 }
 
-bool copy_if::init(cgv::render::context& ctx, const sl::data_type& value_type, const argument_definitions& arguments, const std::string& unary_predicate) {
+bool write_if::init(cgv::render::context& ctx, const sl::data_type& value_type, const argument_definitions& arguments, const std::string& unary_predicate) {
 	if(!value_type.is_valid())
 		return false;
 
@@ -19,7 +20,8 @@ bool copy_if::init(cgv::render::context& ctx, const sl::data_type& value_type, c
 	info.arguments = &arguments;
 	info.types.push_back(value_type);
 	info.typedefs.push_back({ "value_type", value_type });
-	info.default_buffer_count = 5;
+	info.default_buffer_count = 6;
+	info.options.define_macro_if_true(_write_indices, "WRITE_INDICES");
 	info.options.define_snippet("predicate", unary_predicate);
 
 	std::vector<compute_kernel_info> kernels = {
@@ -30,11 +32,12 @@ bool copy_if::init(cgv::render::context& ctx, const sl::data_type& value_type, c
 	};
 
 	_uniform_buffer.create(ctx);
+	_last_sum_buffer.create_or_resize<uint32_t>(ctx, 1);
 
 	return algorithm::init(ctx, info, kernels);
 }
 
-void copy_if::destruct(const cgv::render::context& ctx) {
+void write_if::destruct(const cgv::render::context& ctx) {
 	_uniform_buffer.destruct(ctx);
 	_vote_kernel.destruct(ctx);
 	_scan_local_kernel.destruct(ctx);
@@ -43,10 +46,11 @@ void copy_if::destruct(const cgv::render::context& ctx) {
 	_votes_buffer.destruct(ctx);
 	_prefix_sums_buffer.destruct(ctx);
 	_block_sums_buffer.destruct(ctx);
+	_last_sum_buffer.destruct(ctx);
 	algorithm::destruct(ctx);
 }
 
-void copy_if::resize(cgv::render::context& ctx, uint32_t size) {
+void write_if::resize(cgv::render::context& ctx, uint32_t size) {
 	uint32_t num_values_padded = cgv::math::next_multiple_k_greater_than_n(_block_size, size);
 	uint32_t num_vote_ballots = cgv::math::div_round_up(num_values_padded, 32u);
 
@@ -61,17 +65,21 @@ void copy_if::resize(cgv::render::context& ctx, uint32_t size) {
 	_last_block_sum_idx = (num_values_padded >> block_sum_offset_shift) - 1;
 }
 
-bool copy_if::dispatch(cgv::render::context& ctx, const cgv::render::vertex_buffer& input_buffer, const cgv::render::vertex_buffer& output_buffer, size_t count, const argument_bindings& arguments) {
+bool write_if::dispatch(cgv::render::context& ctx, const cgv::render::vertex_buffer& input_buffer, const cgv::render::vertex_buffer& output_buffer, size_t count, const argument_bindings& arguments) {
 	return dispatch(ctx, begin(input_buffer), begin(input_buffer) + count, begin(output_buffer), arguments);
 }
 
-bool copy_if::dispatch(cgv::render::context& ctx, device_buffer_iterator input_first, device_buffer_iterator input_last, device_buffer_iterator output_first, const argument_bindings& arguments) {
+bool write_if::dispatch(cgv::render::context& ctx, device_buffer_iterator input_first, device_buffer_iterator input_last, device_buffer_iterator output_first, const argument_bindings& arguments) {
+	return dispatch(ctx, input_first, input_last, output_first, begin(_last_sum_buffer), arguments);
+}
+
+bool write_if::dispatch(cgv::render::context& ctx, device_buffer_iterator input_first, device_buffer_iterator input_last, device_buffer_iterator output_first, device_buffer_iterator output_count, const argument_bindings& arguments) {
 	if(!is_valid_range(input_first, input_last))
 		return false;
 
 	if(compatible(input_first, output_first))
 		return false;
-	
+
 	uint32_t count = static_cast<uint32_t>(cgv::gpgpu::distance(input_first, input_last));
 	uint32_t max_count = static_cast<uint32_t>(ctx.get_device_capabilities().max_compute_work_group_count.x()) * _block_size;
 	if(count > max_count)
@@ -86,7 +94,7 @@ bool copy_if::dispatch(cgv::render::context& ctx, device_buffer_iterator input_f
 	uniforms.input_begin = static_cast<uint32_t>(input_first.index());
 	uniforms.input_end = static_cast<uint32_t>(input_last.index());
 	uniforms.output_begin = static_cast<uint32_t>(output_first.index());
-	uniforms.num_values = count;
+	uniforms.output_count_begin = static_cast<uint32_t>(output_count.index());
 	uniforms.num_block_sums = _num_block_sums;
 	uniforms.last_block_sum_idx = _last_block_sum_idx;
 	_uniform_buffer.replace(ctx, uniforms);
@@ -95,19 +103,20 @@ bool copy_if::dispatch(cgv::render::context& ctx, device_buffer_iterator input_f
 	input_first.buffer().bind(ctx, cgv::render::VertexBufferType::VBT_STORAGE, 0);
 	output_first.buffer().bind(ctx, cgv::render::VertexBufferType::VBT_STORAGE, 1);
 	_votes_buffer.bind(ctx, 2);
-	
+
 	_vote_kernel.enable(ctx);
 	_vote_kernel.set_arguments(ctx, arguments);
 	bind_buffer_like_arguments(ctx, arguments);
 
 	dispatch_compute(_num_groups, 1, 1);
 	glMemoryBarrier(GL_SHADER_STORAGE_BARRIER_BIT);
-	
+
 	unbind_buffer_like_arguments(ctx, arguments);
 	_vote_kernel.disable(ctx);
 
 	_prefix_sums_buffer.bind(ctx, 3);
 	_block_sums_buffer.bind(ctx, 4);
+	output_count.buffer().bind(ctx, cgv::render::VertexBufferType::VBT_STORAGE, 5);
 
 	_scan_local_kernel.enable(ctx);
 	dispatch_compute(_num_groups, 1, 1);
@@ -129,93 +138,22 @@ bool copy_if::dispatch(cgv::render::context& ctx, device_buffer_iterator input_f
 	_votes_buffer.unbind(ctx, 2);
 	_prefix_sums_buffer.unbind(ctx, 3);
 	_block_sums_buffer.unbind(ctx, 4);
+	output_count.buffer().unbind(ctx, cgv::render::VertexBufferType::VBT_STORAGE, 5);
 
 	_uniform_buffer.unbind(ctx, 0);
 
 	return true;
 }
 
-
-
-
-
-
-
-
-
-copy_if_atomic::copy_if_atomic() : algorithm("copy_if_atomic") {}
-
-bool copy_if_atomic::init(cgv::render::context& ctx, const sl::data_type& value_type, const std::string& unary_predicate) {
-	return init(ctx, value_type, {}, unary_predicate);
+bool write_if::read_count(cgv::render::context& ctx, size_t& out) {
+	uint32_t count = 0;
+	if(_last_sum_buffer.copy(ctx, 0, &count, 1)) {
+		out = count;
+		return true;
+	}
+	return false;
 }
 
-bool copy_if_atomic::init(cgv::render::context& ctx, const sl::data_type& value_type, const argument_definitions& arguments, const std::string& unary_predicate) {
-	if(!value_type.is_valid())
-		return false;
-
-	algorithm_create_info info;
-	info.arguments = &arguments;
-	info.types.push_back(value_type);
-	info.typedefs.push_back({ "value_type", value_type });
-	info.default_buffer_count = 3;
-	info.options.define_snippet("predicate", unary_predicate);
-
-	_atomic_counter_buffer.create_or_resize<int32_t>(ctx, 1);
-
-	return algorithm::init(ctx, info, { { &_kernel, "gpgpu_copy_if_atomic" }, });
-}
-
-void copy_if_atomic::destruct(const cgv::render::context& ctx) {
-	_kernel.destruct(ctx);
-	_atomic_counter_buffer.destruct(ctx);
-	algorithm::destruct(ctx);
-}
-
-bool copy_if_atomic::dispatch(cgv::render::context& ctx, const cgv::render::vertex_buffer& input_buffer, const cgv::render::vertex_buffer& output_buffer, size_t count, const argument_bindings& arguments) {
-	return dispatch(ctx, begin(input_buffer), begin(input_buffer) + count, begin(output_buffer), arguments);
-}
-
-bool copy_if_atomic::dispatch(cgv::render::context& ctx, device_buffer_iterator input_first, device_buffer_iterator input_last, device_buffer_iterator output_first, const argument_bindings& arguments) {
-	if(!is_valid_range(input_first, input_last))
-		return false;
-
-	if(compatible(input_first, output_first))
-		return false;
-
-	// Reset atomic_counter_buffer to zero.
-	_atomic_counter_buffer.ctx_ptr = &ctx;
-	GLuint id = 0;
-	_atomic_counter_buffer.put_id(id);
-	int32_t zero = 0;
-	glNamedBufferSubData(id, 0, sizeof(int32_t), &zero);
-
-	uint32_t count = static_cast<uint32_t>(cgv::gpgpu::distance(input_first, input_last));
-	uint32_t num_groups = cgv::math::div_round_up(count, _group_size);
-
-	input_first.buffer().bind(ctx, cgv::render::VertexBufferType::VBT_STORAGE, 0);
-	output_first.buffer().bind(ctx, cgv::render::VertexBufferType::VBT_STORAGE, 1);
-	_atomic_counter_buffer.bind(ctx, 2);
-
-	_kernel.enable(ctx);
-	_kernel.set_argument(ctx, "u_num_values", count);
-	_kernel.set_argument<uint32_t>(ctx, "u_input_begin", input_first.index());
-	_kernel.set_argument<uint32_t>(ctx, "u_input_end", input_last.index());
-	_kernel.set_argument<uint32_t>(ctx, "u_output_begin", output_first.index());
-	_kernel.set_arguments(ctx, arguments);
-	bind_buffer_like_arguments(ctx, arguments);
-
-	dispatch_compute(num_groups, 1, 1);
-	glMemoryBarrier(GL_SHADER_STORAGE_BARRIER_BIT);
-
-	unbind_buffer_like_arguments(ctx, arguments);
-	_kernel.disable(ctx);
-
-	input_first.buffer().unbind(ctx, cgv::render::VertexBufferType::VBT_STORAGE, 0);
-	output_first.buffer().unbind(ctx, cgv::render::VertexBufferType::VBT_STORAGE, 1);
-	_atomic_counter_buffer.unbind(ctx, 2);
-
-	return true;
-}
-
+} // namespace detail
 } // namespace gpgpu
 } // namespace cgv
