d5a4ea321ab30ad46bf6aa2424c42535c51f8587
1DaveBig <david.gross.de@gmail.com>
2025-11-03 14:30:55 +0100

    Udpate stream compaction example

--- plugins/examples/stream_compaction.cxx
+++ plugins/examples/stream_compaction.cxx
@@ -6,10 +6,9 @@
 #include <cgv/render/drawable.h>
 #include <cgv_gl/sphere_render_data.h>
 #include <cgv_gl/gl/gl_time_query.h>
-#include <cgv_gpgpu/copy.h>
-#include <cgv_gpgpu/copy_if.h>
+#include <cgv_gpgpu/select_if.h>
 
-/// This example illustrates how to perform stream compaction, i.e. filtering of an array, using the gpgpu::copy_if algorithm.
+/// This example illustrates how to perform stream compaction, i.e. filtering of an array, on the GPU using the gpgpu::select_if algorithm.
 class stream_compaction : public cgv::base::node, public cgv::render::drawable, public cgv::gui::provider {
 public:
 	stream_compaction() : cgv::base::node("Stream Compaction")
@@ -41,31 +40,30 @@ public:
 			return false;
 		}
 
-		// Define a data type that matches the 3d positions of the sphere geometry.
-		// Note: Due to alignment rules for GPU buffers we cannot use sl::kVec3 directly but instead have to use a struct with three float members.
-		sl::data_type vec3_type = { "vec3_type", {
-			{ sl::Type::kFloat, "x" },
-			{ sl::Type::kFloat, "y" },
-			{ sl::Type::kFloat, "z" }
-		} };
+		// Define the value type to match the radius values in floating point format.
+		sl::data_type value_type = sl::Type::kFloat;
+
+		// Define the arguments used by the predicate. Since the predicate depends on uniform, outside variables we have to define them before usage.
+		// The following defines two uniform single-valued arguments of type float. The arguments are then available to use in the predicate functin by their given name.
+		// Is is considered good practice to give arguments a prefix, here "a_", to differentiate them from other identifiers.
+		// In general, arguments can be of type uniform, buffer, texture or image.
+		cgv::gpgpu::argument_definitions arguments = {
+			{ sl::Type::kFloat, "a_radius_min" },
+			{ sl::Type::kFloat, "a_radius_max" }
+		};
 
 		// Define a boolean predicate that returns true for all input elements we want to keep and copy to the output buffer.
 		// This string defines the body of a function with signature bool(value_type element, uint index), where value_type is equal to the value_type as given to the init method of copy_if.
 		// The content needs to follow GLSL syntax.
-		// In this case, value_type is equal to vec3_type as defined in the constructor and the predicate will return true for all positions within the unit sphere.
+		// In this case, value_type is equal to float as defined in the constructor and the predicate will return true for all radii
+		// that are in the range a_radius_min to a_radius_max inclusive.
 		std::string predicate = R"(
-			vec3 position = vec3(element.x, element.y, element.z);
-			return length(position) < 1.0;
+			return a_radius_min <= element && element <= a_radius_max;
 		)";
 
-		// Initialize the filter (copy_if) using the given value type and predicate.
-		if(!filter.init(ctx, vec3_type, predicate)) {
+		// Initialize the filter (select_if) using the given value type, arguments and predicate.
+		if(!filter.init(ctx, sl::Type::kFloat, arguments, predicate)) {
 			std::cout << "Error: could not initialize GPU filter algorithm" << std::endl;
-			return false;
-		}
-
-		if(!copy_helper.init(ctx, vec3_type)) {
-			std::cout << "Error: could not initialize GPU copy algorithm" << std::endl;
 			return false;
 		}
 
@@ -75,14 +73,13 @@ public:
 	void clear(cgv::render::context& ctx)
 	{
 		spheres.destruct(ctx);
-		filtered_positions_buffer.destruct(ctx);
-		copy_helper.destruct(ctx);
 		filter.destruct(ctx);
 		time_query.destruct(ctx);
 	}
 
 	void draw(cgv::render::context& ctx)
 	{	
+		apply_filter();
 		spheres.render(ctx, 0, sphere_render_count);
 	}
 
@@ -92,13 +89,9 @@ public:
 
 		add_member_control(this, "Measure time", measure_time, "check");
 
-		add_member_control(this, "Primitive count", primitive_count, "value_slider", "min=1000;max=10000000;ticks=true");
+		add_member_control(this, "Primitive count", primitive_count, "value_slider", "min=1000;max=1000000;ticks=true");
 		connect_copy(add_button("Generate")->click, cgv::signal::rebind(this, &stream_compaction::create_spheres));
 
-		connect_copy(add_button("Filter")->click, cgv::signal::rebind(this, &stream_compaction::apply_filter));
-
-		//std::string options = "min=" + std::to_string(radius_min) + ";max=" + std::to_string(radius_max) + ";step=0.0001;ticks=true";
-		/*
 		cgv::gui::property_string options;
 		options.add("min", radius_min);
 		options.add("max", radius_max);
@@ -106,8 +99,7 @@ public:
 		add_decorator("Radius range", "heading", "level=3");
 		add_member_control(this, "Min", threshold_min, "value_slider", options);
 		add_member_control(this, "Max", threshold_max, "value_slider", options);
-		*/
-
+		
 		if(begin_tree_node("Sphere style", spheres.style, false)) {
 			align("\a");
 			add_gui("", spheres.style);
@@ -136,17 +128,14 @@ private:
 
 			float t = 0.5f * snorm_distr(random_engine) + 0.5f;
 			float radius = cgv::math::lerp(radius_min, radius_max, t);
-
-			cgv::rgb color = (1.0f - t) * green + t * red;
+			cgv::rgb color = cgv::media::lerp(green, red, t);
 
 			spheres.add(position, color, radius);
-			//spheres.add_index(i);
+			// Add indices that are later filtered and used to draw the reduced amount of spheres.
+			spheres.add_index(i);
 		}
 
-		// Create an empty buffer large enough to hold all filtered positions.
-		filtered_positions_buffer.create_or_resize(*ctx, sizeof(cgv::vec3) * spheres.positions.size());
-
-		// -1 means to render all spheres
+		// A value of -1 means to render all spheres.
 		sphere_render_count = -1;
 
 		post_redraw();
@@ -167,37 +156,39 @@ private:
 			return;
 		}
 
-		if(!copy_helper.is_initialized()) {
-			std::cout << "Error: GPU copy algorithm is not initialized" << std::endl;
-			return;
-		}
-
 		// Ensure the CPU side buffers are transferred to the GPU buffers before trying to access them with the GPU-based filter.
 		cgv::render::sphere_renderer& sr = ref_sphere_renderer(ctx);
 		spheres.early_transfer(ctx, sr);
 
 		auto& aam = spheres.ref_attribute_array_manager();
-		const cgv::render::vertex_buffer* positions_buffer = sr.get_vertex_buffer_ptr(ctx, aam, "position");
+		const cgv::render::vertex_buffer* radius_buffer = sr.get_vertex_buffer_ptr(ctx, aam, "radius");
+		const cgv::render::vertex_buffer* index_buffer = sr.get_index_buffer_ptr(aam);
 
-		if(positions_buffer) {
-			if(measure_time)
-				time_query.begin_scope();
-			// Filter the sphere positions from the render data and write the result to the scratch filtered positions buffer.
-			filter.dispatch(ctx, cgv::gpgpu::begin(*positions_buffer), cgv::gpgpu::end<cgv::vec3>(*positions_buffer), cgv::gpgpu::begin(filtered_positions_buffer));
-			// read back the count of filtered (copied) positions.
-			size_t filtered_count = 0;
-			filter.read_count(ctx, filtered_count);
-			if(measure_time) {
-				double time = time_query.end_scope_and_collect();
-				std::cout << "Filtering done in " << (time / 1'000'000.0f) << " ms. Copied " << filtered_count << " elements." << std::endl;
-			}
-
-			// Copy the filtered positions back to the sphere render data positions buffer.
-			copy_helper.dispatch(ctx, cgv::gpgpu::begin(filtered_positions_buffer), cgv::gpgpu::begin(filtered_positions_buffer) + filtered_count, cgv::gpgpu::begin(*positions_buffer));
-			sphere_render_count = filtered_count;
+		if(!radius_buffer || !index_buffer) {
+			std::cout << "Error: could not get required buffers for filtering" << std::endl;
+			return;
 		}
 
-		post_redraw();
+		// Bind the required arguments. The argument names have to match the names used in the argument definition during initialization of the algorithm.
+		// The argument_binding_list determines the bindings between named arguments and their actual values.
+		cgv::gpgpu::argument_binding_list arguments;
+		arguments.bind_uniform("a_radius_min", threshold_min);
+		arguments.bind_uniform("a_radius_max", threshold_max);
+
+		if(measure_time)
+			time_query.begin_scope();
+
+		// Filter the spheres by their radii and write the resulting indices to the index buffer.
+		filter.dispatch(ctx, cgv::gpgpu::begin(*radius_buffer), cgv::gpgpu::end<cgv::vec3>(*radius_buffer), cgv::gpgpu::begin(*index_buffer), arguments);
+		// Read back the count of filtered indices.
+		size_t filtered_count = 0;
+		filter.read_count(ctx, filtered_count);
+		if(measure_time) {
+			double time = time_query.end_scope_and_collect();
+			std::cout << "Filtering done in " << (time / 1'000'000.0f) << " ms. Copied " << filtered_count << " elements." << std::endl;
+		}
+
+		sphere_render_count = filtered_count;
 	}
 
 	std::default_random_engine random_engine;
@@ -206,11 +197,9 @@ private:
 	const float radius_min = 0.005f;
 	const float radius_max = 0.05f;
 	cgv::render::sphere_render_data<> spheres;
-	cgv::render::vertex_buffer filtered_positions_buffer;
 	int sphere_render_count = -1;
 
-	cgv::gpgpu::copy copy_helper;
-	cgv::gpgpu::copy_if filter;
+	cgv::gpgpu::select_if filter;
 	float threshold_min = radius_min;
 	float threshold_max = radius_max;
 
