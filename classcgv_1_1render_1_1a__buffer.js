ce7ae21ab8804e376db7845b56a06c487cb1c361
1DaveBig <david.gross.de@gmail.com>
2025-11-03 15:28:08 +0000

    Deploying to gh-pages from @ sgumhold/cgv@632158d2199be9bdd423f467f2699a099d44588c 🚀

--- classcgv_1_1render_1_1a__buffer.js
+++ classcgv_1_1render_1_1a__buffer.js
@@ -2,7 +2,7 @@ var classcgv_1_1render_1_1a__buffer =
 [
     [ "a_buffer", "classcgv_1_1render_1_1a__buffer.html#a24f8cfb9af8acbb02f52735dc409e160", null ],
     [ "destruct", "classcgv_1_1render_1_1a__buffer.html#a438e138a0cf2eedc638109494a0d2784", null ],
-    [ "disable", "classcgv_1_1render_1_1a__buffer.html#ae66c6d82f7e0583da5778f65151f1f10", null ],
+    [ "disable", "classcgv_1_1render_1_1a__buffer.html#a9097b8765aab0487f717f9f0cc4fadab", null ],
     [ "enable", "classcgv_1_1render_1_1a__buffer.html#a777f4f37ea82b3cd12d252a1d11dc01c", null ],
     [ "finish_frame", "classcgv_1_1render_1_1a__buffer.html#adf9f12d102f35ccc166d7e8bae5e214c", null ],
     [ "init", "classcgv_1_1render_1_1a__buffer.html#a45d0aab0792c79d62bc7032bcb765b45", null ],
@@ -11,7 +11,7 @@ var classcgv_1_1render_1_1a__buffer =
     [ "depth_tex", "classcgv_1_1render_1_1a__buffer.html#a669c53051b97bcb5bff2156e47a397a1", null ],
     [ "depth_tex_unit", "classcgv_1_1render_1_1a__buffer.html#aba1c95cc46ff93c601dcf4133143f2ab", null ],
     [ "fragments_per_pixel", "classcgv_1_1render_1_1a__buffer.html#a24b651f05fb4feca5b5931a8715dd68f", null ],
-    [ "node_buffer_counter", "classcgv_1_1render_1_1a__buffer.html#a000d5c25182df0de6df27d58a983b484", null ],
     [ "node_counter_binding_point", "classcgv_1_1render_1_1a__buffer.html#aa5b8516e15b937f0196f3a48a72acbca", null ],
+    [ "node_counter_buffer", "classcgv_1_1render_1_1a__buffer.html#a9817b36e2f471e5456d835ad56242c77", null ],
     [ "nodes_per_pixel", "classcgv_1_1render_1_1a__buffer.html#a0b66fb7b28c9fdde1a17a9e6f899a35d", null ]
 ];
