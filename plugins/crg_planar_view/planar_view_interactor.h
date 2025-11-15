d192f05338628c2029426db5aa7d8d64e468a3d0
1DaveBig <david.gross.de@gmail.com>
2025-11-15 20:37:23 +0100

    Fix bugs in planar view interactor and cleanup code

--- plugins/crg_planar_view/planar_view_interactor.h
+++ plugins/crg_planar_view/planar_view_interactor.h
@@ -6,28 +6,6 @@
 #include <cgv/gui/event_handler.h>
 #include <cgv/gui/provider.h>
 
-
-class planar_view
-{
-	cgv::dvec2 focus;
-	cgv::dvec2 center;
-	double y_extent;
-	double rotation;
-	planar_view();
-	void set_default_values();
-	/// return model view transformation matrix
-	cgv::dmat4 get_modelview_matrix() const;
-	/// return orthogonal projection matrix based on the given aspect ratio
-	cgv::dmat4 get_projection_matrix(double aspect) const;
-	/// rotate view around focus by angle given in radians
-	void rotate(double angle);
-	/// move in screen x and screen y directions by given step lengths in world coordinates
-	void pan(double step_x, double step_y);
-	/// zoom by given factor
-	void zoom(double factor);
-};
-
-
 /**
 * A view interactor to control the opengl camera in 2d via mouse and keyboard.
 */
@@ -36,11 +14,10 @@ class planar_view_interactor :
 	public cgv::gui::event_handler, 
 	public cgv::render::drawable,
 	public cgv::gui::provider
-
 {
 public:
 	/// constructor
-	planar_view_interactor(const char* name);
+	planar_view_interactor(const std::string& name);
 	/// return the type name 
 	std::string get_type_name() const;
 	/// overload to show the content of this object
@@ -55,14 +32,12 @@ public:
 	bool handle(cgv::gui::event& e);
 	/// overload to stream help information to the given output stream
 	void stream_help(std::ostream& os);
-	/// this method is called in one pass over all drawables before the draw method
-	void draw(cgv::render::context&);
 	/// 
 	void after_finish(cgv::render::context&);
 
-	///set default values
-	void set_default_values();
-	///return projection matrix (like glu perspective)
+	/// reset view to default values
+	void reset_view();
+	/// return projection matrix (like glu perspective)
 	const cgv::dmat4 get_projection() const;
 	/// return modelview matrix V=T((0,0,-distance)^T)* R_x(elevation) * R_y(azimut)* T(-target)
 	const cgv::dmat4 get_modelview() const;
@@ -98,20 +73,18 @@ public:
 	void keep_rotation_locked(bool lock);
 
 private:
-	float aspect;
-	bool pressed;
+	/// the aspect ratio of the viewport
+	float aspect = 1.0f;
 	/// target point of camera
-	cgv::vec2 target;
-	cgv::vec2 pos_down;
-	cgv::dmat4 MPW;
-
+	cgv::vec2 target = { 0.0f };
 	/// zoom 
-	float magnification;
-	
+	float magnification = 1.0f;;
 	/// rotation angle
-	float angle;
-
+	float angle = 0.0f;
 	/// whether to lock rotation
-	bool lock_rotation;
-	
+	bool lock_rotation = false;
+
+	bool pressed = false;
+	cgv::vec2 pos_down = { 0.0f };
+	cgv::dmat4 MPW = { 0.0f };
 };
