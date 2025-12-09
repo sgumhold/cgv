c9d7783a7894a6faa83c84392bd15747b4798bc4
1DaveBig <david.gross.de@gmail.com>
2025-12-09 10:24:02 +0100

    Improve value accuracy of math constants

--- cgv/math/constants.h
+++ cgv/math/constants.h
@@ -1,15 +1,9 @@
 #pragma once
 
-
-
-#define PI		3.141592654
-#define HALF_PI	1.570796327
-#define TWO_PI	6.283185307
-#define E		2.718281828 
-#define SQRT_2	1.414213562 
-#define SQRT_3	1.732050808
-#define GOLDEN_RATIO 1.618033989
-
-
-
-
+#define PI           3.14159265358979323846 // pi
+#define HALF_PI      1.57079632679489661923 // pi/2
+#define TWO_PI       6.28318530717958647692 // 2pi
+#define E            2.71828182845904523536 // e
+#define SQRT_2       1.41421356237309504880 // sqrt(2)
+#define SQRT_3       1,73205080756887729352 // sqrt(3)
+#define GOLDEN_RATIO 1.61803398874989484820 // (1+sqrt(5))/2
