455340a323c2361e603ccc220893932fd559c272
Paul Hempel <paul.hempel@tu-dresden.de>
2026-05-06 18:03:08 +0200

    Convert non-ASCII characters in comments from Windows-1252 to UTF-8

--- libs/cmc_crameri/schemes.h
+++ libs/cmc_crameri/schemes.h
@@ -9,7 +9,7 @@ Copyright (c) 2023, Fabio Crameri
 
 Permission is hereby granted, free of charge, to any person obtaining
 a copy of this software and associated documentation
-files (the îSoftwareî), to deal in the Software without restriction,
+files (the ‚ÄùSoftware‚Äù), to deal in the Software without restriction,
 including without limitation the rights to use, copy, modify,
 merge, publish, distribute, sublicense, and/or sell copies of the
 Software, and to permit persons to whom the Software is furnished
@@ -18,7 +18,7 @@ to do so, subject to the following conditions:
 The above copyright notice and this permission notice shall be
 included in all copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED îAS ISî, WITHOUT WARRANTY OF
+THE SOFTWARE IS PROVIDED ‚ÄùAS IS‚Äù, WITHOUT WARRANTY OF
 ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
 TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
 PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
