2bdf72b7ad602435fe2efe9c4ade99f05919768d
1DaveBig <david.gross.de@gmail.com>
2026-05-19 14:30:56 +0000

    Deploying to gh-pages from @ sgumhold/cgv@9f611c4dda222c69d0c6e83a93eaed5e10f207b0 🚀

--- classcgv_1_1media_1_1continuous__color__scheme.js
+++ classcgv_1_1media_1_1continuous__color__scheme.js
@@ -2,12 +2,12 @@ var classcgv_1_1media_1_1continuous__color__scheme =
 [
     [ "interpolator_type", "classcgv_1_1media_1_1continuous__color__scheme.html#adc7b0d01c8a591bb2b5678a8228574f3", null ],
     [ "continuous_color_scheme", "classcgv_1_1media_1_1continuous__color__scheme.html#a2e3c0c7eb22ea53378b6a833f45c0b8c", null ],
-    [ "continuous_color_scheme", "classcgv_1_1media_1_1continuous__color__scheme.html#a10bbba6cd42b0c069ef113d108c8ff48", null ],
-    [ "function", "classcgv_1_1media_1_1continuous__color__scheme.html#a312260e019c6b85986e789e0d56a5db2", null ],
+    [ "continuous_color_scheme", "classcgv_1_1media_1_1continuous__color__scheme.html#a8a155ed67aee677b9fe3f67fb0491c93", null ],
+    [ "function", "classcgv_1_1media_1_1continuous__color__scheme.html#a00e5a6e45be609465a58cdd9d7fdde65", null ],
     [ "get_interpolator", "classcgv_1_1media_1_1continuous__color__scheme.html#a1afc99c74861da92159610745bb54df3", null ],
     [ "interpolate", "classcgv_1_1media_1_1continuous__color__scheme.html#a319771e9f03de9fea35e9a4dabb09028", null ],
-    [ "linear", "classcgv_1_1media_1_1continuous__color__scheme.html#a9a19163b9e1767feb9f6add687c7e50f", null ],
-    [ "linear", "classcgv_1_1media_1_1continuous__color__scheme.html#a1556db491e6470137ab2023e51a07f87", null ],
+    [ "linear", "classcgv_1_1media_1_1continuous__color__scheme.html#ac39eacb742d1361038b4eaedb0667f86", null ],
+    [ "linear", "classcgv_1_1media_1_1continuous__color__scheme.html#a24eefdbfcc75dd6a336cebc93b3ea20f", null ],
     [ "quantize", "classcgv_1_1media_1_1continuous__color__scheme.html#a26bed9edf504585752ce5a7b1a9bb9a9", null ],
-    [ "smooth", "classcgv_1_1media_1_1continuous__color__scheme.html#a205e9e0e4b8930c9e03d219e00e8989f", null ]
+    [ "smooth", "classcgv_1_1media_1_1continuous__color__scheme.html#ae946cc157dff7c6e955a4aed644c3d08", null ]
 ];
